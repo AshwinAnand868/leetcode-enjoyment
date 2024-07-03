b2c9f22aab08cea35dd42d23fca3671b76dec6f3
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-07-02 21:49:25 -0300

    Time: 2 ms (97.83%) | Memory: 45 MB (12.09%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,33 +3,18 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         
         HashMap<Integer, Integer> numsHashMap = new HashMap<>();
-        int[] indices = new int[2];
-             
-        for(int i = 0; i < nums.length; i++) {
-            Integer potentialExistingIndex = numsHashMap.get(nums[i]);
-            
-            if(potentialExistingIndex != null) {
-                if(nums[i] * 2 == target) {
-                    indices[0] = potentialExistingIndex;
-                    indices[1] = i;
-                    System.out.println(potentialExistingIndex);
-                    return indices;
-                }
-            }
-            numsHashMap.put(nums[i], i);
-        }
         
         for(int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
             
-            if(numsHashMap.containsKey(diff) && numsHashMap.get(diff) != i) {
-                indices[0] = i;
-                indices[1] = numsHashMap.get(diff);
-                break;
+            if(numsHashMap.containsKey(diff)) {
+                return new int[] {numsHashMap.get(diff), i};
             }
+            
+            numsHashMap.put(nums[i], i);
         }
         
-        return indices;
+        return new int[]{};
         
     }
 }
