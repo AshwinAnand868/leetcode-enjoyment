4abd9307efa83509bf53ae9189be7c061aed3cf7
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-07-09 20:49:57 -0300

    Time: 3 ms (10.67%) | Memory: 44.7 MB (50.22%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,22 +1,31 @@
 class Solution {
+    // premature optimizstion is the root cuase of all evil
     public int removeDuplicates(int[] nums) {
         
         // creating a hashmap to store the value and count for that
         HashMap<Integer, Integer> map = new HashMap<>();
+        int nextUniqueIndex = 0;
         
         // loop through the array to fill the map
         for(int i = 0; i < nums.length; i++) {
+            
             if(map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
+                // not unique
+                // map.put(nums[i], map.get(nums[i]) + 1);
+                
             } else {
+                // unique
                 map.put(nums[i], 1);
+                nums[nextUniqueIndex] = nums[i];
+                nextUniqueIndex++;
             }
+            
         }
                 
         // satisfy the first condition - which is to keep the first k elements (size of hashmap) to be unique
-        for(int i = 0, j = 0; i < nums.length; i = i + map.get(nums[i]), j++) {
-            nums[j] = nums[i];
-        }
+//         for(int i = 0, j = 0; i < nums.length; i = i + map.get(nums[i]), j++) {
+//             nums[j] = nums[i];
+//         }
         
         // second condition to return k
         return map.size();
