361c82b73cc65122e37c1ddfb3b52f5e4d16f3db
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-11-18 09:53:57 -0500

    Time: 0 ms (100.00%) | Memory: 40.7 MB (75.02%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -9,17 +9,12 @@ class Solution {
             int pop = x % 10;
             x = x / 10;
 
-            System.out.println(reversedX * 10);
-
-            if (reversedX > max / 10 || (reversedX == max / 10 && pop > 7))
-                return 0;
-            if (reversedX < min / 10 || (reversedX == min / 10 && pop < -8))
+            if (reversedX > max / 10 || reversedX < min / 10)
                 return 0;
 
             reversedX = reversedX * 10 + pop;
         }
 
         return reversedX;
-
     }
 }
