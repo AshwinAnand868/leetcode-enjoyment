33f7e7c06890ee8d081544d6a821bd06dfc4256d
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-11-18 10:32:56 -0500

    Time: 4 ms (100.00%) | Memory: 44 MB (39.18%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,61 +1,23 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int[] arrayOfX = convertIntToArray(x);
         
-        // special case if the x consists of only 2 digits
-        if(arrayOfX.length == 2) {
-            return arrayOfX[0] == arrayOfX[1];
-        }
+        // negative number is not a palindrome for this case
+        if(x < 0) return false;
         
-        // or if the number is less than 0
-        if(x < 0)
-            return false;
+        int j = x; // copy of x
+        int r = 0;
+        int reversedJ = 0;
         
-        int i = 0;
-        int j = arrayOfX.length - 1;
-        boolean continueL = true;
-        boolean toReturn = false;
-        
-        while(i < j && continueL) {
-            if(arrayOfX[i] == arrayOfX[j]) {
-                i++;
-                j--;
-            } else {
-                continueL = false; // not a palindrome
-            }
-        }
-        
-        if(i == j) // it is a palindrome
-            toReturn = true;
-        
-        // if the number is even and i has crossed j with continueL being true, then it is a palindrome
-        if(!toReturn && continueL && (arrayOfX.length % 2) == 0 && i > j) {
-            toReturn = true;
-        }
-        
-        return toReturn;
-    }
-    
-    public int[] convertIntToArray(int x) {
-        List<Integer> toReturn = new ArrayList<>();
-        int temp =  x;
+        while(j != 0) {
+            r = j % 10; // get the remainder
+            j = j / 10; // take out the last digit from j
             
-        do {
-            toReturn.add(0, temp % 10);
-            temp /= 10;
-        } while(temp > 0);
+            reversedJ = reversedJ * 10 + r; // add the remainder to reversed value of j
+        }
         
-        return toReturn.stream().mapToInt(i -> i).toArray();
+        if(x == reversedJ)
+            return true;
+        
+        return false;
     }
-    
-//     public int[] convertIntToArray(int x) {
-//         String xStr = Integer.toString(x);
-//         int[] toReturn = new int[xStr.length()];
-        
-//         for(int i = 0; i < toReturn.length; i++) {
-//             toReturn[i] = xStr.charAt(i) - '0'; // getting int value for the character number
-//         }
-        
-//         return toReturn;
-//     }
 }
