5bc84202171db14a555ee18e2db5bb94c15ab5cf
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-11-22 09:43:20 -0500

    Time: 0 ms (100.00%) | Memory: 40.5 MB (29.44%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,10 +1,27 @@
 class Solution {
     public int fib(int n) {
         
-        // multiple base conditions
-        if(n == 0) return 0;
-        if(n == 1) return 1;
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        int a = 0;
+        int b = 1;
+        int temp = 0;
+
+        for (int i = 2; i <= n; i++) {
+            temp = a + b;
+            a = b;
+            b = temp; // b will be the result
+        }
+
+        return b;
         
-        return fib(n - 1) + fib(n - 2);
+        // multiple base conditions
+//         if(n == 0) return 0;
+//         if(n == 1) return 1;
+        
+//         return fib(n - 1) + fib(n - 2);
     }
 }
