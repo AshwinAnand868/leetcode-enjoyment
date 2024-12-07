4bf9e261e32b968decc1f9ccf63f1a8f391e1f01
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-06 22:54:19 -0500

    Time: 1 ms (91.65%) | Memory: 41.7 MB (16.78%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,28 +1,48 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
         
-        int minOps = Integer.MAX_VALUE;
-        int i, j;
-        int runningMinOps = 0; 
+        // using just one loop with TC O(n) - learned
         
-        for(int x = 0; x < blocks.length(); x++) {
+        int minOps = Integer.MAX_VALUE;
+        
+        // high inclusive, low exclusive
+        for(int hi = 0, lo = -1, white = 0; hi < blocks.length(); ++hi) {
+            white += blocks.charAt(hi) == 'W' ? 1 : 0;
             
-            i = x;
-            j = k + i - 1;
-            
-            if(j >= blocks.length()) break;
+            if(hi - lo >= k) // when diff reaches the window size
+            {
+                minOps = Math.min(minOps, white);
                 
-            while(i <= j) {
-                if(blocks.charAt(i) == 'W') {
-                    runningMinOps++;
-                }
-                i++;
+                // low exclusive
+                white -= blocks.charAt(++lo) == 'W' ? 1 : 0;
             }
             
-            minOps = Math.min(minOps, runningMinOps);
-            runningMinOps = 0;
             
         }
+        
+        // using loops with TC O(n*k)
+//         int minOps = Integer.MAX_VALUE;
+//         int i, j;
+//         int runningMinOps = 0; 
+        
+//         for(int x = 0; x < blocks.length(); x++) {
+            
+//             i = x;
+//             j = k + i - 1;
+            
+//             if(j >= blocks.length()) break;
+                
+//             while(i <= j) {
+//                 if(blocks.charAt(i) == 'W') {
+//                     runningMinOps++;
+//                 }
+//                 i++;
+//             }
+            
+//             minOps = Math.min(minOps, runningMinOps);
+//             runningMinOps = 0;
+            
+//         }
         
         return minOps;
     }
