3ee0cc054a5e9030093570a810ddee248dabb00b
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-09 06:40:59 -0500

    Time: 1 ms (100.00%) | Memory: 42.1 MB (86.08%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -1,29 +1,51 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-       
         
-        char[] cArr = s.toCharArray();
-        int k = 2;
-        
+        int n = s.length();
+        char[] alpha = new char[26];
         int maxLength = 0;
-        Map<Character, Integer> map;
         
-        for(int i = 0; i < cArr.length; ++i) {
-            map = new HashMap<>();
+        for(int leftPointer = 0, rightPointer = 0; rightPointer < n; rightPointer++) {
+            char rightChar = s.charAt(rightPointer);
+            alpha[rightChar - 'a']++;
             
-            for(int j = i; j < cArr.length; ++j) {
-                map.put(cArr[j], map.getOrDefault(cArr[j], 0) + 1);
-                
-                if(map.get(cArr[j]) > 2) {
-                    break;
-                }
-                
-                maxLength = Math.max(maxLength, j - i + 1);
+            if(alpha[rightChar - 'a'] <= 2) {
+                maxLength = Math.max(maxLength, rightPointer - leftPointer + 1);
+                continue; // skip the next part
             }
             
-            
+            while(alpha[rightChar - 'a'] > 2) {
+                // we decrement the count at the left most char
+                alpha[s.charAt(leftPointer) - 'a']--;
+                leftPointer++;
+            }
         }
         
         return maxLength;
+        
+       
+        
+//         char[] cArr = s.toCharArray();
+        
+//         int maxLength = 0;
+//         Map<Character, Integer> map;
+        
+//         for(int i = 0; i < cArr.length; ++i) {
+//             map = new HashMap<>();
+            
+//             for(int j = i; j < cArr.length; ++j) {
+//                 map.put(cArr[j], map.getOrDefault(cArr[j], 0) + 1);
+                
+//                 if(map.get(cArr[j]) > 2) {
+//                     break;
+//                 }
+                
+//                 maxLength = Math.max(maxLength, j - i + 1);
+//             }
+            
+            
+//         }
+        
+//         return maxLength;
     }
 }
