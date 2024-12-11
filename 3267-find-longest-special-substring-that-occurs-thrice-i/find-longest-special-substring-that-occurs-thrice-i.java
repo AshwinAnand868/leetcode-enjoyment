8e71280158e27b78e1b8b287ea2d40181deaeb29
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-10 22:25:45 -0500

    Time: 11 ms (62.14%) | Memory: 45 MB (53.21%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,48 +1,62 @@
 class Solution {
     public int maximumLength(String s) {
-        Map<Map.Entry<Character, Integer>, Integer> map = new HashMap<>();
-        int count;
+        
+        Map<String, Integer> map = new HashMap<>();
+        int maxLength = -1;
+        
+        // storing special strings and their frequency in the array
+        for(int i = 0; i < s.length(); ++i) {
+            StringBuilder cunS = new StringBuilder();
             
-        for(int i = 0; i < s.length(); i++) {
-            count = 1;
-            map.put(Map.entry(s.charAt(i), count), map.getOrDefault(Map.entry(s.charAt(i), count), 0) + 1);
-            
-            for(int j = i; j < s.length(); j++) {
-                if(j + 1 < s.length() && s.charAt(j) == s.charAt(j + 1)) {
-                    count++;
-                    map.put(Map.entry(s.charAt(i), count), map.getOrDefault(Map.entry(s.charAt(i), count), 0) + 1);
+            for(int j = i; j < s.length(); ++j) {
+                if(cunS.length() == 0 || s.charAt(j) == cunS.charAt(cunS.length() - 1)) {
+                    cunS.append(s.charAt(j));
+                    String str = cunS.toString();
+                    map.put(str, map.getOrDefault(str, 0) + 1);
                 } else {
-                    break; // if same character substring is finished
+                    break;
                 }
             }
         }
         
-        int result = -1;
+        for(Map.Entry<String, Integer> entry: map.entrySet()) {
+            String key = entry.getKey();
+            Integer val = entry.getValue();
+            
+            // frequency of the special substring is > 2
+            if(val > 2) {
+                maxLength = Math.max(maxLength, key.length());
+            }
+        }
         
-                for(Map.Entry<Map.Entry<Character, Integer>, Integer> entry: map.entrySet()) {
-                    if(entry.getValue() >= 3) {
-                        result = Math.max(entry.getKey().getValue(), result);
-                    }
-                }
+        return maxLength;
+        
+//         Map<Map.Entry<Character, Integer>, Integer> map = new HashMap<>();
+//         int count;
+            
+//         for(int i = 0; i < s.length(); i++) {
+//             count = 1;
+//             map.put(Map.entry(s.charAt(i), count), map.getOrDefault(Map.entry(s.charAt(i), count), 0) + 1);
+            
+//             for(int j = i; j < s.length(); j++) {
+//                 if(j + 1 < s.length() && s.charAt(j) == s.charAt(j + 1)) {
+//                     count++;
+//                     map.put(Map.entry(s.charAt(i), count), map.getOrDefault(Map.entry(s.charAt(i), count), 0) + 1);
+//                 } else {
+//                     break; // if same character substring is finished
+//                 }
+//             }
+//         }
+        
+//         int result = -1;
+        
+//                 for(Map.Entry<Map.Entry<Character, Integer>, Integer> entry: map.entrySet()) {
+//                     if(entry.getValue() >= 3) {
+//                         result = Math.max(entry.getKey().getValue(), result);
+//                     }
+//                 }
         
         
-        return result;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+//         return result;
     }
 }
