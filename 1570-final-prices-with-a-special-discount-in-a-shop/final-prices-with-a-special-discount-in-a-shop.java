081047e6a06bbd8dafd2eff3724b56f4ba06a311
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-18 10:22:46 -0500

    Time: 1 ms (98.90%) | Memory: 44.1 MB (25.23%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,16 +1,25 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
-        int[] finalPrices = Arrays.copyOf(prices, n); 
+        boolean valueUpdated = false;
+        int[] finalPrices = new int[n];
+
+        // int[] finalPrices = Arrays.copyOf(prices, n); 
         
         for(int i = 0; i < n; ++i) {
             int price = prices[i];
+            valueUpdated = false;
             
             for(int j = i + 1; j < n; ++j) {
                 if(prices[j] <= price) {
                     finalPrices[i] = price - prices[j];
+                    valueUpdated = true;
                     break;
                 }
+            }
+            
+            if(!valueUpdated) {
+                finalPrices[i] = prices[i];
             }
         }
         
