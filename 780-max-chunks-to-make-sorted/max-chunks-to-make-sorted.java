0ff244ca3b6d270bdf903f9e1dd76d20f4b70bd3
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-19 11:55:24 -0500

    Time: 0 ms (100.00%) | Memory: 41.3 MB (15.09%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,28 +1,45 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-                int chunks = 0;
-
+        int cummSum = 0;
+        int indicesSum = 0;
+        int chunks = 0;
         int n = arr.length;
-        int[] prefixMax = Arrays.copyOf(arr, n);
-        int[] suffixMin = Arrays.copyOf(arr, n);
-
-        for(int i = 1; i < n; ++i) {
-            prefixMax[i] = Math.max(prefixMax[i], prefixMax[i - 1]);
-        }
-
-        for(int i = n - 2; i >= 0; --i) {
-            suffixMin[i] = Math.min(suffixMin[i], suffixMin[i + 1]);
-        }
-
+        
         for(int i = 0; i < n; ++i) {
-            int previousMaxToI = i > 0 ? prefixMax[i - 1] : -1;
-            int suffixMinFromI = suffixMin[i];
-
-            if(previousMaxToI < suffixMinFromI) {
+            cummSum += arr[i]; // cummulative sum till now
+            indicesSum += i;
+            
+            if(indicesSum == cummSum) {
                 chunks++;
             }
         }
-
+        
         return chunks;
+        
+        
+//         int chunks = 0;
+
+//         int n = arr.length;
+//         int[] prefixMax = Arrays.copyOf(arr, n);
+//         int[] suffixMin = Arrays.copyOf(arr, n);
+
+//         for(int i = 1; i < n; ++i) {
+//             prefixMax[i] = Math.max(prefixMax[i], prefixMax[i - 1]);
+//         }
+
+//         for(int i = n - 2; i >= 0; --i) {
+//             suffixMin[i] = Math.min(suffixMin[i], suffixMin[i + 1]);
+//         }
+
+//         for(int i = 0; i < n; ++i) {
+//             int previousMaxToI = i > 0 ? prefixMax[i - 1] : -1;
+//             int suffixMinFromI = suffixMin[i];
+
+//             if(previousMaxToI < suffixMinFromI) {
+//                 chunks++;
+//             }
+//         }
+
+//         return chunks;
     }
 }
