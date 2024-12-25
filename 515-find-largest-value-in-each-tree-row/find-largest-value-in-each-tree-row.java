c03e2ef86a894663416600476577947cf32743a4
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-25 16:44:10 -0500

    Time: 1 ms (94.70%) | Memory: 45.4 MB (65.61%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -14,39 +14,57 @@
  * }
  */
 class Solution {
-    public List<Integer> largestValues(TreeNode root) {
 
-        if (root == null)
-            return new ArrayList<>();
+    public void buildLargestValues(TreeNode root, List<Integer> result, int depth) {
+        if(root == null) return;
 
-        List<Integer> result = new ArrayList<>();
-
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int n = queue.size();
-            // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a)); // max heap
-            int max = Integer.MIN_VALUE;
-
-            while (n-- > 0) {
-                TreeNode current = queue.poll();
-                // pq.offer(current.val);
-                max = Math.max(max, current.val);
-
-                if (current.left != null) {
-                    queue.offer(current.left);
-                }
-
-                if (current.right != null) {
-                    queue.offer(current.right);
-                }
-            }
-
-            // int max = pq.poll();
-            result.add(max);
+        // visiting this depth for the first time
+        if(result.size() == depth) {
+            result.add(root.val);
+        } else {
+            // visiting the depth for the 2nd time, means
+            // there is already a value at that index.
+            // compare both values and set the greater
+            int current = result.get(depth);
+            result.set(depth, Math.max(current, root.val));
         }
 
+        buildLargestValues(root.left, result, depth + 1);
+        buildLargestValues(root.right, result, depth + 1);
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        buildLargestValues(root, result, 0);
         return result;
+
+        // BFS
+        // Queue<TreeNode> queue = new ArrayDeque<>();
+        // queue.offer(root);
+
+        // while (!queue.isEmpty()) {
+        //     int n = queue.size();
+        //     // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a)); // max heap
+        //     int max = Integer.MIN_VALUE;
+
+        //     while (n-- > 0) {
+        //         TreeNode current = queue.poll();
+        //         // pq.offer(current.val);
+        //         max = Math.max(max, current.val);
+
+        //         if (current.left != null) {
+        //             queue.offer(current.left);
+        //         }
+
+        //         if (current.right != null) {
+        //             queue.offer(current.right);
+        //         }
+        //     }
+
+        //     // int max = pq.poll();
+        //     result.add(max);
+        // }
+
+        // return result;
     }
 }
