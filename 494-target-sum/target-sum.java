93869d743da76ff596f12b0c0fb38a7372411725
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-27 10:55:14 -0500

    Time: 151 ms (40.18%) | Memory: 45.6 MB (10.20%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,21 +1,29 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return recursive(nums, 0, 0, target);
+        Map<String, Integer> map = new HashMap<>();
+        return recursive(nums, 0, 0, target, map);
     }
 
-    private int recursive(int[] nums, int currentIdx, int currentSum, int target) {
+    private int recursive(int[] nums, int currentIdx, int currentSum, int target, Map<String, Integer> map) {
         if(currentIdx == nums.length) {
             if(currentSum == target) {
                 return 1;
             } else {
                 return 0;
             }
-        } 
+        }
+
+        String key = Integer.toString(currentIdx) + "_" + Integer.toString(currentSum);
+
+        if(map.containsKey(key)) {
+            return map.get(key);
+        }
 
         // exploring both possibilities for each integer
-        int plus = recursive(nums, currentIdx + 1, currentSum + nums[currentIdx], target);
-        int minus = recursive(nums, currentIdx + 1, currentSum - nums[currentIdx], target);
+        int plus = recursive(nums, currentIdx + 1, currentSum + nums[currentIdx], target, map);
+        int minus = recursive(nums, currentIdx + 1, currentSum - nums[currentIdx], target, map);
 
+        map.put(key, plus + minus);
         return plus + minus;
     }
 }
