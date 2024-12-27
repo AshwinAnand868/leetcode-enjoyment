383855513a6c2dcf394c0836cb885f7d5994724a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-27 11:10:14 -0500

    Time: 10 ms (61.31%) | Memory: 45.1 MB (21.84%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,10 +1,20 @@
 class Solution {
+    int totalSum;
+
     public int findTargetSumWays(int[] nums, int target) {
-        Map<String, Integer> map = new HashMap<>();
-        return recursive(nums, 0, 0, target, map);
+        // Map<String, Integer> map = new HashMap<>();
+        
+        totalSum = Arrays.stream(nums).sum();
+
+        int[][] memo = new int[nums.length][2 * totalSum + 1];
+        for(int[] row: memo) {
+            Arrays.fill(row, -1);
+        }
+        
+        return calcWays(nums, 0, 0, target, memo);
     }
 
-    private int recursive(int[] nums, int currentIdx, int currentSum, int target, Map<String, Integer> map) {
+    private int calcWays(int[] nums, int currentIdx, int currentSum, int target, int[][] memo) {
         if(currentIdx == nums.length) {
             if(currentSum == target) {
                 return 1;
@@ -13,17 +23,21 @@ class Solution {
             }
         }
 
-        String key = Integer.toString(currentIdx) + "_" + Integer.toString(currentSum);
-
-        if(map.containsKey(key)) {
-            return map.get(key);
+        // String key = Integer.toString(currentIdx) + "_" + Integer.toString(currentSum);
+        if(memo[currentIdx][currentSum + totalSum] != -1) {
+            return memo[currentIdx][currentSum + totalSum]; // just a state
         }
 
-        // exploring both possibilities for each integer
-        int plus = recursive(nums, currentIdx + 1, currentSum + nums[currentIdx], target, map);
-        int minus = recursive(nums, currentIdx + 1, currentSum - nums[currentIdx], target, map);
+        // if(map.containsKey(key)) {
+        //     return map.get(key);
+        // }
 
-        map.put(key, plus + minus);
-        return plus + minus;
+        // exploring both possibilities for each integer
+        int plus = calcWays(nums, currentIdx + 1, currentSum + nums[currentIdx], target, memo);
+        int minus = calcWays(nums, currentIdx + 1, currentSum - nums[currentIdx], target, memo);
+
+        // map.put(key, plus + minus);
+        memo[currentIdx][currentSum + totalSum] = plus + minus;
+        return memo[currentIdx][currentSum + totalSum];
     }
 }
