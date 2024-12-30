f92b1cdaca97418df92c940d33a369928c6282de
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-30 12:39:08 -0500

    Time: 0 ms (100.00%) | Memory: 42 MB (76.24%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,57 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int j = 0, i = m; j < n; j++) {
-            nums1[i] = nums2[j];
-            i++;
+        int[] tempArray = new int[m];
+
+        for(int i = 0; i < m; ++i) {
+            tempArray[i] = nums1[i];
         }
-        Arrays.sort(nums1);
+
+        int i = 0;
+        int j = 0;
+        int indx = 0;
+
+        while(i < m && j < n) {
+            if(tempArray[i] < nums2[j]) {
+                nums1[indx] = tempArray[i];
+                i++;
+            } else {
+                nums1[indx] = nums2[j];
+                j++;
+            }
+            indx++;
+        }
+
+        // leftover lements
+        for(int k = i; k < m; k++) {
+            nums1[indx++] = tempArray[k];
+        }
+
+        for(int k = j; k < n; k++) {
+            nums1[indx++] = nums2[k];
+        }
+
+
+        // int mIndex = m - 1;
+        // int nIndex = n - 1;
+        // int r = m + n - 1;
+
+        // while(nIndex >= 0) {
+        //     if(mIndex >= 0 && nums1[mIndex] >= nums2[nIndex]) {
+        //         nums1[r] = nums1[mIndex];
+        //         mIndex--;
+        //     } else {
+        //         nums1[r] = nums2[nIndex];
+        //         nIndex--;
+        //     }
+
+        //     r--;
+        // }
+
+        // for (int j = 0, i = m; j < n; j++) {
+        //     nums1[i] = nums2[j];
+        //     i++;
+        // }
+        // Arrays.sort(nums1);
 
     }
 }
