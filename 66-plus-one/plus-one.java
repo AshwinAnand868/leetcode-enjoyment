3bd52e3ba0c20acbb92823104bd896f2d1f4fc7e
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-30 11:16:48 -0500

    Time: 0 ms (100.00%) | Memory: 41.7 MB (67.51%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+        int carry = -1;
+        int n = digits.length;
+
+        for(int i = n - 1; i >= 0; --i) {
+            int temp = digits[i] + 1;
+            if(temp < 10) {
+                digits[i] += 1;
+                return digits;
+            } else {
+                carry = (digits[i] + 1) / 10;
+                digits[i] = 0;
+            }
+        }
+
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = carry;
+
+        return newDigits;
+        // long largeNumber = digits[0];
+        // int n = digits.length;
+
+        // if(n == 1) {
+        //     digits[0] = digits[0] + 1;
+
+        //     if(digits[0] > 9) {
+        //         return new int[] { digits[0] / 10, digits[0] % 10};
+        //     }
+        //     return digits;
+        // }
+
+        // for(int i = 1; i < n; ++i) {
+        //     largeNumber = largeNumber * 10 + digits[i]; // construct the large number
+        // }
+
+
+        // System.out.println(largeNumber);
+        // // plus one
+        // largeNumber += 1;
+
+        // // convert large number to string
+        // String large = Long.toString(largeNumber);
+
+        // int[] result = large.chars()
+        //     .map(Character::getNumericValue)
+        //     .toArray();
+
+        // return result;
+    }
+}
