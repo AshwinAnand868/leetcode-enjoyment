8f6b54241a03dae458faa68b74e25fc621d95f60
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-29 19:22:31 -0500

    Time: 0 ms (100.00%) | Memory: 41.7 MB (80.73%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,14 +1,33 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int nextUniqueIndex = 0;
-        
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != val) {
-                nums[nextUniqueIndex] = nums[i];
-                nextUniqueIndex++;
+        int j = -1;
+        int n = nums.length;
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == val) {
+                j = i;
+                break;
             }
         }
-        
-        return nextUniqueIndex;
+
+        if (j == -1) {
+            return n;
+        }
+
+        // j index is point to "val" at the starting
+        int i = j + 1;
+        while (i < n) {
+            if (nums[i] == val) {
+                ++i;
+            } else {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                ++j;
+            }
+        }
+
+        return j;
+
     }
 }
