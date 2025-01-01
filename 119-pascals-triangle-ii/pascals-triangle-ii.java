441b4ca43e349782a1f1ee65a67294863a793534
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2024-12-31 23:41:47 -0500

    Time: 0 ms (100.00%) | Memory: 41.2 MB (50.54%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,24 +1,42 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> pT = new ArrayList<>();
 
-        for(int i = 0; i <= rowIndex; ++i) {
-            List<Integer> row = new ArrayList<>();
+        int[] arr = new int[rowIndex + 1];
+        arr[0] = 1;
 
-            for(int j = 0; j <= i; ++j) {
-                row.add(1);
+        for(int i = 1; i <= rowIndex; ++i) {
+            for(int j = i; j > 0; --j) {
+                arr[j] += arr[j - 1];
             }
-
-            for(int j = 1; j < i; ++j) {
-                int aboveValOnLeft = pT.get(i - 1).get(j - 1);
-                int aboveValOnRight = pT.get(i - 1).get(j);
-
-                row.set(j, aboveValOnLeft + aboveValOnRight);
-            }
-
-            pT.add(row);
         }
 
-        return pT.get(rowIndex);
+        List<Integer> res = new ArrayList<>();
+
+        for(int a : arr) {
+            res.add(a);
+        }
+
+        return res;
+
+        // List<List<Integer>> pT = new ArrayList<>();
+
+        // for(int i = 0; i <= rowIndex; ++i) {
+        //     List<Integer> row = new ArrayList<>();
+
+        //     for(int j = 0; j <= i; ++j) {
+        //         row.add(1);
+        //     }
+
+        //     for(int j = 1; j < i; ++j) {
+        //         int aboveValOnLeft = pT.get(i - 1).get(j - 1);
+        //         int aboveValOnRight = pT.get(i - 1).get(j);
+
+        //         row.set(j, aboveValOnLeft + aboveValOnRight);
+        //     }
+
+        //     pT.add(row);
+        // }
+
+        // return pT.get(rowIndex);
     }
 }
