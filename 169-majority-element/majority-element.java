41a40db8ffc6d29a32740fd0237af49de4c89b8d
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-02 11:06:14 -0500

    Time: 1 ms (99.83%) | Memory: 53.4 MB (24.31%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,21 +1,43 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majorityElement = 0;
+
+        // Boyer-Moore Majority Vote Algorithm
+
+        int majority = nums[0];
         int n = nums.length;
+        int count = 1;
 
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < n; ++i) {
-            map.merge(nums[i], 1, Integer::sum);
-        }
-
-        int halfN = n / 2;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > halfN) {
-                majorityElement = entry.getKey();
+        for(int i = 1; i < n; ++i) {
+            if(count == 0) { // a fresh cycle
+                majority = nums[i]; // update majority
+                count++; // update count
+            } else if(nums[i] == majority) // if current element is majority
+            {
+                count++; // continue incrementing the count
+            } else {
+                count--; // decrement count if current is not majority
             }
         }
 
-        return majorityElement;
+
+        return majority;
+
+        // int majorityElement = 0;
+        // int n = nums.length;
+
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // for (int i = 0; i < n; ++i) {
+        //     map.merge(nums[i], 1, Integer::sum);
+        // }
+
+        // int halfN = n / 2;
+        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        //     if (entry.getValue() > halfN) {
+        //         majorityElement = entry.getKey();
+        //     }
+        // }
+
+        // return majorityElement;
     }
 }
