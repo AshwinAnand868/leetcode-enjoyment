e9151d86ae4a71d07cbe536d633799c91165f597
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-04 17:23:54 -0500

    Time: 0 ms (100.00%) | Memory: 50.4 MB (35.07%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,11 +1,46 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        int xor = 0;
+    public int singleNonDuplicate(int[] arr) {
 
-        for (int i = 0; i < nums.length; i++) {
-            xor = xor ^ nums[i];
+        int n = arr.length;
+        int low = 1; // starting from 2nd element
+        int high = n - 2; // going till 2nd last element
+
+        if (n == 1) {
+            return arr[0];
         }
 
-        return xor;
+        // Edge case I
+        if (arr[0] != arr[1]) {
+            return arr[0];
+        }
+
+        if (arr[n - 1] != arr[n - 2]) {
+            return arr[n - 1];
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1]) {
+                return arr[mid];
+            } else if ((mid % 2 == 1 && arr[mid] == arr[mid + 1]) ||
+                    (mid % 2 == 0 && arr[mid] == arr[mid - 1])) {
+                // eliminate left half - single is in right half
+                high = mid - 1;
+            } else {
+                // eliminate right half - single is in left half
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+
+        // int xor = 0;
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     xor = xor ^ nums[i];
+        // }
+
+        // return xor;
     }
 }
