1b6838795aaca4d8e81c1353fab097e64e846bed
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-05 22:05:36 -0500

    Time: 0 ms (100.00%) | Memory: 41.8 MB (82.71%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,56 +1,75 @@
 class Solution {
 
-    public void merge(int[] arr, int low, int mid, int high) {
-        int[] temp = new int[high - low + 1];
+    // public void merge(int[] arr, int low, int mid, int high) {
+    //     int[] temp = new int[high - low + 1];
 
-        int left = low;
-        int right = mid + 1;
-        int idx = 0;
+    //     int left = low;
+    //     int right = mid + 1;
+    //     int idx = 0;
 
-        while(left <= mid && right <= high) {
-            if(arr[left] <= arr[right]) {
-                temp[idx] = arr[left];
-                left++;
-            } else {
-                temp[idx] = arr[right];
-                right++;
-            }
+    //     while(left <= mid && right <= high) {
+    //         if(arr[left] <= arr[right]) {
+    //             temp[idx] = arr[left];
+    //             left++;
+    //         } else {
+    //             temp[idx] = arr[right];
+    //             right++;
+    //         }
 
-            idx++;
-        }
+    //         idx++;
+    //     }
 
-        while(left <= mid) {
-            temp[idx] = arr[left];
-            idx++;
-            left++;
-        }
+    //     while(left <= mid) {
+    //         temp[idx] = arr[left];
+    //         idx++;
+    //         left++;
+    //     }
 
-        while(right <= high) {
-            temp[idx] = arr[right];
-            idx++;
-            right++;
-        }
+    //     while(right <= high) {
+    //         temp[idx] = arr[right];
+    //         idx++;
+    //         right++;
+    //     }
 
-        for(int i = 0; i< temp.length; ++i) {
-            arr[low + i] = temp[i];
-        }
-    }
+    //     for(int i = 0; i< temp.length; ++i) {
+    //         arr[low + i] = temp[i];
+    //     }
+    // }
 
-    public void mergeSort(int[] arr, int low, int high) {
-        if(low == high) {
-            return;
-        }
+    // public void mergeSort(int[] arr, int low, int high) {
+    //     if(low == high) {
+    //         return;
+    //     }
 
-        int mid = (low + high) / 2;
+    //     int mid = (low + high) / 2;
 
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
-        merge(arr, low, mid, high);
-    }
+    //     mergeSort(arr, low, mid);
+    //     mergeSort(arr, mid + 1, high);
+    //     merge(arr, low, mid, high);
+    // }
 
     public void sortColors(int[] arr) {
 
-        mergeSort(arr, 0, arr.length - 1);
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+        for(int i = 0; i < arr.length; ++i) {
+            if(arr[i] == 0) cnt0++;
+            else if(arr[i] == 1) cnt1++;
+            else cnt2++;
+        }
+
+        for(int i = 0; i < arr.length; ++i) {
+            if(i < cnt0) {
+                arr[i] = 0;
+            } else if(i < cnt0 + cnt1) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 2;
+            }
+        }
+
+
+        // mergeSort(arr, 0, arr.length - 1);
         
 
         // int n = arr.length;
