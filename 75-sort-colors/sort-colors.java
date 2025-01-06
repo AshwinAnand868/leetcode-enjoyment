cd1ec0ef5321de597d85f4e47721d07599fa33ff
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-05 22:42:00 -0500

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.27%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -50,23 +50,44 @@ class Solution {
 
     public void sortColors(int[] arr) {
 
-        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        int low = 0, mid = 0, high = arr.length - 1;
 
-        for(int i = 0; i < arr.length; ++i) {
-            if(arr[i] == 0) cnt0++;
-            else if(arr[i] == 1) cnt1++;
-            else cnt2++;
-        }
+        while(mid <= high) {
+            if(arr[mid] == 0) {
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
 
-        for(int i = 0; i < arr.length; ++i) {
-            if(i < cnt0) {
-                arr[i] = 0;
-            } else if(i < cnt0 + cnt1) {
-                arr[i] = 1;
+                low++;
+                mid++;
+            } else if(arr[mid] == 1) {
+                mid++;
             } else {
-                arr[i] = 2;
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+
+                high--;
             }
         }
+
+        // int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+        // for(int i = 0; i < arr.length; ++i) {
+        //     if(arr[i] == 0) cnt0++;
+        //     else if(arr[i] == 1) cnt1++;
+        //     else cnt2++;
+        // }
+
+        // for(int i = 0; i < arr.length; ++i) {
+        //     if(i < cnt0) {
+        //         arr[i] = 0;
+        //     } else if(i < cnt0 + cnt1) {
+        //         arr[i] = 1;
+        //     } else {
+        //         arr[i] = 2;
+        //     }
+        // }
 
 
         // mergeSort(arr, 0, arr.length - 1);
