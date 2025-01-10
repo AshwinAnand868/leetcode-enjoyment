93581a7b2c8a1d72a176df1cf9ad4cd634a38d9b
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-10 11:23:24 -0500

    Time: 1 ms (64.48%) | Memory: 46.2 MB (61.44%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,35 +1,33 @@
 class Solution {
+    public int[] getLeftMaxArray(int[] height) {
+        int[] leftMax = new int[height.length];
+        leftMax[0] = height[0];
 
-    public int[] getLeftMostArray(int[] height) {
-        int[] leftMost = new int[height.length];
-        leftMost[0] = height[0];
-
-        for(int i = 1; i < leftMost.length; ++i) {
-            leftMost[i] = Math.max(leftMost[i - 1], height[i]);
+        for (int i = 1; i < leftMax.length; ++i) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
 
-        return leftMost;
+        return leftMax;
     }
 
-    public int[] getRightMostArray(int[] height) {
+    public int[] getRightMaxArray(int[] height) {
         int n = height.length;
-        int[] rightMost = new int[n];
-        rightMost[n - 1] = height[n - 1];
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
 
-        for(int i = n - 2; i >= 0; --i) {
-            rightMost[i] = Math.max(rightMost[i + 1], height[i]);
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
         }
 
-        return rightMost;
+        return rightMax;
     }
-
 
     public int trap(int[] height) {
-        int[] leftMost = getLeftMostArray(height);
-        int[] rightMost = getRightMostArray(height);
+        int[] leftMost = getLeftMaxArray(height);
+        int[] rightMost = getRightMaxArray(height);
         int sum = 0;
 
-        for(int i = 0; i < height.length; ++i) {
+        for (int i = 0; i < height.length; ++i) {
             int width = 1;
             int h = Math.min(leftMost[i], rightMost[i]) - height[i];
 
