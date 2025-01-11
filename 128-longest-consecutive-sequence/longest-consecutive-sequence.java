60fefcd6466e24003253214f7e7f92bffcfe942d
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-10 22:25:53 -0500

    Time: 33 ms (48.32%) | Memory: 68.5 MB (5.26%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,38 +1,68 @@
 class Solution {
     public static int longestConsecutive(int[] nums) {
+
         if (nums.length == 0) {
             return 0;
         }
 
-        PriorityQueue<Integer> minNums = new PriorityQueue<Integer>();
+        Set<Integer> set = new HashSet<>();
+        int n = nums.length;
 
-        for (int i = 0; i < nums.length; ++i) {
-            minNums.offer(nums[i]);
+        for (int i = 0; i < n; i++) {
+            set.add(nums[i]);
         }
 
-        int count = 1;
-        int maxCount = 1;
+        int count = 1, longestCount = 1;
 
-        int prevElement = minNums.poll();
+        for (int i : set) {
+            if (!set.contains(i - 1)) {
+                int y = i;
 
-        while (!minNums.isEmpty()) {
-
-            int nextElement = minNums.poll();
-
-            if (prevElement == nextElement) {
-                continue;
+                while (set.contains(y + 1)) {
+                    count++;
+                    y++;
+                }
             }
 
-            if (prevElement + 1 == nextElement) {
-                count++;
-            } else {
-                count = 1;
-            }
-
-            maxCount = Math.max(maxCount, count);
-            prevElement = nextElement;
+            longestCount = Math.max(count, longestCount);
+            count = 1;
         }
 
-        return maxCount;
+        return longestCount;
+
+        // if (nums.length == 0) {
+        // return 0;
+        // }
+
+        // PriorityQueue<Integer> minNums = new PriorityQueue<Integer>();
+
+        // for (int i = 0; i < nums.length; ++i) {
+        // minNums.offer(nums[i]);
+        // }
+
+        // int count = 1;
+        // int maxCount = 1;
+
+        // int prevElement = minNums.poll();
+
+        // while (!minNums.isEmpty()) {
+
+        // int nextElement = minNums.poll();
+
+        // if (prevElement == nextElement) {
+        // continue;
+        // }
+
+        // if (prevElement + 1 == nextElement) {
+        // count++;
+        // } else {
+        // count = 1;
+        // }
+
+        // maxCount = Math.max(maxCount, count);
+        // prevElement = nextElement;
+        // }
+
+        // return maxCount;
     }
 }
