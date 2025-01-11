f73aac6fe7f1c9b76bbd7eb413df88ee6930683d
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-11 08:44:50 -0500

    Time: 1 ms (73.89%) | Memory: 45.5 MB (87.09%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,25 +1,58 @@
 class Solution {
 
     public void setZeroes(int[][] matrix) {
-        int[] row = new int[matrix.length];
-        int[] col = new int[matrix[0].length];
+        // int[] row = new int[matrix.length]; -- matrix[..][0]
+        // int[] col = new int[matrix[0].length]; -- matrix[0][..]
+
+        int col0 = 1;
 
         for(int i = 0; i < matrix.length; ++i) {
             for(int j = 0; j < matrix[0].length; ++j) {
                 if(matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    // mark the ith row
+                    matrix[i][0] = 0;
+
+                    // mark the jth col
+                    if(j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
                 }
             }
         }
 
-        for(int i = 0; i < matrix.length; ++i) {
-            for(int j = 0; j < matrix[0].length; ++j) {
-                if(row[i] == 1 || col[j] == 1) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for(int i = m - 1; i >= 1; --i) {
+            for(int j = n - 1; j >= 1; --j) {
+                if(matrix[i][j] != 0 && matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
+
+        if(matrix[0][0] == 0) {
+            for(int j = 1; j < n; ++j) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(col0 == 0) {
+            for(int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+
+
+        // for(int i = 0; i < matrix.length; ++i) {
+        //     for(int j = 0; j < matrix[0].length; ++j) {
+        //         if(row[i] == 1 || col[j] == 1) {
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
     }
 
 
