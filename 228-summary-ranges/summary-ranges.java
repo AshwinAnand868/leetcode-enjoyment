e3d7e5d74334208a2e60929086895c2d2d94f7a9
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-14 12:37:06 -0500

    Time: 5 ms (80.29%) | Memory: 42 MB (31.24%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -1,42 +1,60 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
 
-        if(nums.length == 0) return new ArrayList<>();
-
-        if (nums.length == 1) {
-            List<String> one = new ArrayList<>();
-            one.add(nums[0] + "");
-            return one;
-        }
-
         List<String> ranges = new ArrayList<>();
 
-        int start = nums[0];
-        int end = start;
+        for (int i = 0; i < nums.length; ++i) {
+            int start = nums[i];
 
-        for (int i = 0; i < nums.length - 1; ++i) {
-            if (nums[i] + 1 == nums[i + 1]) {
-                end = nums[i + 1];
+            while(i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+
+            if(start != nums[i]) {
+                ranges.add("" + start + "->" + nums[i]);
             } else {
-                String temp = "";
-
-                if (start >= end) {
-                    temp = "" + start;
-                } else {
-                    temp = start + "->" + end;
-                }
-
-                ranges.add(temp);
-                start = nums[i + 1];
+                ranges.add("" + start);
             }
         }
 
-        if (start == nums[nums.length - 1]) {
-            ranges.add("" + start);
-        } else {
-            ranges.add(start + "->" + end);
-        }
-
         return ranges;
+
+        // if(nums.length == 0) return new ArrayList<>();
+
+        // if (nums.length == 1) {
+        //     List<String> one = new ArrayList<>();
+        //     one.add(nums[0] + "");
+        //     return one;
+        // }
+
+        // List<String> ranges = new ArrayList<>();
+
+        // int start = nums[0];
+        // int end = start;
+
+        // for (int i = 0; i < nums.length - 1; ++i) {
+        //     if (nums[i] + 1 == nums[i + 1]) {
+        //         end = nums[i + 1];
+        //     } else {
+        //         String temp = "";
+
+        //         if (start >= end) {
+        //             temp = "" + start;
+        //         } else {
+        //             temp = start + "->" + end;
+        //         }
+
+        //         ranges.add(temp);
+        //         start = nums[i + 1];
+        //     }
+        // }
+
+        // if (start == nums[nums.length - 1]) {
+        //     ranges.add("" + start);
+        // } else {
+        //     ranges.add(start + "->" + end);
+        // }
+
+        // return ranges;
     }
 }
