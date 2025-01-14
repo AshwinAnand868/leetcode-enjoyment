430aeb7455e629d9524831551042882bce1bebe0
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-14 12:14:23 -0500

    Time: 5 ms (80.29%) | Memory: 41.8 MB (46.49%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+
+        if(nums.length == 0) return new ArrayList<>();
+
+        if (nums.length == 1) {
+            List<String> one = new ArrayList<>();
+            one.add(nums[0] + "");
+            return one;
+        }
+
+        List<String> ranges = new ArrayList<>();
+
+        int start = nums[0];
+        int end = start;
+
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i] + 1 == nums[i + 1]) {
+                end = nums[i + 1];
+            } else {
+                String temp = "";
+
+                if (start >= end) {
+                    temp = "" + start;
+                } else {
+                    temp = start + "->" + end;
+                }
+
+                ranges.add(temp);
+                start = nums[i + 1];
+            }
+        }
+
+        if (start == nums[nums.length - 1]) {
+            ranges.add("" + start);
+        } else {
+            ranges.add(start + "->" + end);
+        }
+
+        return ranges;
+    }
+}
