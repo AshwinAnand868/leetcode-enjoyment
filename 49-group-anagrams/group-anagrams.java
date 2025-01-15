b13c4ac732b6fd638ecaa9f47fce830ce69be985
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-15 13:00:26 -0500

    Time: 11 ms (31.06%) | Memory: 48.9 MB (14.74%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,4 +1,25 @@
 class Solution {
+
+    private String generate(String input) {
+        int[] counts = new int[26];
+
+        for (char ch : input.toCharArray()) {
+            counts[ch - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 26; ++i) {
+            int freq = counts[i];
+
+            if (freq > 0) {
+                sb.append(String.valueOf((char) i + 'a').repeat(freq));
+            }
+        }
+
+        return sb.toString();
+    }
+
     public List<List<String>> groupAnagrams(String[] strs) {
 
         int n = strs.length;
@@ -8,20 +29,34 @@ class Solution {
         for (int i = 0; i < n; ++i) {
             String temp = strs[i];
 
-            char[] tempCharArray = temp.toCharArray();
+            String newWord = generate(temp);
 
-            Arrays.sort(tempCharArray);
-
-            String sorted = String.valueOf(tempCharArray);
-
-            if (sortedToAnagrams.containsKey(sorted)) {
-                sortedToAnagrams.get(sorted).add(temp);
+            if (sortedToAnagrams.containsKey(newWord)) {
+                sortedToAnagrams.get(newWord).add(temp);
             } else {
                 List<String> tempList = new ArrayList<>();
                 tempList.add(temp);
-                sortedToAnagrams.put(sorted, tempList);
+                sortedToAnagrams.put(newWord, tempList);
             }
         }
+
+        // for (int i = 0; i < n; ++i) {
+        // String temp = strs[i];
+
+        // char[] tempCharArray = temp.toCharArray();
+
+        // Arrays.sort(tempCharArray);
+
+        // String sorted = String.valueOf(tempCharArray);
+
+        // if (sortedToAnagrams.containsKey(sorted)) {
+        // sortedToAnagrams.get(sorted).add(temp);
+        // } else {
+        // List<String> tempList = new ArrayList<>();
+        // tempList.add(temp);
+        // sortedToAnagrams.put(sorted, tempList);
+        // }
+        // }
 
         for (List<String> list : sortedToAnagrams.values()) {
             result.add(list);
