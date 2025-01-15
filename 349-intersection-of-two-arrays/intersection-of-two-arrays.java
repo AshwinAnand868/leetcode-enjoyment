7483f1c6b2a28d3ad883c3d142342390c131fbcc
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-14 22:30:52 -0500

    Time: 4 ms (34.27%) | Memory: 42.8 MB (98.12%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,26 +1,65 @@
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
 
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+    private boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
 
-        int i = 0;
-        int j = 0;
-        Set<Integer> intersect = new HashSet<>();
-        
-        while(i < nums1.length && j < nums2.length) {
-            if(nums1[i] == nums2[j]) {
-            intersect.add(nums1[i]);
-                i++;
-                j++;
-            } else if(nums1[i] < nums2[j]) {
-                i++;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(nums[mid] == target) {
+                return true;
+            } else if(nums[mid] < target) {
+                low = mid + 1;
             } else {
-                j++;
+                high = mid - 1;
             }
         }
 
-        return intersect.stream().mapToInt(e -> e).toArray();
+        return false;
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+
+        for(Integer num : nums1) {
+            if(binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+
+        int i = 0;
+        int[] result = new int[set.size()];
+
+        for(Integer num : set) {
+            result[i++] = num;
+        }
+
+        return result;
+
+
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+
+        // int i = 0;
+        // int j = 0;
+        // Set<Integer> intersect = new HashSet<>();
+        
+        // while(i < nums1.length && j < nums2.length) {
+        //     if(nums1[i] == nums2[j]) {
+        //     intersect.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     } else if(nums1[i] < nums2[j]) {
+        //         i++;
+        //     } else {
+        //         j++;
+        //     }
+        // }
+
+        // return intersect.stream().mapToInt(e -> e).toArray();
 
         // Set<Integer> set1 = new HashSet<>();
         // List<Integer> result = new ArrayList<>();
