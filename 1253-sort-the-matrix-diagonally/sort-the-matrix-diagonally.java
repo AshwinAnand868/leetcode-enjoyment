e4d46baca4b2a5f8dc4d0297d32d8f9998ecee3d
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-17 10:47:38 -0500

    Time: 13 ms (21.21%) | Memory: 45.3 MB (29.48%) - LeetSync

---
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        Map<Integer, List<Integer>> diagonalsMap = new HashMap<>();
+        int m = mat.length;
+        int n = mat[0].length;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (diagonalsMap.containsKey(i - j)) {
+                    diagonalsMap.get(i - j).add(mat[i][j]);
+                } else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(mat[i][j]);
+                    diagonalsMap.put(i - j, list);
+                }
+
+            }
+        }
+
+        // sort the diagonals
+        for (List<Integer> diagonal : diagonalsMap.values()) {
+            Collections.sort(diagonal);
+        }
+
+        // now we will put them back in sorted fashion
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; --j) {
+                List<Integer> diagonal = diagonalsMap.get(i - j);
+                mat[i][j] = diagonal.get(diagonal.size() - 1); // get the last element of the current diagonal list
+                diagonal.remove(diagonal.size() - 1); // remove the last element of the current diagonal list
+            }
+        }
+
+        return mat;
+    }
+}
