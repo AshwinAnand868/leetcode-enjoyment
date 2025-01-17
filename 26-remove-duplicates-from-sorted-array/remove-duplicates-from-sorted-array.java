20330e34132a5db82f086d6497fefcca170989ba
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-17 12:18:35 -0500

    Time: 1 ms (65.14%) | Memory: 44.7 MB (85.39%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,26 +1,38 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // two pointers approach
-        int i = 0;
-        int j = 1;
+
         int n = nums.length;
-        int k = n;
-
-        if (n == 1) {
-            return 1;
-        }
-
-        while (j < n) {
-            if (nums[i] == nums[j]) {
-                ++j;
-            } else if (nums[i] != nums[j]) {
-                nums[i + 1] = nums[j];
-                ++i;
+        int i = 1;
+        
+        for(int j = 1; j < n; ++j) {
+            if(nums[j] != nums[j - 1]) {
+                nums[i] = nums[j];
+                i++;
             }
         }
 
-        k = i + 1;
+        return i;
+        // // two pointers approach
+        // int i = 0; // slow pointer
+        // int j = 1; // fast pointer
+        // int n = nums.length;
+        // int k = n;
 
-        return k;
+        // if (n == 1) {
+        //     return 1;
+        // }
+
+        // while (j < n) {
+        //     if (nums[i] == nums[j]) {
+        //         ++j;
+        //     } else if (nums[i] != nums[j]) {
+        //         nums[i + 1] = nums[j];
+        //         ++i; 
+        //     }
+        // }
+
+        // k = i + 1;
+
+        // return k;   
     }
 }
