51290e30e4a1070ad9091da5819e89790447d0e4
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-20 10:18:05 -0500

    Time: 2 ms (85.03%) | Memory: 46.2 MB (29.91%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,27 +1,63 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
+        // Revision 1
         int n = nums.length;
-        int j = -1;
+        int i = -1;
 
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == 0) {
-                j = i;
+        for(int k = 0; k < n; ++k) {
+            if(nums[k] == 0) {
+                i = k;
                 break;
             }
         }
 
-        if (j == -1) {
+        if(i == -1) {
             return;
         }
 
-        for (int i = j + 1; i < n; ++i) {
-            if (nums[i] != 0) {
+        for(int j = i + 1; j < n; ++j) {
+            if(nums[j] != 0) {
                 int temp = nums[i];
-                nums[i] = nums[j]; // a[j] will point to zero always
+                nums[i] = nums[j];
                 nums[j] = temp;
-                j++; // move j to next zero
+                ++i;
             }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int n = nums.length;
+        // int j = -1;
+
+        // for (int i = 0; i < n; ++i) {
+        //     if (nums[i] == 0) {
+        //         j = i;
+        //         break;
+        //     }
+        // }
+
+        // if (j == -1) {
+        //     return;
+        // }
+
+        // for (int i = j + 1; i < n; ++i) {
+        //     if (nums[i] != 0) {
+        //         int temp = nums[i];
+        //         nums[i] = nums[j]; // a[j] will point to zero always
+        //         nums[j] = temp;
+        //         j++; // move j to next zero
+        //     }
+        // }
     }
 }
