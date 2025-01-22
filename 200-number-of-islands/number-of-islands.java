bed9d29671ebcf2797882be98d8d29657554b80a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-22 15:34:24 -0500

    Time: 5 ms (34.57%) | Memory: 51.9 MB (24.70%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,46 +1,82 @@
+class Pair {
+    int i;
+    int j;
+
+    Pair() {
+        this.i = 0;
+        this.j = 0;
+    }
+
+    Pair(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+}
+
 class Solution {
 
-    private int[][] directions = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+    private int[][] directions = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
 
     public boolean isSafe(char[][] grid, int i, int j) {
-        if(i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == '1') {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == '1') {
             return true;
         }
         return false;
     }
 
-    public void dfs(char[][] grid, int i, int j) {
-        if(!isSafe(grid, i, j)) {
-            return;
+    public void bfs(char[][] grid, int i, int j) {
+        Queue<Pair> pairs = new LinkedList<>();
+        pairs.offer(new Pair(i, j));
+        grid[i][j] = '$';
+
+        while (!pairs.isEmpty()) {
+            Pair pair = pairs.poll();
+
+            for (int[] dir : directions) {
+                int newI = dir[0] + pair.i;
+                int newJ = dir[1] + pair.j;
+                
+                if(isSafe(grid, newI, newJ)) {
+                    pairs.offer(new Pair(newI, newJ));
+                    grid[newI][newJ] = '$';
+                }
+            }
         }
 
-        grid[i][j] = '$'; // mark it as visited
-
-        for(int[] dir: directions) {
-            int newI = dir[0] + i;
-            int newJ = dir[1] + j;
-
-            dfs(grid, newI, newJ);
-        }
     }
 
+    // public void dfs(char[][] grid, int i, int j) {
+    // if(!isSafe(grid, i, j)) {
+    // return;
+    // }
+
+    // grid[i][j] = '$'; // mark it as visited
+
+    // for(int[] dir: directions) {
+    // int newI = dir[0] + i;
+    // int newJ = dir[1] + j;
+
+    // dfs(grid, newI, newJ);
+    // }
+    // }
+
     public int numIslands(char[][] grid) {
-        if(grid.length == 0)
+        if (grid.length == 0)
             return 0;
-        
+
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == '1') {
-                    dfs(grid, i, j);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j);
                     count++;
                 }
             }
         }
-        
+
         return count;
 
     }
