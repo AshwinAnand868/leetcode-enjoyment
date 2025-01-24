fe75ead88505538c58146cf35beead67741d5b95
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-01-24 13:01:18 -0500

    Time: 5 ms (25.78%) | Memory: 43.8 MB (13.02%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,24 +1,43 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> resultList = new ArrayList<>();
 
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        HashMap<Integer, Integer> valToFreqMapNums1 = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
 
-        int i = 0, j = 0;
+        for(int val : nums1) {
+            valToFreqMapNums1.merge(val, 1, Integer::sum);
+        }
 
-        while(i < nums1.length && j < nums2.length) {
-            if(nums1[i] < nums2[j]) {
-                i++;
-            } else if(nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                resultList.add(nums1[i]);
-                ++i;
-                ++j;
+
+        for(int val : nums2) {
+            if(valToFreqMapNums1.containsKey(val) && valToFreqMapNums1.get(val) > 0) {
+                result.add(val);
+                valToFreqMapNums1.merge(val, -1, Integer::sum);
             }
         }
 
-        return resultList.stream().mapToInt(val -> val).toArray();
+        return result.stream().mapToInt(val -> val).toArray();
+
+
+    //     List<Integer> resultList = new ArrayList<>();
+
+    //     Arrays.sort(nums1);
+    //     Arrays.sort(nums2);
+
+    //     int i = 0, j = 0;
+
+    //     while(i < nums1.length && j < nums2.length) {
+    //         if(nums1[i] < nums2[j]) {
+    //             i++;
+    //         } else if(nums1[i] > nums2[j]) {
+    //             j++;
+    //         } else {
+    //             resultList.add(nums1[i]);
+    //             ++i;
+    //             ++j;
+    //         }
+    //     }
+
+    //     return resultList.stream().mapToInt(val -> val).toArray();
     }
 }
