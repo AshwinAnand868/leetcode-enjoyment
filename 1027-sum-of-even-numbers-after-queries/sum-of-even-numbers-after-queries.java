7b8ae5a0e17883751a3cd902fe1c0da00485d18a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-02-04 05:34:39 -0500

    Time: 5 ms (67.46%) | Memory: 49.4 MB (38.17%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.java
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.java
@@ -2,22 +2,54 @@ class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int[] answer = new int[queries.length];
         int j = 0;
+
+        // calculate initial sum of even values
+        int sumEven = 0;
+
+        for(int i = 0; i < nums.length; ++i) {
+            if(nums[i] % 2 == 0) {
+                sumEven += nums[i];
+            }
+        }
+
         for(int[] query : queries) {
             int val = query[0];
             int index = query[1];
 
-            nums[index] = val + nums[index];
+            int old = nums[index];
 
-            // calculate the sum of even values of nums
-            int sum = 0;
-            for(int i = 0; i < nums.length; ++i) {
-                if(nums[i] % 2 == 0) {
-                    sum += nums[i];
-                }
+            if(old % 2 == 0) {
+                sumEven -= old;
             }
 
-            answer[j] = sum;
+            nums[index] = val + nums[index];
+
+            if(nums[index] % 2 == 0) {
+                sumEven += nums[index];
+            }
+
+            answer[j] = sumEven;
+
+            // if(old % 2 != 0 && nums[index] % 2 == 0) {
+            //     answer[j] = sumEvenInitial + nums[index];
+            // } else if(old % 2 == 0 && nums[index] % 2 != 0) {
+            //     answer[j] = sumEvenInitial - old;
+            // } else if(old % 2 == 0 && nums[index] % 2 == 0) {
+            //     sumEvenInitial -= old;
+            //     sumEvenInitial += nums[index];
+            //     answer[j] = sumEvenInitial;
+            // }
+
+            // calculate the sum of even values of nums
+            // int sum = 0;
+            // for(int i = 0; i < nums.length; ++i) {
+            //     if(nums[i] % 2 == 0) {
+            //         sum += nums[i];
+            //     }
+            // }
+
             ++j;
+
         }
 
         return answer;
