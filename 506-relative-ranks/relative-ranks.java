4022e9e1a59f224b4389d11d82d3c35c2553d7cc
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-02-06 22:13:01 +0530

    Time: 3 ms (96.82%) | Memory: 45.7 MB (81.10%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -4,29 +4,65 @@ class Solution {
 
         int n = score.length;
 
-        TreeMap<Integer, Integer> map = new TreeMap<>(Comparator.reverseOrder());
+        int maxScore = 0;
 
-        for(int i = 0; i < score.length; ++i) {
-            map.put(score[i], i);
+        for (int i = 0; i < n; ++i) {
+            maxScore = Math.max(maxScore, score[i]);
         }
+
+        int[] scoreToIndex = new int[maxScore + 1];
+
+        for (int i = 0; i < n; ++i) {
+            scoreToIndex[score[i]] = i + 1;
+        }
+        
 
         int place = 1;
 
-        for(int key : map.keySet()) {
+        for (int i = maxScore; i >= 0; --i) {
+            if (scoreToIndex[i] == 0)
+                continue;
 
-            if(place == 1) {
-                ranks[map.get(key)] = "Gold Medal";
-            } else if(place == 2) {
-                ranks[map.get(key)] = "Silver Medal";
-            } else if(place == 3) {
-                ranks[map.get(key)] = "Bronze Medal";
+            int originalIndex = scoreToIndex[i] - 1;
+
+            if (place == 1) {
+                ranks[originalIndex] = "Gold Medal";
+            } else if (place == 2) {
+                ranks[originalIndex] = "Silver Medal";
+            } else if (place == 3) {
+                ranks[originalIndex] = "Bronze Medal";
             } else {
-                ranks[map.get(key)] = String.valueOf(place);
+                ranks[originalIndex] = String.valueOf(place);
             }
 
             place++;
         }
 
         return ranks;
+
+        // TreeMap<Integer, Integer> map = new TreeMap<>(Comparator.reverseOrder());
+
+        // for(int i = 0; i < score.length; ++i) {
+        // map.put(score[i], i);
+        // }
+
+        // int place = 1;
+
+        // for(int key : map.keySet()) {
+
+        // if(place == 1) {
+        // ranks[map.get(key)] = "Gold Medal";
+        // } else if(place == 2) {
+        // ranks[map.get(key)] = "Silver Medal";
+        // } else if(place == 3) {
+        // ranks[map.get(key)] = "Bronze Medal";
+        // } else {
+        // ranks[map.get(key)] = String.valueOf(place);
+        // }
+
+        // place++;
+        // }
+
+        // return ranks;
     }
 }
