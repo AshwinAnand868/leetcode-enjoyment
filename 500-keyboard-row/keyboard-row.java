8a68f8a41336feff1b8586975ca27b3a487b8640
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-02-06 19:31:33 +0530

    Time: 1 ms (51.82%) | Memory: 41.8 MB (15.99%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -1,36 +1,71 @@
 class Solution {
     public String[] findWords(String[] words) {
 
-        String qwerty = "qwertyuiop";
-        String asdf = "asdfghjkl";
-        String zx = "zxcvbnm";
-        int count1 = 0, count2 = 0, count3 = 0;
-        List<String> result = new ArrayList<>();
+        String[] rows = new String[3];
 
-        for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < words[i].length(); j++) {
-                if (qwerty.indexOf(words[i].toLowerCase().charAt(j)) != -1) {
-                    count1++;
-                }
+        Set<Character> row1 = stringToCharSet("qwertyuiop"); 
+        Set<Character> row2 = stringToCharSet("asdfghjkl");
+        Set<Character> row3 = stringToCharSet("zxcvbnm");
 
-                if (asdf.indexOf(words[i].toLowerCase().charAt(j)) != -1) {
-                    count2++;
-                }
+        List<String> res = new ArrayList<>();
 
-                if (zx.indexOf(words[i].toLowerCase().charAt(j)) != -1) {
-                    count3++;
-                }
+        for(String word : words) {
+            if(isValid(row1, word) || isValid(row2, word) || isValid(row3, word)){
+                res.add(word);    
             }
-
-            if(count1 == words[i].length() || count2 == words[i].length() || count3 == words[i].length()) {
-                result.add(words[i]);
-            }
-
-            count1 = 0;
-            count2 = 0;
-            count3 = 0;
         }
 
-        return result.toArray(new String[0]);
+        return res.toArray(new String[0]);
+
+        // String qwerty = "qwertyuiop";
+        // String asdf = "asdfghjkl";
+        // String zx = "zxcvbnm";
+        // int count1 = 0, count2 = 0, count3 = 0;
+        // List<String> result = new ArrayList<>();
+
+        // for (int i = 0; i < words.length; i++) {
+        //     for (int j = 0; j < words[i].length(); j++) {
+        //         if (qwerty.indexOf(words[i].toLowerCase().charAt(j)) != -1) {
+        //             count1++;
+        //         }
+
+        //         if (asdf.indexOf(words[i].toLowerCase().charAt(j)) != -1) {
+        //             count2++;
+        //         }
+
+        //         if (zx.indexOf(words[i].toLowerCase().charAt(j)) != -1) {
+        //             count3++;
+        //         }
+        //     }
+
+        //     if(count1 == words[i].length() || count2 == words[i].length() || count3 == words[i].length()) {
+        //         result.add(words[i]);
+        //     }
+
+        //     count1 = 0;
+        //     count2 = 0;
+        //     count3 = 0;
+        // }
+
+        // return result.toArray(new String[0]);
+    }
+
+    private boolean isValid(Set<Character> row, String word) {
+        for(char c : word.toCharArray()) {
+            if(!row.contains(Character.toLowerCase(c))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Set<Character> stringToCharSet(String str) {
+        Set<Character> set = new HashSet<>();
+
+        for(char c: str.toCharArray()) {
+            set.add(c);
+        }
+
+        return set;
     }
 }
