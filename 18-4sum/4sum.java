80dfbd4c4b79275cdbde18195f84313075f8986c
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-02-07 13:16:42 +0530

    Time: 464 ms (5.00%) | Memory: 45.7 MB (9.42%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,34 +1,34 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length; // size of the array
-        Set<List<Integer>> st = new HashSet<>();
+        Set<List<Integer>> set = new HashSet<>();
+        int n = nums.length;
 
-        // checking all possible quadruplets:
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                Set<Long> hashset = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    // taking bigger data type
-                    // to avoid integer overflow:
+        for(int i = 0; i < n; ++i) {
+            for(int j = i + 1; j < n; ++j) {
+                Set<Long> betweenJAndK = new HashSet<>();
+
+                for(int k = j + 1; k < n; ++k) {
                     long sum = nums[i] + nums[j];
                     sum += nums[k];
                     long fourth = target - sum;
-                    if (hashset.contains(fourth)) {
+
+                    if(betweenJAndK.contains(fourth)) {
                         List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
                         temp.add(nums[k]);
                         temp.add((int) fourth);
                         temp.sort(Integer::compareTo);
-                        st.add(temp);
+                        set.add(temp);
                     }
-                    // put the kth element into the hashset:
-                    hashset.add((long) nums[k]);
+
+                    betweenJAndK.add((long) nums[k]);
                 }
+
+
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(st);
+        List<List<Integer>> ans = new ArrayList<>(set);
         return ans;
-
     }
 }
