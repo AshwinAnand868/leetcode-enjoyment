d99e52d6c5ecd34d1d49f0eb6cd4d678a4bce01a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-02-10 09:49:46 +0530

    Time: 5 ms (99.36%) | Memory: 46 MB (98.92%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,14 +1,33 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        
-        Arrays.sort(nums);
 
+        int[] exists = new int[20001];
         int maxSum = 0;
+        boolean odd = true;
 
-        for(int i = 0; i < nums.length - 1; i+=2) {
-            maxSum += nums[i];            
+        for (int i = 0; i < nums.length; ++i) {
+            exists[nums[i] + 10000]++;
+        }
+
+        for (int i = 0; i < exists.length; ++i) {
+            while (exists[i] > 0) {
+                if (odd) {
+                    maxSum += i - 10000;
+                }
+                odd = !odd; // toggle to select odds only
+                exists[i]--; // decrementing the value
+            }
         }
 
         return maxSum;
+        // Arrays.sort(nums);
+
+        // int maxSum = 0;
+
+        // for(int i = 0; i < nums.length - 1; i+=2) {
+        // maxSum += nums[i];
+        // }
+
+        // return maxSum;
     }
 }
