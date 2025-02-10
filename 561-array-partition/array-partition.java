419561b80d04a92260f5755e03a327348bbfafb0
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-02-10 09:18:56 +0530

    Time: 13 ms (32.66%) | Memory: 46.6 MB (76.12%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,13 +1,14 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
+        
         Arrays.sort(nums);
 
-        int sum = 0;
+        int maxSum = 0;
 
-        for(int i = 0; i < nums.length - 1; i += 2) {
-            sum += nums[i];
+        for(int i = 0; i < nums.length - 1; i+=2) {
+            maxSum += nums[i];            
         }
 
-        return sum;
+        return maxSum;
     }
 }
