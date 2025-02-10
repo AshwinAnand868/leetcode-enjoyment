8c9a53586aedc6285fba26d7e8564726c409b39e
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-02-10 13:34:54 +0530

    Time: 4 ms (100.00%) | Memory: 46.1 MB (30.82%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,17 +1,31 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        HashMap<Integer, Integer> candyTypeToCount = new HashMap<>();
+        boolean[] types = new boolean[200001];
+        int count = 0;
+        int max = candyType.length / 2;
 
         for(int i = 0; i < candyType.length; ++i) {
-            candyTypeToCount.merge(candyType[i], 1, Integer::sum);
+            // index calc
+            int t = 100000 + candyType[i];
+            if(!types[t]) {
+                count++; // new candy type
+                if(count == max) return max;
+                types[t] = true;
+            }
         }
+        return count;
+        // HashMap<Integer, Integer> candyTypeToCount = new HashMap<>();
 
-        int n = candyType.length;
+        // for(int i = 0; i < candyType.length; ++i) {
+        //     candyTypeToCount.merge(candyType[i], 1, Integer::sum);
+        // }
 
-        if(candyTypeToCount.size() >= (n/2)) {
-            return n/2;
-        } else {
-            return candyTypeToCount.size();
-        }
+        // int n = candyType.length;
+
+        // if(candyTypeToCount.size() >= (n/2)) {
+        //     return n/2;
+        // } else {
+        //     return candyTypeToCount.size();
+        // }
     }
 }
