19b93c93324a4346c67290341d1036772a899755
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-02-25 12:32:17 +0530

    Time: 5 ms (75.76%) | Memory: 41.6 MB (85.61%) - LeetSync

--- 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
@@ -1,26 +1,38 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
-        char[] chars = s.toCharArray();
+        // char[] chars = s.toCharArray();
         int seconds = 0;
+        int zeros = 0;
 
-        for (int i = 0; i < chars.length; ++i) {
-            boolean didSwap = false;
-            for (int j = 0; j < chars.length - 1; ++j) {
-                if (s.charAt(j) == '0' && s.charAt(j + 1) == '1') {
-                    chars[j + 1] = '0';
-                    chars[j] = '1';
-                    didSwap = true;
-                }
+        for(int i = 0; i < s.length(); ++i) {
+            zeros += s.charAt(i) == '0' ? 1 : 0;
+
+            if(zeros > 0 && s.charAt(i) == '1') {
+                seconds = Math.max(seconds + 1, zeros);
             }
-
-            s = new String(chars);
-
-            if (!didSwap) {
-                break;
-            }
-            seconds++;
         }
 
         return seconds;
+
+
+        // for (int i = 0; i < chars.length; ++i) {
+        //     boolean didSwap = false;
+        //     for (int j = 0; j < chars.length - 1; ++j) {
+        //         if (s.charAt(j) == '0' && s.charAt(j + 1) == '1') {
+        //             chars[j + 1] = '0';
+        //             chars[j] = '1';
+        //             didSwap = true;
+        //         }
+        //     }
+
+        //     s = new String(chars);
+
+        //     if (!didSwap) {
+        //         break;
+        //     }
+        //     seconds++;
+        // }
+
+        // return seconds;
     }
 }
