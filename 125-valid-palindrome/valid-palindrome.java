0494da0883956851ce9c5646ad5564210fba240a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-03 17:37:08 +0530

    Time: 3 ms (74.76%) | Memory: 43.1 MB (76.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,57 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
 
-        for(char c : s.toCharArray()) {
-            if(Character.isLetterOrDigit(c)) {
-                sb.append(c);
-            }
+        // Approach - 1
+        // Checking edge cases
+
+        if(s == null || s.isEmpty()) {
+            return true;
         }
 
-        StringBuilder reversed = new StringBuilder(sb.toString().toLowerCase());
-        reversed.reverse();
+        int start = 0;
+        int end = s.length() - 1;
 
-        return sb.toString().toLowerCase().equals(reversed.toString());
+        while(start <= end) {
+            char currFirst = s.charAt(start);
+            char currEnd = s.charAt(end);
 
+            if(!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            }
+
+            if(!Character.isLetterOrDigit(currEnd)) {
+                end--;
+            }
+
+            if(Character.isLetterOrDigit(currFirst) && Character.isLetterOrDigit(currEnd)) {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currEnd)) {
+        			return false;
+        		}
+
+                start++;
+                end--;
+            }
+
+
+        }
+
+        return true;
+
+        // Approach - 2
+        // StringBuilder sb = new StringBuilder();
+
+        // for(char c : s.toCharArray()) {
+        //     if(Character.isLetterOrDigit(c)) {
+        //         sb.append(c);
+        //     }
+        // }
+
+        // StringBuilder reversed = new StringBuilder(sb.toString().toLowerCase());
+        // reversed.reverse();
+
+        // return sb.toString().toLowerCase().equals(reversed.toString());
+
+        // Approach 3
         // String cleanString = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
 
         // StringBuilder sb = new StringBuilder(cleanString).reverse();
