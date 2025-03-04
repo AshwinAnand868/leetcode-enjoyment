6fc4b5600ad4cff2c56e0fe01d24ef8a67cc42af
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-04 10:00:05 +0530

    Time: 25 ms (9.57%) | Memory: 42.3 MB (92.99%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,18 +3,14 @@ class Solution {
         Map<Character, Character> sToTMapping = new HashMap<>();
         Map<Character, Character> tToSMapping = new HashMap<>();
 
-        for(int i = 0; i < s.length(); ++i) {
+        for (int i = 0; i < s.length(); ++i) {
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
 
-            if(sToTMapping.containsKey(sChar)) {
-                if(sToTMapping.get(sChar) != tChar) {
-                    return false;
-                }
-            } else if(tToSMapping.containsKey(tChar)) {
-                if(tToSMapping.get(tChar) != sChar) {
-                    return false;
-                }
+            if (sToTMapping.containsKey(sChar) && sToTMapping.get(sChar) != tChar) {
+                return false;
+            } else if (tToSMapping.containsKey(tChar) && tToSMapping.get(tChar) != sChar) {
+                return false;
             } else {
                 sToTMapping.put(sChar, tChar);
                 tToSMapping.put(tChar, sChar);
