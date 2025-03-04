7ce37990855df27e7f509b2d2df0d5ac7cd7be9d
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-04 11:34:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (36.16%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,33 +1,50 @@
 class Solution {
 
-    public void reverse(int start, int end, StringBuilder given) {
-        while (start < end) {
-
-            char temp = given.charAt(end);
-            given.setCharAt(end, given.charAt(start));
-            given.setCharAt(start, temp);
-
-            start++;
-            end--;
+    public boolean rotateString(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
         }
-    }
 
-    public boolean rotateString(String s, String goal) {
-        int n = s.length();
+        StringBuilder temp = new StringBuilder(s1);
 
-        if(s.equals(goal)) return true;
-
-        for(int i = 1; i < s.length(); ++i) {
-            StringBuilder given = new StringBuilder(s);
-            reverse(n - i, n - 1, given);
-            reverse(0, n - i - 1, given);
-            reverse(0, n - 1, given);
-
-            if(given.toString().equals(goal)) {
+        for (int i = 0; i < s1.length(); i++) {
+            temp.deleteCharAt(0);
+            temp.append(s1.charAt(i));
+            if (temp.toString().equals(s2)) {
                 return true;
             }
         }
-
         return false;
     }
+
+    // public void reverse(int start, int end, StringBuilder given) {
+    // while (start < end) {
+
+    // char temp = given.charAt(end);
+    // given.setCharAt(end, given.charAt(start));
+    // given.setCharAt(start, temp);
+
+    // start++;
+    // end--;
+    // }
+    // }
+
+    // public boolean rotateString(String s, String goal) {
+    // int n = s.length();
+
+    // if(s.equals(goal)) return true;
+
+    // for(int i = 1; i < s.length(); ++i) {
+    // StringBuilder given = new StringBuilder(s);
+    // reverse(n - i, n - 1, given);
+    // reverse(0, n - i - 1, given);
+    // reverse(0, n - 1, given);
+
+    // if(given.toString().equals(goal)) {
+    // return true;
+    // }
+    // }
+
+    // return false;
+    // }
 }
