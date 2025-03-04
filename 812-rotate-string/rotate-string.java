fd19091de3cb3366f653bad8795017d654dc4a01
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-04 12:01:02 +0530

    Time: 5 ms (8.93%) | Memory: 42.4 MB (5.39%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,23 +1,44 @@
 class Solution {
 
-    public boolean rotateString(String s1, String s2) {
-        if (s1.length() != s2.length()) {
+    public boolean rotateString(String s, String goal) {
+        int m = s.length();
+        int n = goal.length();
+
+        // If the lengths are not equal, they can't be rotations
+        if (m != n) {
             return false;
         }
 
-        if(s1.equals(s2)) return true;
-
-        StringBuilder temp = new StringBuilder(s1);
-
-        for (int i = 0; i < s1.length(); i++) {
-            temp.deleteCharAt(0);
-            temp.append(s1.charAt(i));
-            if (temp.toString().equals(s2)) {
+        // Check all possible rotations
+        for (int rotationCount = 1; rotationCount <= m; ++rotationCount) {
+            // Perform one rotation by shifting left by one character
+            s = s.substring(1) + s.charAt(0);
+            if (s.equals(goal)) {
                 return true;
             }
         }
         return false;
     }
+
+
+    // public boolean rotateString(String s1, String s2) {
+    //     if (s1.length() != s2.length()) {
+    //         return false;
+    //     }
+
+    //     if(s1.equals(s2)) return true;
+
+    //     StringBuilder temp = new StringBuilder(s1);
+
+    //     for (int i = 0; i < s1.length(); i++) {
+    //         temp.deleteCharAt(0);
+    //         temp.append(s1.charAt(i));
+    //         if (temp.toString().equals(s2)) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 
     // public void reverse(int start, int end, StringBuilder given) {
     // while (start < end) {
