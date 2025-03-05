54f576860f8bf62464c0e08316e8a107c2d60b9e
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-05 13:19:40 +0530

    Time: 7 ms (44.39%) | Memory: 41.1 MB (88.28%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,31 +1,61 @@
 class Solution {
     public String countAndSay(int n) {
-        if(n == 1) {
-            return "1";
-        }
 
-        // get the previous strings
-        String str = countAndSay(n - 1);
+        String result = "";
 
-        // process the str
-        int i = 0;
+        for (int j = 1; j <= n; ++j) {
+            if (j == 1) {
+                result += "1";
+            } else {
+                int i = 0;
 
-        StringBuilder sb = new StringBuilder();
+                StringBuilder sb = new StringBuilder();
 
-        while(i < str.length()) {
-            int count = 0;
-            char current = str.charAt(i);
+                while (i < result.length()) {
+                    int count = 0;
+                    char current = result.charAt(i);
 
-            while(i < str.length() && current == str.charAt(i)) {
-                count++;
-                i++;
+                    while (i < result.length() && current == result.charAt(i)) {
+                        count++;
+                        i++;
+                    }
+
+                    sb.append(count);
+                    sb.append(current);
+                }
+
+                result = sb.toString();
             }
-
-            sb.append(count);
-            sb.append(current);
         }
 
-        return sb.toString();
+        return result;
+
+        // if(n == 1) {
+        // return "1";
+        // }
+
+        // // get the previous strings
+        // String str = countAndSay(n - 1);
+
+        // // process the str
+        // int i = 0;
+
+        // StringBuilder sb = new StringBuilder();
+
+        // while(i < str.length()) {
+        // int count = 0;
+        // char current = str.charAt(i);
+
+        // while(i < str.length() && current == str.charAt(i)) {
+        // count++;
+        // i++;
+        // }
+
+        // sb.append(count);
+        // sb.append(current);
+        // }
+
+        // return sb.toString();
 
     }
 }
