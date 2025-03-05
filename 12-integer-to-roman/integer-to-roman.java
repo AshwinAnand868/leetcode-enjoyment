9fe4948ba91def3087b088d698c0e5fb1bd5faf2
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-05 12:26:10 +0530

    Time: 2 ms (100.00%) | Memory: 44.4 MB (71.86%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -3,20 +3,37 @@ class Solution {
     private static final String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     public String intToRoman(int num) {
-        return convert(num, 0);
-    }
 
-    private String convert(int num, int index) {
-        // Base case: If num is 0, return an empty string
-        if (num == 0) return "";
+        StringBuilder resultBuilder = new StringBuilder();
+        int i = 0;
 
-        // Get how many times the current Roman numeral fits in num
-        int quotient = num / values[index];  
+        while(num > 0) {
+            while(num >= values[i]) {
+                num = num - values[i];
+                resultBuilder.append(symbols[i]);
+            }
+            i++; // increment i when current num is less than val at i
+        }
+
+        return resultBuilder.toString();
+    
+
+    // Using recursion
+    // public String intToRoman(int num) {
+    //     return convert(num, 0);
+    // }
+
+    // private String convert(int num, int index) {
+    //     // Base case: If num is 0, return an empty string
+    //     if (num == 0) return "";
+
+    //     // Get how many times the current Roman numeral fits in num
+    //     int quotient = num / values[index];  
         
-        // Get the remainder for next recursive call
-        int remainder = num % values[index];  
+    //     // Get the remainder for next recursive call
+    //     int remainder = num % values[index];  
 
-        // Append the Roman numeral `quotient` times and recursively process the remainder
-        return symbols[index].repeat(quotient) + convert(remainder, index + 1);
+    //     // Append the Roman numeral `quotient` times and recursively process the remainder
+    //     return symbols[index].repeat(quotient) + convert(remainder, index + 1);
     }
 }
