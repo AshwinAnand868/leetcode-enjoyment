f141bed52df0af4468649f7b08520a21dfd90ec7
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-07 12:42:56 +0530

    Time: 57 ms (77.83%) | Memory: 43 MB (49.42%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,30 +1,44 @@
+
 class Solution {
-    public int getCurrentStringBeauty(String s) {
-        int[] counters = new int[26];
-        int maxFreq = Integer.MIN_VALUE;
-        int minFreq = Integer.MAX_VALUE;
+    // public int getCurrentStringBeauty(String s) {
+    // int[] counters = new int[26];
+    // int maxFreq = Integer.MIN_VALUE;
+    // int minFreq = Integer.MAX_VALUE;
 
-        for (int i = 0; i < s.length(); ++i) {
-            counters[s.charAt(i) - 'a']++;
-        }
+    // for (int i = 0; i < s.length(); ++i) {
+    // counters[s.charAt(i) - 'a']++;
+    // }
 
-        for (int i = 0; i < s.length(); ++i) {
-            minFreq = Math.min(minFreq, counters[s.charAt(i) - 'a']);
-        }
+    // for (int i = 0; i < s.length(); ++i) {
+    // minFreq = Math.min(minFreq, counters[s.charAt(i) - 'a']);
+    // }
 
-        for (int i = 0; i < s.length(); ++i) {
-            maxFreq = Math.max(maxFreq, counters[s.charAt(i) - 'a']);
-        }
+    // for (int i = 0; i < s.length(); ++i) {
+    // maxFreq = Math.max(maxFreq, counters[s.charAt(i) - 'a']);
+    // }
 
-        return maxFreq - minFreq;
-    }
+    // return maxFreq - minFreq;
+    // }
 
     public int beautySum(String s) {
         int beautySum = 0;
 
         for (int i = 0; i < s.length(); ++i) {
+            int[] freqs = new int[26];
             for (int j = i; j < s.length(); ++j) {
-                beautySum += getCurrentStringBeauty(s.substring(i, j + 1));
+                freqs[s.charAt(j) - 'a']++;
+                int maxFreq = Integer.MIN_VALUE;
+                int minFreq = Integer.MAX_VALUE;
+
+                for(int k = 0; k < 26; ++k) {
+                    if(freqs[k] > 0) {
+                        minFreq = Math.min(minFreq, freqs[k]);
+                        maxFreq = Math.max(maxFreq, freqs[k]);
+                    }
+                }
+                beautySum += (maxFreq - minFreq);
+
+                // beautySum += getCurrentStringBeauty(s.substring(i, j + 1));
             }
         }
 
