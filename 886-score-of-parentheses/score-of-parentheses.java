cd7dc6cea640eda41fb4bf224b8c0d17654566c1
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-08 12:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (26.80%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.java
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -1,19 +1,44 @@
 class Solution {
-    public int scoreOfParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int n = s.length();
+    public int scoreOfParentheses(String s) {  
+        
+        // Using depth logic
+        int depth = 0;
         int score = 0;
         char[] chars = s.toCharArray();
+        char prev = '(';
 
         for(char c : chars) {
             if(c == '(') {
-                stack.push(score); // push the current score onto the stack
-                score = 0; // reset score when new opening bracket encountered
+                depth++;
             } else {
-                score = stack.pop() + Math.max(2 * score, 1);
+                depth--;
+                if(prev == '(') {
+                    score += Math.pow(2, depth);
+                }
             }
+
+            prev = c;
         }
 
         return score;
     }
+    
+
+    //     Using Stack
+    //     Stack<Integer> stack = new Stack<>();
+    //     int n = s.length();
+    //     int score = 0;
+    //     char[] chars = s.toCharArray();
+
+    //     for(char c : chars) {
+    //         if(c == '(') {
+    //             stack.push(score); // push the current score onto the stack
+    //             score = 0; // reset score when new opening bracket encountered
+    //         } else {
+    //             score = stack.pop() + Math.max(2 * score, 1);
+    //         }
+    //     }
+
+    //     return score;
+    // }
 }
