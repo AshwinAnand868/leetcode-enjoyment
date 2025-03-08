ea39bb5ac8c49ea41965f1eaa6ed16e50ff8ad55
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-08 13:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (91.74%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,33 +1,33 @@
 class Solution {
     public int minAddToMakeValid(String s) {
 
-        int openingBracket = 0;
-        int minAddRequired = 0;
+        int minClosingBracketsReq = 0;
+        int minOpeningBracketsReq = 0;
 
-        for(int i = 0; i < s.length(); ++i) {
-            if(s.charAt(i) == '(') {
-                openingBracket++;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') {
+                minClosingBracketsReq++;
             } else {
-                if(openingBracket > 0) {
-                    openingBracket--;
+                if (minClosingBracketsReq > 0) {
+                    minClosingBracketsReq--;
                 } else {
-                    minAddRequired++;
+                    minOpeningBracketsReq++;
                 }
             }
         }
 
-        return minAddRequired + openingBracket;
+        return minClosingBracketsReq + minOpeningBracketsReq;
 
         // Using Stack
         // Stack<Character> stack = new Stack<>();
         // for(int i = 0; i < s.length(); ++i) {
-        //     char curr = s.charAt(i);
+        // char curr = s.charAt(i);
 
-        //     if(!stack.isEmpty() && stack.peek() == '(' && curr == ')') {
-        //         stack.pop(); // remov the pair
-        //     } else {
-        //         stack.push(curr);
-        //     }
+        // if(!stack.isEmpty() && stack.peek() == '(' && curr == ')') {
+        // stack.pop(); // remov the pair
+        // } else {
+        // stack.push(curr);
+        // }
         // }
 
         // return stack.size();
