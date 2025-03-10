512aea4a2f13348de36bf14d770c72c604892e50
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-10 11:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (75.84%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -1,43 +1,67 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
 
-        if (s.length() < 2)
-            return s;
+        int left = 0;
+        int right = s.length() - 1;
+        char[] chars = s.toCharArray();
 
-        StringBuilder sb = new StringBuilder();
-        HashMap<Integer, Character> map = new HashMap<>();
+        while(left < right) {
+            while(left < right && !Character.isLetter(s.charAt(left))) {
+                left++;
+            }
 
-        for (int i = 0; i < s.length(); ++i) {
-            char ch = s.charAt(i);
+            while(left < right && !Character.isLetter(s.charAt(right))) {
+                right--;
+            }
 
-            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-                sb.append(ch);
-            } else {
-                map.put(i, ch);
+            if(left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
             }
         }
 
-        if (map.size() == s.length())
-            return s;
+        return new String(chars);
 
-        sb.reverse();
+        // if (s.length() < 2)
+        //     return s;
 
-        StringBuilder res = new StringBuilder();
-        int i = 0;
+        // StringBuilder sb = new StringBuilder();
+        // HashMap<Integer, Character> map = new HashMap<>();
 
-        while (res.length() <= s.length()) {
-            if (map.containsKey(res.length())) {
-                int len = res.length();
-                res.append(map.get(len));
-                map.remove(len);
-            } else {
-                if (i >= sb.length())
-                    break;
-                res.append(sb.charAt(i));
-                i++;
-            }
-        }
+        // for (int i = 0; i < s.length(); ++i) {
+        //     char ch = s.charAt(i);
 
-        return res.toString();
+        //     if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        //         sb.append(ch);
+        //     } else {
+        //         map.put(i, ch);
+        //     }
+        // }
+
+        // if (map.size() == s.length())
+        //     return s;
+
+        // sb.reverse();
+
+        // StringBuilder res = new StringBuilder();
+        // int i = 0;
+
+        // while (res.length() <= s.length()) {
+        //     if (map.containsKey(res.length())) {
+        //         int len = res.length();
+        //         res.append(map.get(len));
+        //         map.remove(len);
+        //     } else {
+        //         if (i >= sb.length())
+        //             break;
+        //         res.append(sb.charAt(i));
+        //         i++;
+        //     }
+        // }
+
+        // return res.toString();
     }
 }
