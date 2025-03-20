1b6675af13464f392258d3e891d36f6f5b5c9ac0
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-20 09:56:14 +0530

    Time: 2 ms (97.68%) | Memory: 41.8 MB (65.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,31 +1,36 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+        boolean isValid = true;
 
-        if(s.length() == 1) return false;
+        if (s.length() == 1)
+            return false;
 
-        for(int i = 0; i < s.length(); ++i) {
+        for (int i = 0; i < s.length(); ++i) {
             char curr = s.charAt(i);
 
-            if(curr == '(' || curr == '{' || curr == '[') {
+            if (curr == '(' || curr == '{' || curr == '[') {
                 stack.push(curr);
-            } else if(!stack.isEmpty()) {
-                if(curr == ')' && stack.peek() == '(') {
+            } else if (!stack.isEmpty()) {
+                if (curr == ')' && stack.peek() == '(') {
                     stack.pop();
-                } else if(curr == '}' && stack.peek() == '{') {
+                } else if (curr == '}' && stack.peek() == '{') {
                     stack.pop();
-                } else if(curr == ']' && stack.peek() == '[') {
+                } else if (curr == ']' && stack.peek() == '[') {
                     stack.pop();
                 } else {
-                    return false;
+                    isValid = false;
+                    break;
                 }
             } else {
-                return false;
+                isValid = false;
+                break;
             }
         }
 
-        if(stack.size() > 0) return false;
+        if (stack.size() > 0)
+            isValid = false;
 
-        return true;
+        return isValid;
     }
 }
