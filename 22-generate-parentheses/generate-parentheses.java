01799b319901834558fb4548d7ff5f3b53a9f26e
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-20 12:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (94.26%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void recursivelyGenerateParenthesis(List<String> result, int open, int close, StringBuilder temp, int n) {
+        if (n == (temp.length() / 2)) {
+            result.add(temp.toString());
+            return;
+        }
+
+        if (open < n) {
+            temp.append("(");
+            recursivelyGenerateParenthesis(result, open + 1, close, temp, n);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+
+        if (close < open) {
+            temp.append(")");
+            recursivelyGenerateParenthesis(result, open, close + 1, temp, n);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+    }
+
+    public List<String> generateParenthesis(int n) {
+        if (n == 1) {
+            List<String> res = new ArrayList<>();
+            res.add("()");
+            return res;
+        }
+
+        List<String> result = new ArrayList<>();
+
+        recursivelyGenerateParenthesis(result, 0, 0, new StringBuilder(), n);
+
+        return result;
+    }
+}
