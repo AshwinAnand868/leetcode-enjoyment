330d7c1a8241dc5a005cd350a3aa614b76022f55
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-25 10:38:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (65.69%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,21 +1,32 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        HashMap<Integer, Integer> diagonalsMap = new HashMap<>();
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                if(diagonalsMap.containsKey(i - j)) {
-                    if(diagonalsMap.get(i - j) != matrix[i][j]) {
-                        return false;
-                    }
-                } else {
-                    diagonalsMap.put(i - j, matrix[i][j]);
+        final int rows = matrix.length;
+        final int cols = matrix[0].length;
+        for (int r = 1; r < rows; r++) {
+            for (int c = 1; c < cols; c++) {
+                if (matrix[r][c] != matrix[r - 1][c - 1]) {
+                    return false;
                 }
             }
         }
 
         return true;
+        // HashMap<Integer, Integer> diagonalsMap = new HashMap<>();
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+
+        // for(int i = 0; i < m; ++i) {
+        //     for(int j = 0; j < n; ++j) {
+        //         if(diagonalsMap.containsKey(i - j)) {
+        //             if(diagonalsMap.get(i - j) != matrix[i][j]) {
+        //                 return false;
+        //             }
+        //         } else {
+        //             diagonalsMap.put(i - j, matrix[i][j]);
+        //         }
+        //     }
+        // }
+
+        // return true;
     }
 }
