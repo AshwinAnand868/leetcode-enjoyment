30b6637b57dd187d791e9e825a7114c51fa94cbc
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-26 09:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (39.00%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,23 +8,61 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
 class Solution {
+
+    public ListNode recurse(ListNode head, ListNode prev) {
+        if(head.next == null) {
+            // head.next = prev;
+            // return prev;
+            return head;
+        }
+
+        ListNode newHead = recurse(head.next, head);
+
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        // head.next = temp;
+        // newPrev.next = prev;
+        return newHead;  
+    }
+
     public ListNode reverseList(ListNode head) {
-        Stack<Integer> reversed = new Stack<>();
+        if(head == null || head.next == null) {
+            return head;
+        }
+
         ListNode temp = head;
+        ListNode prev = null;
 
-        while(temp != null) {
-            reversed.push(temp.val);
-            temp = temp.next;
-        }
+        return recurse(head, prev);
 
-        temp = head;
-        while(!reversed.isEmpty()) {
-            int val = reversed.pop();
-            temp.val = val;
-            temp = temp.next;
-        }
+        // while(temp != null) {
+        //     ListNode front = temp.next;
+        //     temp.next = prev;
+        //     prev = temp;
+        //     temp = front;
+        // }
 
-        return head;
+        // return prev;
+
+        // Stack<Integer> reversed = new Stack<>();
+        // ListNode temp = head;
+
+        // while(temp != null) {
+        //     reversed.push(temp.val);
+        //     temp = temp.next;
+        // }
+
+        // temp = head;
+        // while(!reversed.isEmpty()) {
+        //     int val = reversed.pop();
+        //     temp.val = val;
+        //     temp = temp.next;
+        // }
+
+        // return head;
     }
 }
