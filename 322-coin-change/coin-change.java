aed1424be2fce774ec2b35646b1ced4e5fde6e95
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-27 16:50:13 +0530

    Time: 35 ms (17.12%) | Memory: 45.9 MB (9.68%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    private int minCoins = Integer.MAX_VALUE;
+
+    public int backtrack(int[] coins, int amount, int index, int[][] dp) {
+        if(amount == 0) { 
+            return 0;
+        }
+
+        if(amount < 0 || index >= coins.length) return Integer.MAX_VALUE;
+
+        if(dp[index][amount] != -1) return dp[index][amount];
+
+        int take = backtrack(coins, amount - coins[index], index, dp);
+        if(take != Integer.MAX_VALUE) {
+            take += 1;
+        }
+
+        int skip = backtrack(coins, amount, index + 1, dp);
+
+        dp[index][amount] = Math.min(take, skip);
+        return dp[index][amount];
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        // Arrays.sort(coins);
+        
+        // backtrack(coins, amount, 0, 0);
+        // return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+
+        int[][] dp = new int[coins.length][amount + 1]; // Create memoization table
+        for (int[] row : dp) Arrays.fill(row, -1); // Initialize with -1 (uncomputed)
+
+        int result = backtrack(coins, amount, 0, dp);
+        return result == Integer.MAX_VALUE ? -1 : result;
+
+    }
+}
