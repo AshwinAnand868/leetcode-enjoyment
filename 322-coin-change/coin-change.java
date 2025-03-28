8ecde91a28436301b927c77313569db1fbe555db
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-28 12:51:19 +0530

    Time: 16 ms (58.73%) | Memory: 44 MB (97.27%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -28,11 +28,31 @@ class Solution {
         // backtrack(coins, amount, 0, 0);
         // return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
 
-        int[][] dp = new int[coins.length][amount + 1]; // Create memoization table
-        for (int[] row : dp) Arrays.fill(row, -1); // Initialize with -1 (uncomputed)
+        // int[][] dp = new int[coins.length][amount + 1]; // Create memoization table
+        // for (int[] row : dp) Arrays.fill(row, -1); // Initialize with -1 (uncomputed)
 
-        int result = backtrack(coins, amount, 0, dp);
-        return result == Integer.MAX_VALUE ? -1 : result;
+        // int result = backtrack(coins, amount, 0, dp);
+        // return result == Integer.MAX_VALUE ? -1 : result;
+
+        if(amount < 1) return 0;
+
+        int[] minCoinsAtI = new int[amount + 1];
+
+        for(int varyingAmount = 1; varyingAmount <= amount; ++varyingAmount) {
+            minCoinsAtI[varyingAmount] = Integer.MAX_VALUE;
+
+            for(int coin: coins) {
+                if(coin <= varyingAmount && minCoinsAtI[varyingAmount - coin] != Integer.MAX_VALUE) {
+                    minCoinsAtI[varyingAmount] = Math.min(minCoinsAtI[varyingAmount], 1 + minCoinsAtI[varyingAmount - coin]);
+                }
+            }
+        }
+
+        if(minCoinsAtI[amount] != Integer.MAX_VALUE) {
+            return minCoinsAtI[amount];
+        }
+
+        return -1;
 
     }
 }
