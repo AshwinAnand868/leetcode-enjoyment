d97dd615cc24d3f274ca9c04fae4d2701c067e6d
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-29 09:22:46 +0530

    Time: 52 ms (30.02%) | Memory: 44.8 MB (24.41%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -26,11 +26,32 @@ class Solution {
     }
 
     public int lengthOfLIS(int[] nums) {
+
+        // track the LIS till each index i
+        int[] dp = new int[nums.length];
+
+        // initially each LIS is of length 1
+        Arrays.fill(dp, 1);
+
+        int maxLength = 1; // initial max length is 1 only
         int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
+
+        for(int i = 0; i < n; ++i) { // loop through each element of nums
+            for(int j = 0; j < i; ++j) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    maxLength = Math.max(dp[i], maxLength);
+                }
+            }
         }
-        return backtrack(nums, 0, -1, dp);
+
+        return maxLength;
+
+        // int n = nums.length;
+        // int[][] dp = new int[n + 1][n + 1];
+        // for(int[] row : dp) {
+        //     Arrays.fill(row, -1);
+        // }
+        // return backtrack(nums, 0, -1, dp);
     }
 }
