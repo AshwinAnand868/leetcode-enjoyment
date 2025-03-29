835e5d242a6dcbed8fda9185fb1780342e71f7df
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-29 17:36:51 +0530

    Time: 1 ms (99.90%) | Memory: 48.8 MB (35.43%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,23 +11,34 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode tempHeadA = headA;
-        ListNode tempHeadB = headB;
-        HashMap<ListNode, Integer> map = new HashMap<>();
 
-        while(tempHeadA != null) {
-            map.put(tempHeadA, 1);
-            tempHeadA = tempHeadA.next;
+        ListNode d1 = headA;
+        ListNode d2 = headB;
+
+        while(d1 != d2) {
+            d1 = d1 == null ? headB : d1.next;
+            d2 = d2 == null ? headA : d2.next;
         }
 
-        while(tempHeadB != null) {
+        return d1;
 
-            if(map.containsKey(tempHeadB)) {
-                return tempHeadB;
-            }
-            tempHeadB = tempHeadB.next;
-        }
+        // ListNode tempHeadA = headA;
+        // ListNode tempHeadB = headB;
+        // HashMap<ListNode, Integer> map = new HashMap<>();
 
-        return null;
+        // while(tempHeadA != null) {
+        //     map.put(tempHeadA, 1);
+        //     tempHeadA = tempHeadA.next;
+        // }
+
+        // while(tempHeadB != null) {
+
+        //     if(map.containsKey(tempHeadB)) {
+        //         return tempHeadB;
+        //     }
+        //     tempHeadB = tempHeadB.next;
+        // }
+
+        // return null;
     }
 }
