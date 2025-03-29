bd343633a9d45582b5364429d625919a191d84e9
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-03-29 14:36:26 +0530

    Time: 9 ms (94.73%) | Memory: 57.2 MB (30.13%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,38 +1,49 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+
+    public ListNode mergeTwoSortedLists(ListNode list1, ListNode list2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        temp.next = (list1 != null) ? list1 : list2;
+
+        return dummyNode.next;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode slow = head, fast = head, prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (prev != null) prev.next = null; // Properly split the list
+
+        return slow;
+    }
+
     public ListNode sortList(ListNode head) {
-        // edge cases first
-        if(head == null || head.next == null) {
-            return head;
-        }
+        if (head == null || head.next == null) return head;
 
-        ArrayList<Integer> tempList = new ArrayList<>();
-        ListNode temp = head;
+        ListNode middle = findMiddle(head);
+        ListNode right = middle;
+        ListNode left = head;
 
-        while(temp != null) {
-            tempList.add(temp.val);
-            temp = temp.next;
-        }
+        left = sortList(left);
+        right = sortList(right);
 
-        Collections.sort(tempList);
-
-        temp = head;
-        int i = 0;
-        while(temp != null) {
-            temp.val = tempList.get(i);
-            temp = temp.next;
-            i++;
-        }
-
-        return head;
+        return mergeTwoSortedLists(left, right);
     }
 }
