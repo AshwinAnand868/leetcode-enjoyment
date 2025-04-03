b06acd3f81f89213162016ec4bde47a6b81f2d41
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-03 11:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (94.92%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,41 +1,109 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // find the total count of nodes
-        ListNode current = head;
+
+        if(head == null) {
+            return null;
+        }
+
         int totalNodes = 0;
+        ListNode temp = head;
 
-        while(current != null) {
+        while(temp != null) {
             totalNodes++;
-            current = current.next;
+            temp = temp.next;
         }
 
-        if(n == totalNodes) {
-            head = head.next;
-            return head;
+        int nodeBeforeTargetPos = totalNodes - n;
+
+        // if the nth node is head
+        if(nodeBeforeTargetPos == 0) {
+            return head.next;
         }
 
-        int nodePosBeforeTarget = totalNodes - n;
-        ListNode prev = null;
+        ListNode nodeBeforeTarget = head;
 
-        current = head;
-        while(nodePosBeforeTarget != 0) {
-            prev = current;
-            current = current.next;
-            nodePosBeforeTarget--;
+        while(nodeBeforeTargetPos-- > 1) {
+            nodeBeforeTarget = nodeBeforeTarget.next;
         }
 
-        prev.next = current.next;
+        if(nodeBeforeTarget != null && nodeBeforeTarget.next != null) {
+            nodeBeforeTarget.next = nodeBeforeTarget.next.next;
+        }
 
         return head;
+
+
+
+        // if (head == null)
+        //     return null;
+
+        // // Step 1: Count total number of nodes
+        // int totalNodes = 0;
+        // Node temp = head;
+        // while (temp != null) {
+        //     totalNodes++;
+        //     temp = temp.next;
+        // }
+
+        // // Step 2: Find the position to remove
+        // int targetIndex = totalNodes - n; // This is already the node before the target
+
+        // // If we need to remove the head node
+        // if (targetIndex == 0) {
+        //     return head.next;
+        // }
+
+        // // Step 3: Traverse to the node before the target
+        // Node current = head;
+        // int count = 0;
+        // while (count < targetIndex - 1) {
+        //     current = current.next;
+        //     count++;
+        // }
+
+        // // Step 4: Remove the target node
+        // if (current != null && current.next != null) {
+        //     current.next = current.next.next;
+        // }
+
+        // return head;
+
+        // // find the total count of nodes
+        // ListNode current = head;
+        // int totalNodes = 0;
+
+        // while(current != null) {
+        // totalNodes++;
+        // current = current.next;
+        // }
+
+        // if(n == totalNodes) {
+        // head = head.next;
+        // return head;
+        // }
+
+        // int nodePosBeforeTarget = totalNodes - n;
+        // ListNode prev = null;
+
+        // current = head;
+        // while(nodePosBeforeTarget != 0) {
+        // prev = current;
+        // current = current.next;
+        // nodePosBeforeTarget--;
+        // }
+
+        // prev.next = current.next;
+
+        // return head;
     }
 }
