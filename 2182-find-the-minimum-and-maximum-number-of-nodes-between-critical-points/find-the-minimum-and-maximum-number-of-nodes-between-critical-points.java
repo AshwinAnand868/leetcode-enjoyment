5adefe8ef053ffa7d8747cd87d4f5753e0ae53ff
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-04 16:25:06 +0530

    Time: 4 ms (100.00%) | Memory: 60.5 MB (78.87%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -13,41 +13,52 @@ class Solution {
         if(head == null) return null;
 
         int[] result = {-1, -1};
-        HashMap<ListNode, Integer> criticalPointsMap = new HashMap<>();
 
         ListNode prev = head;
         ListNode curr = head.next;
         int position = 1;
-        ArrayList<Integer> positions = new ArrayList<>();
+        // ArrayList<Integer> positions = new ArrayList<>();
+        int first = -1;
+        int last = -1;
+        int prevCrit = 0; // 1 based indexing
+        int minDistance = Integer.MAX_VALUE;
 
         while(curr != null && curr.next != null) {
             position++;
-            if(curr.val < prev.val && curr.val < curr.next.val) {
-                // criticalPointsMap.put(curr, position);
-                positions.add(position);
-            } else if(curr.val > prev.val && curr.val > curr.next.val) {
-                // criticalPointsMap.put(curr, position);
-                positions.add(position);
+            if((curr.val < prev.val && curr.val < curr.next.val) || (curr.val > prev.val && curr.val > curr.next.val)) {
+                if(first == -1) {
+                    first = position;
+                } else {
+                    minDistance = Math.min(minDistance, position - prevCrit);
+                    last = position;
+                }
+                prevCrit = position;
             }
 
             prev = curr;
             curr = curr.next;
         }
 
-        if(positions.size() < 2) {
-            return result;
-        }
- 
-        Collections.sort(positions);
-        int diff = Integer.MAX_VALUE;
-        for(int i = 0; i < positions.size() - 1; ++i) {
-            int e1 = positions.get(i);
-            int e2 = positions.get(i + 1);
-            diff = Math.min(e2 - e1, diff);
+        if (first != -1 && last != -1) {
+            result[0] = minDistance;
+            result[1] = last - first;
         }
 
-        result[0] = diff;
-        result[1] = positions.get(positions.size() - 1) - positions.get(0);
         return result;
+
+        // if(positions.size() < 2) {
+        //     return result;
+        // }
+ 
+        // int diff = Integer.MAX_VALUE;
+        // for(int i = 0; i < positions.size() - 1; ++i) {
+        //     int e1 = positions.get(i);
+        //     int e2 = positions.get(i + 1);
+        //     diff = Math.min(e2 - e1, diff);
+        // }
+
+        // result[0] = diff;
+        // result[1] = positions.get(positions.size() - 1) - positions.get(0);
+        // return result;
     }
 }
