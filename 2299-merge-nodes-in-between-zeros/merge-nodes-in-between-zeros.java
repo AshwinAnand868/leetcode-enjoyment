6e86d9aaaae6b9b068535975c968f2e0522373db
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-04 11:22:31 +0530

    Time: 4 ms (100.00%) | Memory: 79.6 MB (94.37%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -11,26 +11,45 @@
 class Solution {
     public ListNode mergeNodes(ListNode head) {
 
-        if(head == null) return head;
+        ListNode p1 = head.next;
+        ListNode p2 = p1;
 
-        ListNode dummy = new ListNode(0);
-        ListNode newNode = dummy;
+        while(p2 != null) {
+            int sum = 0;
 
-        ListNode curr = head.next;
-        int val = 0;
-
-        while(curr.next != null) {
-            val += curr.val;
-            if(curr.val == 0) {
-                newNode.next = new ListNode(val);
-                newNode = newNode.next;
-                val = 0;
+            while(p2 != null && p2.val != 0) {
+                sum += p2.val;
+                p2 = p2.next;
             }
-            curr = curr.next;
+
+            p1.val = sum;
+            p2 = p2.next;
+            p1.next = p2;
+            p1 = p1.next;
         }
 
-        newNode.next = new ListNode(val);
+        return head.next;
 
-        return dummy.next;
+        // if(head == null) return head;
+
+        // ListNode dummy = new ListNode(0);
+        // ListNode newNode = dummy;
+
+        // ListNode curr = head.next;
+        // int val = 0;
+
+        // while(curr.next != null) {
+        //     val += curr.val;
+        //     if(curr.val == 0) {
+        //         newNode.next = new ListNode(val);
+        //         newNode = newNode.next;
+        //         val = 0;
+        //     }
+        //     curr = curr.next;
+        // }
+
+        // newNode.next = new ListNode(val);
+
+        // return dummy.next;
     }
 }
