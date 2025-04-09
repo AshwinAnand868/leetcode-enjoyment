7f7c4e7294b6fa622e76e8563be070041c7586f9
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-09 09:39:56 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (18.69%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,25 @@
 class Solution {
 
     public void backTrack(List<List<Integer>> result, List<Integer> temp, int index, int[] nums) {
-        if(index >= nums.length) {
-            result.add(new ArrayList<>(temp));
-            return;
+        result.add(new ArrayList<>(temp));
+
+        for(int i = index; i < nums.length; ++i) {
+            temp.add(nums[i]);
+            backTrack(result, temp, i + 1, nums);
+            temp.remove(temp.size() - 1);
         }
+        // if(index >= nums.length) {
+        //     result.add(new ArrayList<>(temp));
+        //     return;
+        // }
 
-        // take
-        temp.add(nums[index]);
-        backTrack(result, temp, index + 1, nums);
+        // // take
+        // temp.add(nums[index]);
+        // backTrack(result, temp, index + 1, nums);
 
-        // skip
-        temp.remove(temp.size() - 1);
-        backTrack(result, temp, index + 1, nums);
+        // // skip
+        // temp.remove(temp.size() - 1);
+        // backTrack(result, temp, index + 1, nums);
 
     }
 
