86d94deed5236f1b65fdfeefd541b5564d5417cf
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-16 10:59:24 +0530

    Time: 1 ms (93.58%) | Memory: 41.3 MB (86.23%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,49 +1,73 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
 
-        // Using binary search and a hashmap - Good for multiple queries
+        public boolean isSubsequence(String s, String t) {
 
-        HashMap<Character, List<Integer>> map = new HashMap<>();
+            if(s.length() == 0) return true;
 
-        // Preprocessing the string t
-        for(int i = 0; i < t.length(); ++i) {
-            map.computeIfAbsent(t.charAt(i), k -> new ArrayList<>()).add(i);
+        int i = 0;
+        int j = 0;
+
+        int sl = s.length();
+        int tl = t.length();
+
+        while(i < tl) {
+            if(j < sl && s.charAt(j) == t.charAt(i)) {
+                ++j;
+
+                if(j == sl) {
+                    return true;
+                }
+            }
+            ++i;
         }
 
-        int previousIndex = -1; // initially no previous value
-
-        for(char ch : s.toCharArray()) {
-            if(!map.containsKey(ch)) {
-                return false; // if map never contained that character present in s, then return false
-            }
-
-            List<Integer> indices = map.get(ch);
-
-            int index = Collections.binarySearch(indices, previousIndex + 1);
-            
-            if(index < 0) {
-                index = -index - 1; // find where the insertion point should be
-            }
-
-            if(index == indices.size()) {
-                return false;
-            }
-
-            previousIndex = indices.get(index);
-        }
-
-        return true;
-        // Good for one time query, but for many queries it is not efficient 
-        // as we need to scan through t each time
-        // int i = 0, j = 0;
-        
-        // while (i < s.length() && j < t.length()) {
-        //     if (s.charAt(i) == t.charAt(j)) {
-        //         i++;
-        //     }
-        //     j++;
-        // }
-        
-        // return i == s.length();
+        return false;
     }
+    // public boolean isSubsequence(String s, String t) {
+
+    //     // Using binary search and a hashmap - Good for multiple queries
+
+    //     HashMap<Character, List<Integer>> map = new HashMap<>();
+
+    //     // Preprocessing the string t
+    //     for(int i = 0; i < t.length(); ++i) {
+    //         map.computeIfAbsent(t.charAt(i), k -> new ArrayList<>()).add(i);
+    //     }
+
+    //     int previousIndex = -1; // initially no previous value
+
+    //     for(char ch : s.toCharArray()) {
+    //         if(!map.containsKey(ch)) {
+    //             return false; // if map never contained that character present in s, then return false
+    //         }
+
+    //         List<Integer> indices = map.get(ch);
+
+    //         int index = Collections.binarySearch(indices, previousIndex + 1);
+            
+    //         if(index < 0) {
+    //             index = -index - 1; // find where the insertion point should be
+    //         }
+
+    //         if(index == indices.size()) {
+    //             return false;
+    //         }
+
+    //         previousIndex = indices.get(index);
+    //     }
+
+    //     return true;
+    //     // Good for one time query, but for many queries it is not efficient 
+    //     // as we need to scan through t each time
+    //     // int i = 0, j = 0;
+        
+    //     // while (i < s.length() && j < t.length()) {
+    //     //     if (s.charAt(i) == t.charAt(j)) {
+    //     //         i++;
+    //     //     }
+    //     //     j++;
+    //     // }
+        
+    //     // return i == s.length();
+    // }
 }
