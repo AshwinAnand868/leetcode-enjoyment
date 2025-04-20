1f0db30f702f527c15668fafb8b7b28663557e08
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-20 14:33:21 +0530

    Time: 11 ms (4.99%) | Memory: 58.2 MB (52.08%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,35 +1,60 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int[] prefixSum = new int[n + 1];
 
-        if(nums.length == 1) {
-            if(nums[0]>= target) {
-                return 1;
-            } else {
-                return 0;
+        // build a prefix sum array
+        for(int i = 0; i < n; ++i) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        int minLen = Integer.MAX_VALUE;
+
+        // Binary search for each i to find j
+        for(int i = 0; i < n; ++i) {
+            int required = prefixSum[i] + target;
+
+            // searh space of prefix sum array is from 1 to 6 as its size is n + 1
+            int left = i + 1;
+            int right = n;
+
+            while(left <= right) {
+                int mid = (left + right) / 2;
+
+                if(prefixSum[mid] >= required) {
+                    minLen = Math.min(minLen, mid - i);
+                    // reduce the search space
+                    right = mid - 1; // move right to mid - 1 to find any other mid meeting the condition
+                } else {
+                    left = mid + 1;
+                }
             }
         }
 
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        int len = Integer.MAX_VALUE;
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
 
-        while(j < nums.length) {
-            sum += nums[j];
 
-            while(sum >= target) {
-                len = Math.min(len, j - i + 1);
-                sum = sum - nums[i];
-                i++;
-            }
+        // Sliding Window Approach (O(n))
+        // int i = 0;
+        // int j = 0;
+        // int sum = 0;
+        // int len = Integer.MAX_VALUE;
 
-            j++;
-        }
+        // while(j < nums.length) {
+        //     sum += nums[j];
 
-        if(len == Integer.MAX_VALUE) { // no valid subarray found
-            return 0;
-        } else {
-            return len;
-        }
+        //     while(sum >= target) {
+        //         len = Math.min(len, j - i + 1);
+        //         sum = sum - nums[i];
+        //         i++;
+        //     }
+
+        //     j++;
+        // }
+
+        // if(len == Integer.MAX_VALUE) { // no valid subarray found
+        //     return 0;
+        // } else {
+        //     return len;
+        // }
     }
 }
