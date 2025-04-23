fe429fb8aff4d538041cc9eb30a908c796712c0a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-23 12:19:56 +0530

    Time: 38 ms (17.28%) | Memory: 46.6 MB (6.02%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,74 +1,103 @@
 class Solution {
 
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        // Queue maintains the farthest element on the top
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> {
+            int diffA = Math.abs(a - x);
+            int diffB = Math.abs(b - x);
+
+            if(diffA != diffB) {
+                return diffB - diffA; // Max heap
+            } else {
+                return b - a; // if difference is same, then the larger value goes on the top
+            }
+        });
+
+        for(int num : arr) {
+            maxHeap.offer(num);
+
+            if(maxHeap.size() > k) {
+                maxHeap.poll(); // if more than k elems in the heap, then the remove the top most elem
+            }
+        }
+
+        List<Integer> result = new ArrayList<>(maxHeap);
+        Collections.sort(result); // final result must be sorted
+        return result;
+
+    }
+
+
+
     // find lower bound
     // it returns the index of target if it exists, otherwise
     // it returns the position where it should be in the array
-    private int binarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-        int ans = arr.length; // default if all elements are less than target
+    // private int binarySearch(int[] arr, int target) {
+    //     int left = 0;
+    //     int right = arr.length - 1;
+    //     int ans = arr.length; // default if all elements are less than target
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    //     while (left <= right) {
+    //         int mid = left + (right - left) / 2;
 
-            if (arr[mid] >= target) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
+    //         if (arr[mid] >= target) {
+    //             ans = mid;
+    //             right = mid - 1;
+    //         } else {
+    //             left = mid + 1;
+    //         }
+    //     }
 
-        return ans;
-    }
+    //     return ans;
+    // }
 
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int xPotentialIdx = binarySearch(arr, x);
+    // public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    //     int xPotentialIdx = binarySearch(arr, x);
 
-        int ptr1 = xPotentialIdx - 1;
-        int ptr2 = xPotentialIdx;
-        int chosenElem = 0;
-        int n = arr.length;
+    //     int ptr1 = xPotentialIdx - 1;
+    //     int ptr2 = xPotentialIdx;
+    //     int chosenElem = 0;
+    //     int n = arr.length;
 
-        List<Integer> result = new ArrayList<>();
+    //     List<Integer> result = new ArrayList<>();
 
-        while (ptr1 >= 0 && ptr2 < n && chosenElem < k) {
-            int leftDiff = Math.abs(arr[ptr1] - x);
-            int rightDiff = Math.abs(arr[ptr2] - x);
+    //     while (ptr1 >= 0 && ptr2 < n && chosenElem < k) {
+    //         int leftDiff = Math.abs(arr[ptr1] - x);
+    //         int rightDiff = Math.abs(arr[ptr2] - x);
 
-            if (leftDiff < rightDiff) {
-                result.add(arr[ptr1]);
-                ptr1--;
-            } else if (leftDiff > rightDiff) {
-                result.add(arr[ptr2]);
-                ptr2++;
-            } else {
-                if (arr[ptr1] <= arr[ptr2]) {
-                    result.add(arr[ptr1]);
-                    ptr1--;
-                } else {
-                    result.add(arr[ptr2]);
-                    ptr2++;
-                }
-            }
+    //         if (leftDiff < rightDiff) {
+    //             result.add(arr[ptr1]);
+    //             ptr1--;
+    //         } else if (leftDiff > rightDiff) {
+    //             result.add(arr[ptr2]);
+    //             ptr2++;
+    //         } else {
+    //             if (arr[ptr1] <= arr[ptr2]) {
+    //                 result.add(arr[ptr1]);
+    //                 ptr1--;
+    //             } else {
+    //                 result.add(arr[ptr2]);
+    //                 ptr2++;
+    //             }
+    //         }
 
-            chosenElem++;
-        }
+    //         chosenElem++;
+    //     }
 
-        // Add remaining elements if needed
-        while (chosenElem < k && ptr1 >= 0) {
-            result.add(arr[ptr1]);
-            ptr1--;
-            chosenElem++;
-        }
+    //     // Add remaining elements if needed
+    //     while (chosenElem < k && ptr1 >= 0) {
+    //         result.add(arr[ptr1]);
+    //         ptr1--;
+    //         chosenElem++;
+    //     }
 
-        while (chosenElem < k && ptr2 < n) {
-            result.add(arr[ptr2]);
-            ptr2++;
-            chosenElem++;
-        }
+    //     while (chosenElem < k && ptr2 < n) {
+    //         result.add(arr[ptr2]);
+    //         ptr2++;
+    //         chosenElem++;
+    //     }
 
-        Collections.sort(result);
-        return result;
-    }
+    //     Collections.sort(result);
+    //     return result;
+    // }
 }
