34aec8da759d918f3c35fafd3fc17a78ee99758b
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-28 20:32:14 +0530

    Time: 7 ms (92.62%) | Memory: 55 MB (77.82%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,23 +1,55 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        // track the number of odd counts
-        Map<Integer, Integer> map = new HashMap<>();
+
         int n = nums.length;
         int oddCount = 0;
-        map.put(0, 1); // we have seen zeroes once initially
-        int i = 0, j = 0;
+        int previousCount = 0;
+
         int result = 0;
 
-        while(j < n) {
-            oddCount += (nums[j] % 2 == 0) ? 0 : 1;
+        int i = 0, j = 0;
 
-            if(map.containsKey(oddCount - k)) {
-                result += map.get(oddCount - k);
+        while(j < n) {
+            if(nums[j] % 2 != 0) {
+                oddCount++;
+                previousCount = 0;
             }
 
-            map.put(oddCount, map.getOrDefault(oddCount, 0) + 1);
-            j++;
+            while(oddCount == k) {
+                previousCount++;
+
+                if(i < n && nums[i] % 2 != 0) {
+                    oddCount--;
+                }
+
+                ++i;
+            }
+
+            result += previousCount;
+            ++j;
         }
+
+
+
+
+        // track the number of odd counts
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int n = nums.length;
+        // int oddCount = 0;
+        // map.put(0, 1); // we have seen zeroes once initially
+        // int i = 0, j = 0;
+        // int result = 0;
+
+        // while(j < n) {
+        //     oddCount += (nums[j] % 2 == 0) ? 0 : 1;
+
+        //     if(map.containsKey(oddCount - k)) {
+        //         result += map.get(oddCount - k);
+        //     }
+
+        //     map.put(oddCount, map.getOrDefault(oddCount, 0) + 1);
+        //     j++;
+        // }
 
         return result;
     }
