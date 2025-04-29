a19c3219eb2179469b175859c1d24f0388b396e0
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-29 09:55:55 +0530

    Time: 48 ms (65.65%) | Memory: 48 MB (24.90%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,31 +1,32 @@
 class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return getNGoodSubarrays(nums, k) - getNGoodSubarrays(nums, k - 1);
+    }
 
-    private int slidingWindow(int[] nums, int k) {
+    public int getNGoodSubarrays(int[] nums, int k) {
+
+        if(k < 0) return 0;
+
+        int i = 0, j = 0, n = nums.length;
+        int count = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        int i = 0, j = 0, count = 0;
-        int n = nums.length;
-        
         while(j < n) {
             map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
 
             while(map.size() > k) {
                 map.put(nums[i], map.get(nums[i]) - 1);
-
-                if(map.get(nums[i]) == 0) {
+                if(map.get(nums[i]) <= 0) {
                     map.remove(nums[i]);
                 }
                 i++;
             }
+            
 
-            // formula to get number of subarray from i to j is j - i + 1
-            count += j - i + 1;
+            count += (j - i + 1);
             j++;
         }
-        return count;
-    }
 
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        return slidingWindow(nums, k) - slidingWindow(nums, k - 1);
+        return count;
     }
 }
