0d5738558e7034c27660ab806cd4dff68691af8a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-04-30 16:41:42 +0530

    Time: 132 ms (16.28%) | Memory: 59.5 MB (32.71%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,31 +1,53 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        PriorityQueue<int[]> maxPq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
-        PriorityQueue<int[]> minPq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
 
         int i = 0, j = 0, maxLength = 0, n = nums.length;
 
+        TreeMap<Integer,Integer> treeMap = new TreeMap<>();
+
         while(j < n) {
-            maxPq.offer(new int[] {nums[j], j});
-            minPq.offer(new int[] {nums[j], j});
+            treeMap.put(nums[j], treeMap.getOrDefault(nums[j], 0) + 1);
 
-            while(maxPq.peek()[0] - minPq.peek()[0] > limit) {
-                i = Math.min(maxPq.peek()[1], minPq.peek()[1]) + 1;
-
-                while(maxPq.peek()[1] < i) {
-                    maxPq.poll();
+            while(treeMap.lastKey() - treeMap.firstKey() > limit) {
+                treeMap.put(nums[i], treeMap.get(nums[i]) - 1);
+                if(treeMap.get(nums[i]) == 0) {
+                    treeMap.remove(nums[i]);
                 }
-
-                while (minPq.peek()[1] < i) {
-                    minPq.poll();
-                }
-
-
+                i++;
             }
+
             maxLength = Math.max(maxLength, j - i + 1);
             j++;
         }
 
         return maxLength;
+
+        // PriorityQueue<int[]> maxPq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        // PriorityQueue<int[]> minPq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+
+        // int i = 0, j = 0, maxLength = 0, n = nums.length;
+
+        // while(j < n) {
+        //     maxPq.offer(new int[] {nums[j], j});
+        //     minPq.offer(new int[] {nums[j], j});
+
+        //     while(maxPq.peek()[0] - minPq.peek()[0] > limit) {
+        //         i = Math.min(maxPq.peek()[1], minPq.peek()[1]) + 1;
+
+        //         while(maxPq.peek()[1] < i) {
+        //             maxPq.poll();
+        //         }
+
+        //         while (minPq.peek()[1] < i) {
+        //             minPq.poll();
+        //         }
+
+
+        //     }
+        //     maxLength = Math.max(maxLength, j - i + 1);
+        //     j++;
+        // }
+
+        // return maxLength;
     }
 }
