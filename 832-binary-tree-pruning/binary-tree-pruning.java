59dc5b44659caf5f143d01f375d33e4333abcea4
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-06 15:04:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (86.51%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.java
+++ 832-binary-tree-pruning/binary-tree-pruning.java
@@ -26,16 +26,16 @@ class Solution {
     private TreeNode pruneTreeHelper(TreeNode root) {
         if(root == null) return null;
 
-        if(!isOnePresent(root.left)) {
-            root.left = null;
-        }
+        // if(!isOnePresent(root.left)) {
+        //     root.left = null;
+        // }
 
-        if(!isOnePresent(root.right)) {
-            root.right = null;
-        }
+        // if(!isOnePresent(root.right)) {
+        //     root.right = null;
+        // }
 
-        pruneTreeHelper(root.left);
-        pruneTreeHelper(root.right);
+        root.left = pruneTreeHelper(root.left);
+        root.right = pruneTreeHelper(root.right);
 
         if(root.left == null && root.right == null && root.val == 0) {
             return null;
