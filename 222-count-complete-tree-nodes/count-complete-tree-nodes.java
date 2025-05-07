03fcc676720f7e2c2f9f707aad0ac98243f692b2
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-07 16:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (10.66%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,15 +15,40 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        return dfs(root);
+        // return dfs(root);
+        if(root == null) return 0;
+
+        int leftHeight = getDepth(root.left, true);
+        int rightDepth = getDepth(root.right, false);
+
+        // if left and right depths are equal, we know it is a balanced tree
+        if(leftHeight == rightDepth) {
+            return (int) Math.pow(2, leftHeight + 1) - 1;
+        }
+
+        // otherwise count the nodes in the left and right side of the node
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
-    private int dfs(TreeNode root) {
-        if (root == null) return 0;
+    public int getDepth(TreeNode root, boolean isLeft) {
+        TreeNode temp = root;
+        int height = 0;
 
-        int lc = dfs(root.left);
-        int rc = dfs(root.right);
+        while (temp != null) {
+            temp = isLeft ? temp.left : temp.right;
+            height++;
+        }
 
-        return 1 + lc + rc;
+        return height;
     }
+
+
+    // private int dfs(TreeNode root) {
+        // if (root == null) return 0;
+
+        // int lc = dfs(root.left);
+        // int rc = dfs(root.right);
+
+        // return 1 + lc + rc;
+    // }
 }
