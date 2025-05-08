ec48fb24d628ea8b59e17a7fdd87261c41702014
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-08 22:28:11 +0530

    Time: 1 ms (99.69%) | Memory: 63.8 MB (10.63%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -13,24 +13,67 @@
  *     }
  * }
  */
+
+/**
+* Definition for a binary tree node.
+* public class TreeNode {
+*     int val;
+*     TreeNode left;
+*     TreeNode right;
+*     TreeNode() {}
+*     TreeNode(int val) { this.val = val; }
+*     TreeNode(int val, TreeNode left, TreeNode right) {
+*         this.val = val;
+*         this.left = left;
+*         this.right = right;
+*     }
+* }
+*/
 class Solution {
-    int minDepth = Integer.MAX_VALUE;
-
     public int minDepth(TreeNode root) {
-        if(root == null) return 0;
-        minDepth(root, 1);
-        return minDepth;
-    }
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root == null)
+            return 0;
+        q.offer(root);
+        int depth = 0;
+        while (!q.isEmpty()) {
+            int n = q.size();
+            depth++;
+            for (int i = 0; i < n; i++) {
+                TreeNode node = q.poll();
 
-    public void minDepth(TreeNode root, int depth) {
-        if(root == null) return;
+                // return depth as soon as we encouncter the shortest path
+                if (node.left == null && node.right == null)
+                    return depth;
 
-        if(root.right == null && root.left == null) {
-            minDepth = Math.min(minDepth, depth);
-            return;
+                if (node.left != null)
+                    q.add(node.left);
+                if (node.right != null)
+                    q.add(node.right);
+            }
         }
-
-        minDepth(root.left, depth + 1);
-        minDepth(root.right, depth + 1);
+        return depth;
     }
 }
+
+// class Solution {
+//     int minDepth = Integer.MAX_VALUE;
+
+//     public int minDepth(TreeNode root) {
+//         if(root == null) return 0;
+//         minDepth(root, 1);
+//         return minDepth;
+//     }
+
+//     public void minDepth(TreeNode root, int depth) {
+//         if(root == null) return;
+
+//         if(root.right == null && root.left == null) {
+//             minDepth = Math.min(minDepth, depth);
+//             return;
+//         }
+
+//         minDepth(root.left, depth + 1);
+//         minDepth(root.right, depth + 1);
+//     }
+// }
