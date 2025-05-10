448524d028728556b1d5ea50be6790b7db9d805b
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-10 09:42:17 +0530

    Time: 14 ms (8.71%) | Memory: 44.4 MB (45.26%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -18,24 +18,18 @@ class Solution {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
-        List<TreeNode> list = new ArrayList<>();
+        int count = 0;
 
         while(!queue.isEmpty()) {
             TreeNode curr = queue.poll();
-            list.add(curr);
-
-            if(curr.left != null) queue.add(curr.left);
-            if(curr.right != null) queue.add(curr.right);
-        }
-
-        int count = 0;
-        for(int i = 0; i < list.size(); ++i) {
-            TreeNode curr = list.get(i);
 
             int avg = getSubtreeAvg(curr);
             if (curr.val == avg) {
                 count++;
             }
+
+            if(curr.left != null) queue.add(curr.left);
+            if(curr.right != null) queue.add(curr.right);
         }
 
         return count;
@@ -59,21 +53,4 @@ class Solution {
         getSumSubtreeAndCountNodes(root.left, sum, count);
         getSumSubtreeAndCountNodes(root.right, sum, count);
     }
-
-    // private int countNodesEqualToAvg(TreeNode curr, int avg) {
-    //     int[] count = {0};
-    //     countNodesEqualToAvgHelper(curr, avg, count);
-    //     return count[0];
-    // }
-
-    // private void countNodesEqualToAvgHelper(TreeNode curr, int avg, int[] count) {
-    //     if(curr == null) return;
-
-    //     if(curr.val == avg) {
-    //         count[0]++;
-    //     }
-
-    //     countNodesEqualToAvgHelper(curr.left, avg, count);
-    //     countNodesEqualToAvgHelper(curr.right, avg, count);
-    // }
 }
