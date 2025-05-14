0b63f6e6e74d7f2ff541ad447ee29c0e2eb1cba4
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-14 12:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (64.16%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -19,36 +19,19 @@ class Solution {
         if(root == null) {
             return new ArrayList<>();
         }
-    
-        // Using stack
+
+        List<Integer> result = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
-        List<Integer> result = new ArrayList<>();
 
         while(!stack.isEmpty()) {
             TreeNode curr = stack.pop();
+
             result.add(curr.val);
 
             if(curr.right != null) stack.push(curr.right);
-            if(curr.left != null) stack.push(curr.left); // pushing left later to access left at the top
+            if(curr.left != null) stack.push(curr.left);
         }
-
         return result;
-
-        // Using recursion
-
-        // List<Integer> result = new ArrayList<>();
-        // preorderTraversalHelper(root, result);
-
-        // return result;
-    }
-
-    public void preorderTraversalHelper(TreeNode root, List<Integer> result) {
-
-        if(root == null) return;
-
-        result.add(root.val);
-        preorderTraversalHelper(root.left, result);
-        preorderTraversalHelper(root.right, result);
     }
 }
