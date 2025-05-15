b9bbea002483ba1b4674680901a3a477be633614
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-15 12:48:09 +0530

    Time: 1 ms (99.15%) | Memory: 43.2 MB (86.83%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -13,47 +13,53 @@
  *     }
  * }
  */
-class Pair {
-    TreeNode node;
-    int index;
 
+class Pair {
+    int index;
+    TreeNode node;
+
+    Pair() {}
     Pair(TreeNode node, int index) {
-        this.node = node;
         this.index = index;
+        this.node = node;
     }
 }
 
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0));
 
-        if(root == null) return 0;
-
-        Deque<Pair> deque = new LinkedList<>();
-
-        deque.offerLast(new Pair(root, 0));
         int maxWidth = 0;
 
-        while(!deque.isEmpty()) {
-            int size = deque.size();
-            int first = deque.peekFirst().index;
-            int last = deque.peekLast().index;
-            maxWidth = Math.max(maxWidth, last - first + 1);
-
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            int lastIndex = 0, firstIndex = 0;
+            // int minIndex = 0;
+            
             for(int i = 0; i < size; ++i) {
-                TreeNode curr = deque.peek().node;
-                int idx = deque.peek().index;
-                deque.poll();
+                Pair pair = queue.poll();
+                TreeNode curr = pair.node;
+
+                if(i == 0) {
+                    firstIndex = pair.index;
+                }
+
+                if(i == size - 1) {
+                    lastIndex = pair.index;
+                }
 
                 if(curr.left != null) {
-                    deque.offerLast(new Pair(curr.left, 2 * idx + 1));
+                    queue.offer(new Pair(curr.left, 2 * pair.index + 1));
                 }
                 if(curr.right != null) {
-                    deque.offerLast(new Pair(curr.right, 2 * idx + 2));
+                    queue.offer(new Pair(curr.right, 2 * pair.index + 2));
                 }
             }
 
-           
+            maxWidth = Math.max(maxWidth, lastIndex - firstIndex + 1);
         }
+
         return maxWidth;
     }
 }
