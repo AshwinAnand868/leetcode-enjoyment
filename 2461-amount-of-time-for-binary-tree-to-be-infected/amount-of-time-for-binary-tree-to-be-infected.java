ae8d13eaab9cfc11cddde4483caa3531384e89cc
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-15 15:54:29 +0530

    Time: 115 ms (34.30%) | Memory: 114.5 MB (16.99%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -14,85 +14,49 @@
  * }
  */
 class Solution {
-    HashMap<TreeNode, TreeNode> childToParent = new HashMap<>();
+
+    public void buildGraph(TreeNode node, TreeNode parent, Map<Integer, List<Integer>> graph) {
+        if(node == null) return;
+
+        graph.putIfAbsent(node.val, new ArrayList<>());
+
+        if(parent != null) {
+            graph.putIfAbsent(parent.val, new ArrayList<>());
+            graph.get(node.val).add(parent.val);
+            graph.get(parent.val).add(node.val);
+        }
+
+        buildGraph(node.left, node, graph);
+        buildGraph(node.right, node, graph);
+    }
 
     public int amountOfTime(TreeNode root, int start) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        buildGraph(root, null, graph);
 
-        if(root == null) return 0;
-        if(root.left == null && root.right == null) return 0;
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
 
-        populateChildToParentMap(root);
+        queue.offer(start);
+        visited.add(start);
 
-        TreeNode st = findStartNode(root, start);
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(st);
-
-        Set<TreeNode> infected = new HashSet<>();
-        infected.add(st);
-
-        int minutes = 0;
+        int minutes = -1;
 
         while(!queue.isEmpty()) {
             int size = queue.size();
+            minutes++;
 
             for(int i = 0; i < size; ++i) {
-                TreeNode curr = queue.poll();
-
-                if(curr.left != null && !infected.contains(curr.left)) {
-                    queue.offer(curr.left);
-                    infected.add(curr.left);
+                int curr = queue.poll();
+                for(int neighbour : graph.get(curr)) {
+                    if(!visited.contains(neighbour)) {
+                        visited.add(neighbour);
+                        queue.offer(neighbour);
+                    }
                 }
-
-                if(curr.right != null && !infected.contains(curr.right)) {
-                    queue.offer(curr.right);
-                    infected.add(curr.right);
-                }
-
-                if(childToParent.get(curr) != null && !infected.contains(childToParent.get(curr))) {
-                    queue.offer(childToParent.get(curr));
-                    infected.add(childToParent.get(curr));
-                }
-            }
-            if(!queue.isEmpty()) {
-                minutes++;
             }
         }
 
         return minutes;
-    }
-
-    private TreeNode findStartNode(TreeNode root, int start) {
-        if(root == null) return null;
-        if(root.val == start) {
-            return root;
-        }
-
-        // find in the left subtree
-        TreeNode leftResult  = findStartNode(root.left, start);
-        if(leftResult  != null) { // start node is this left one
-            return leftResult;
-        }
-
-        // Else search in the right subtree
-        return findStartNode(root.right, start);
-    }
-
-    private void populateChildToParentMap(TreeNode root) {
-        if(root == null) {
-            return;
-        }
-
-        if(root.left != null) {
-            childToParent.put(root.left, root);
-        }
-
-
-        if(root.right != null) {
-            childToParent.put(root.right, root);
-        }
-
-        populateChildToParentMap(root.left);
-        populateChildToParentMap(root.right);
     }
 }
