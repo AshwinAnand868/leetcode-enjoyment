f21a1633ed4c49de11ca24ce1c361d33048f5187
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-17 14:43:40 +0530

    Time: 23 ms (33.28%) | Memory: 47.6 MB (36.99%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
@@ -20,19 +20,40 @@ class FindElements {
 
     public FindElements(TreeNode root) {
         this.root = root;
-        recoverBinaryTree(this.root, 0);
+        recoverBinaryTree(this.root);
     }
 
-    private void recoverBinaryTree(TreeNode root, int val) {
-        if(root == null) return;
-
-        root.val = val;
+    private void recoverBinaryTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        root.val = 0;
         elements.add(root.val);
+        queue.offer(root);
 
-        recoverBinaryTree(root.left, root.val * 2 + 1);
-        recoverBinaryTree(root.right, root.val * 2 + 2);
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+
+            if (curr.left != null) {
+                curr.left.val = 2 * curr.val + 1;
+                elements.add(curr.left.val);
+                queue.offer(curr.left);
+            }
+
+            if (curr.right != null) {
+                curr.right.val = 2 * curr.val + 2;
+                elements.add(curr.right.val);
+                queue.offer(curr.right);
+            }
+        }
+
+        // if(root == null) return;
+
+        // root.val = val;
+        // elements.add(root.val);
+
+        // recoverBinaryTree(root.left, root.val * 2 + 1);
+        // recoverBinaryTree(root.right, root.val * 2 + 2);
     }
-    
+
     public boolean find(int target) {
         return elements.contains(target);
     }
