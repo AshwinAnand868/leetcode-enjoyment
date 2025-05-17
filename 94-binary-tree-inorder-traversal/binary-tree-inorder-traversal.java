075d8812ab7da185f16f34b1e4da3e3b337a20d8
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-17 17:20:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (8.03%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,23 +14,33 @@
  * }
  */
 class Solution {
-
+    // Using morris traversal logic without extra memory
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) return new ArrayList<>();
-
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
         List<Integer> result = new ArrayList<>();
+        TreeNode curr = root;
 
-        while(curr != null || !stack.isEmpty()) {
-            while(curr != null) {
-                stack.push(curr);
-                curr = curr.left;
+        while(curr != null) {
+            if(curr.left == null) {
+                // print root or curr, move to right side
+                result.add(curr.val);
+                curr = curr.right;
+            } else {
+                // go to the right most part of the left subtree
+                TreeNode prev = curr.left;
+
+                while(prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+
+                if(prev.right == null) { // no thread exists - create the thread
+                    prev.right = curr;
+                    curr = curr.left; // visit teh left side
+                } else {
+                    prev.right = null;
+                    result.add(curr.val);
+                    curr = curr.right; // visit the right side as left is done
+                }
             }
-
-            curr = stack.pop();
-            result.add(curr.val);
-            curr = curr.right;
         }
 
         return result;
