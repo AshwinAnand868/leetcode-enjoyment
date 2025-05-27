9e0c1f2d08da92ba176ec07fcfa905844dcbf205
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-27 12:30:44 +0530

    Time: 1 ms (99.86%) | Memory: 47.5 MB (70.03%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -2,32 +2,32 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] dp = new int[m][n];
 
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                if(i == 0 && j == 0) {
-                    dp[i][j] = grid[i][j];
-                } else {
-                    int up = grid[i][j];
-                    if(i > 0) {
-                        up += dp[i - 1][j];
-                    } else {
-                        up += (int) Math.pow(10, 9);
-                    }
-
-                    int left = grid[i][j];
-                    if(j > 0) {
-                        left += dp[i][j - 1];
-                    } else {
-                        left += (int) Math.pow(10, 9);
-                    }
-
-                    dp[i][j] = Math.min(up, left);
-                }
-            }
+        int[][] memo = new int[m][n];
+        for(int[] row : memo) {
+            Arrays.fill(row, -1);
         }
 
-        return dp[m - 1][n - 1];
+        return helper(0, 0, grid, m, n, memo);
+    }
+
+    public int helper(int i, int j, int[][] grid, int m, int n, int[][] memo) {
+        if(i == m - 1 && j == n - 1) {
+            return grid[i][j];
+        }
+
+        if(i >= m || j >= n) {
+            return Integer.MAX_VALUE;
+        }
+
+        if(memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        int current = grid[i][j];
+        int down = helper(i + 1, j, grid, m, n, memo);
+        int right = helper(i, j + 1, grid, m, n, memo);
+
+        return memo[i][j] = current + Math.min(down, right);
     }
 }
