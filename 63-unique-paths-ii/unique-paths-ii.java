7b5b01c4984b09ff3ecd53d394c2a6d592035710
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-27 11:14:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (81.40%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,31 +1,40 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
-        if (grid[0][0] == 1 || grid[m - 1][n - 1] == 1) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
 
-        int dp[][] = new int[m][n];
-
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                if(grid[i][j] == 1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-
-                if(i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-
-                int up = (i > 0) ? dp[i - 1][j] : 0;
-                int left = (j > 0) ? dp[i][j - 1] : 0;
-
-                dp[i][j] = up + left;
-            }
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
         }
 
-        return dp[m - 1][n - 1];
+        int[][] memo = new int[m][n];
+
+        for(int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+
+        return helper(obstacleGrid, 0, 0, m - 1, n - 1, memo);
+    }
+
+    public int helper(int[][] grid, int i, int j, int m, int n, int[][] memo) {
+
+        if(i > m || j > n) {
+            return 0;
+        }
+
+        if(i == m && j == n) {
+            return 1;
+        }
+
+        if(memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        if(grid[i][j] == 1) {
+            return memo[i][j] = 0; // 0 ways to reach this obstacle cell
+        }
+
+        return memo[i][j] = helper(grid, i + 1, j, m, n, memo) + helper(grid, i, j + 1, m, n, memo);
     }
 }
