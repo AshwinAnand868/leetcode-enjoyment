80d017b6e3b2dcd5c826e95cf5772096b3949438
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-27 11:31:48 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (29.31%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,40 +1,38 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
             return 0;
         }
 
-        int[][] memo = new int[m][n];
+        int[][] dp = new int[m][n];
 
-        for(int[] row : memo) {
-            Arrays.fill(row, -1);
+        // Initialize the first cell
+        dp[0][0] = 1;
+
+        // Fill the first column
+        for (int i = 1; i < m; ++i) {
+            dp[i][0] = (obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
         }
 
-        return helper(obstacleGrid, 0, 0, m - 1, n - 1, memo);
-    }
-
-    public int helper(int[][] grid, int i, int j, int m, int n, int[][] memo) {
-
-        if(i > m || j > n) {
-            return 0;
+        // Fill the first row
+        for (int j = 1; j < n; ++j) {
+            dp[0][j] = (obstacleGrid[0][j] == 0 && dp[0][j - 1] == 1) ? 1 : 0;
         }
 
-        if(i == m && j == n) {
-            return 1;
+        // Fill the rest of the grid
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
         }
 
-        if(memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        if(grid[i][j] == 1) {
-            return memo[i][j] = 0; // 0 ways to reach this obstacle cell
-        }
-
-        return memo[i][j] = helper(grid, i + 1, j, m, n, memo) + helper(grid, i, j + 1, m, n, memo);
+        return dp[m - 1][n - 1];
     }
 }
