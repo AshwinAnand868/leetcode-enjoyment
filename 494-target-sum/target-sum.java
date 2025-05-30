f5a252ade2c5e62b7ab3928800277d9264285896
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-30 17:21:08 +0530

    Time: 5 ms (86.29%) | Memory: 43.1 MB (56.04%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,36 +1,54 @@
 class Solution {
+    static final int MOD = (int) 1e9 + 7;
+
     public int findTargetSumWays(int[] nums, int target) {
         int totalSum = 0;
+        int n = nums.length;
 
-        for(int num : nums) {
-            totalSum += num;
+        for (int i = 0; i < n; ++i) {
+            totalSum += nums[i];
         }
 
-        int[][] memo = new int[nums.length][2 * totalSum + 1];
-
-        for(int[] row : memo) {
-            Arrays.fill(row, -1);
+        // Check for invalid conditions
+        if (totalSum < Math.abs(target) || (totalSum - target) % 2 != 0) {
+            return 0;
         }
 
-        return calcWays(nums, target, 0, 0, memo, totalSum);
+        int subsetSum = (totalSum - target) / 2;
+        return countSubsetSum(nums, subsetSum);
     }
 
-    public int calcWays(int[] nums, int target, int currentSum, int index, int[][] memo, int totalSum) {
-        if(index == nums.length) {
-            if(currentSum == target) {
-                return 1;
+    public int countSubsetSum(int[] nums, int target) {
+        int n = nums.length;
+        int[][] dp = new int[n][target + 1];
+
+        // Base case
+        // We can always make sum 0 — by taking nothing
+        for (int i = 0; i < n; ++i) {
+            dp[i][0] = 1;
+        }
+
+        // Special case handling for the first element
+        if (nums[0] <= target) {
+            if (nums[0] == 0) {
+                dp[0][0] = 2; // take or not take
             } else {
-                return 0;
+                dp[0][nums[0]] = 1;
             }
         }
 
-        if(memo[index][currentSum + totalSum] != -1) {
-            return memo[index][currentSum + totalSum];
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                int notPick = dp[i - 1][j];
+                int pick = 0;
+                if (nums[i] <= j) {
+                    pick = dp[i - 1][j - nums[i]];
+                }
+
+                dp[i][j] = (pick + notPick) % MOD;
+            }
         }
 
-        int plus = calcWays(nums, target, currentSum + nums[index], index + 1, memo, totalSum);
-        int minus = calcWays(nums, target, currentSum - nums[index], index + 1, memo, totalSum);
-
-        return memo[index][currentSum + totalSum] = plus + minus;
+        return dp[n - 1][target];
     }
 }
