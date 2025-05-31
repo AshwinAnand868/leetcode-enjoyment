cd37c5de6d80090dca4381f9e17c5352a01c68fd
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-05-31 14:05:59 +0530

    Time: 15 ms (78.54%) | Memory: 45 MB (33.58%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,11 +1,10 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-
         int[][] dp = new int[n][amount + 1];
 
-        for(int a = 0; a <= amount; ++a) {
-            if (a % coins[0] == 0) {
+        for(int a = 0; a <= amount; a++) {
+            if(a % coins[0] == 0) {
                 dp[0][a] = a / coins[0];
             } else {
                 dp[0][a] = (int) 1e9;
@@ -13,7 +12,7 @@ class Solution {
         }
 
         for(int i = 1; i < n; ++i) {
-            for(int a = 0; a <= amount; a++) {
+            for(int a = 0; a <= amount; ++a) {
                 int nonPick = dp[i - 1][a];
                 int pick = (int) 1e9;
 
@@ -22,9 +21,9 @@ class Solution {
                 }
 
                 dp[i][a] = Math.min(nonPick, pick);
+
             }
         }
-
         return dp[n - 1][amount] >= (int) 1e9 ? -1 : dp[n - 1][amount];
     }
 }
