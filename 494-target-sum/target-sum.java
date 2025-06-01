e535ea7602081a4422bc3f32ff8c54ccb16aff5d
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-01 10:10:41 +0530

    Time: 12 ms (58.96%) | Memory: 44.8 MB (35.79%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,43 +1,42 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-
         int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
+        for (int num : nums) totalSum += num;
 
-        // if absolute target exceeds totalSum, it's not possible
+        // if absolute value of target is more than totalSum, not possible
         if (Math.abs(target) > totalSum) return 0;
 
+        int offset = totalSum;
 
-        // the sum index could go negative
-        int[][] memo = new int[n][2 * totalSum + 1];
+        int[][] dp = new int[n][2 * totalSum + 1];
 
-        for(int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
-        
-        return helper(nums, target, n - 1, 0, memo, totalSum);
-    }
-
-    private int helper(int[] nums, int target, int index, int runningSum, int[][] memo, int totalSum) {
-        if(index < 0) { // when all elemnts are used
-            return runningSum == target ? 1 : 0;
+        if(nums[0] == 0) {
+            dp[0][offset] = 2; // +0 and -0 both give same results
+        } else {
+            dp[0][nums[0] + offset] = 1;
+            dp[0][-nums[0] + offset] = 1;
         }
 
-        int shiftedIndex = runningSum + totalSum;
+        for(int i = 1; i < n; ++i) {
+            for(int sum = -totalSum; sum <= totalSum; ++sum) {
+                int pos = offset + sum;
 
-        if(memo[index][shiftedIndex] != -1) {
-            return memo[index][shiftedIndex];
+                int plus = 0;
+                int minus = 0;
+
+                if((sum + nums[i]) + offset <= 2 * totalSum) {
+                    plus = dp[i - 1][(sum + nums[i]) + offset];
+                }
+
+                if((sum - nums[i]) + offset >= 0) {
+                    minus = dp[i - 1][(sum - nums[i]) + offset];
+                }
+
+                dp[i][pos] = plus + minus;
+            }
         }
 
-        // take
-        int plus = helper(nums, target, index - 1, runningSum + nums[index], memo, totalSum);
-
-        // skip
-        int minus = helper(nums, target, index - 1, runningSum - nums[index], memo, totalSum);
-
-        return memo[index][shiftedIndex] = plus + minus;
+        return dp[n - 1][target + offset];
     }
 }
