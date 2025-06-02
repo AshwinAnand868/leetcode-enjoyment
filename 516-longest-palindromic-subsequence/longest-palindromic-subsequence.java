055bc8487fb93d4e185ba943771d5b4b22ab611f
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-02 22:05:33 +0530

    Time: 36 ms (76.34%) | Memory: 55.4 MB (43.84%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,39 +1,24 @@
-class Solution {
+public class Solution {
     public int longestPalindromeSubseq(String s) {
-        // return lps(s, 0, s.length() - 1);
-        String reversed = new StringBuilder(s).reverse().toString();
-        return lcs(s, reversed);
-    }
-
-    private int lcs(String s, String reversed) {
         int n = s.length();
-        int m = reversed.length();
+        int[][] dp = new int[n][n];
 
-        int[][] dp = new int[n + 1][m + 1];
+        // Base case: one character
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
 
-        // base cases are for length 0 dp[i][0] = 0 or dp[0][j] = 0
-
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                if(s.charAt(i - 1) == reversed.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1]; // shrink both strings
+        // Build the table bottom-up
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // shrink one and the other
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        return dp[n][m];
+        return dp[0][n - 1]; // Entire string
     }
-
-    // private int lps(String s, int i, int j) {
-    //     if(i > j) return 0;
-    //     if(i == j) return 1;
-
-    //     if(s.charAt(i) == s.charAt(j)) { // 2 chars are equal
-    //         return 2 + lps(s, i + 1, j - 1);
-    //     } else {
-    //         return Math.max(lps(s, i, j - 1), lps(s, i + 1, j));
-    //     }
-    // }
 }
