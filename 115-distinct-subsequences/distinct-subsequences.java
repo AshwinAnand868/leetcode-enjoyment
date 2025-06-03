285a41056b741a7bfe195b8d41f3687a6f92a188
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-03 16:44:55 +0530

    Time: 19 ms (65.07%) | Memory: 44.9 MB (92.42%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -2,53 +2,24 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-        
-        int[][] dp = new int[n + 1][m + 1];
 
-        // base case 1 - if t is empty
-        for(int i = 0; i <= n; ++i) {
-            dp[i][0] = 1; // there is always an empty subsequence
-        }
+        int[] prev = new int[m + 1];
+        prev[0] = 1; // base case: empty t is a subsequence of any s
 
-        // base case 2 - if s is empty
-        for(int i = 1; i <= m; ++i) {
-            dp[0][i] = 0;
-        }
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m + 1];
+            curr[0] = 1; // empty t is always a subsequence
 
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                if(s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[i][j] += dp[i - 1][j - 1];
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    curr[j] = prev[j - 1];
                 }
-                dp[i][j] += dp[i - 1][j];
+                curr[j] += prev[j];
             }
+
+            prev = curr; // move to the next row
         }
 
-        return dp[n][m];
-
-        // for(int[] row : dp) {
-        //     Arrays.fill(row, -1);
-        // }
-
-        // return helper(s, t, n - 1, m - 1, dp);
-    }
-
-    private int helper(String s, String t, int sdx, int tdx, int[][] dp) {
-        if(tdx < 0) return 1; // full t string has been matched
-        if(sdx < 0) return 0; // s is exhausted, but t didn't match
-
-        int count = 0;
-
-        if(dp[sdx][tdx] != -1) {
-            return dp[sdx][tdx];
-        }
-
-        if(s.charAt(sdx) == t.charAt(tdx)) {
-            count += helper(s, t, sdx - 1, tdx - 1, dp); // including matching character
-        }
-
-        count += helper(s, t, sdx - 1, tdx, dp); // skipping character
-
-        return dp[sdx][tdx] = count;
+        return prev[m];
     }
 }
