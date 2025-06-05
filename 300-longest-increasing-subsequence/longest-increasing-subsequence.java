1bf8439a3ca8bae0f1a016fc99beb061e0db8216
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-05 17:17:16 +0530

    Time: 365 ms (13.62%) | Memory: 79.7 MB (9.16%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,57 +1,32 @@
 class Solution {
-    
-    private int backtrack(int[] nums, int index, int previousIndex, int[][] dp) {
-        if(index >= nums.length) { // if index goes out of bounds, return 0 - no LIS to consider
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] memo = new int[n][n + 1];
+
+        for(int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return helper(nums, 0 , -1, memo);
+    }
+
+    private int helper(int[] nums, int index, int prevIndex, int[][] memo) {
+        if(index == nums.length) {
             return 0;
         }
 
-        if(previousIndex != -1 && dp[index][previousIndex] != -1) {
-            return dp[index][previousIndex];
+        if(memo[index][prevIndex + 1] != -1) { // if it is -1, then shift the index
+            return memo[index][prevIndex + 1];
         }
 
-        // take current index
+        // skip the current num
+        int skip = helper(nums, index + 1, prevIndex, memo);
 
+        // take the current num
         int take = 0;
-        if(previousIndex == -1 || nums[index] > nums[previousIndex]) {
-            take = backtrack(nums, index + 1, index, dp) + 1;
+        if(prevIndex == -1 || nums[index] > nums[prevIndex]) {
+            take = 1 + helper(nums, index + 1, index, memo);
         }
 
-        int skip = backtrack(nums, index + 1, previousIndex, dp);
-
-        if(previousIndex != -1) {
-            dp[index][previousIndex] = Math.max(take, skip);
-        }
-
-        return Math.max(take, skip);
-    }
-
-    public int lengthOfLIS(int[] nums) {
-
-        // track the LIS till each index i
-        int[] dp = new int[nums.length];
-
-        // initially each LIS is of length 1
-        Arrays.fill(dp, 1);
-
-        int maxLength = 1; // initial max length is 1 only
-        int n = nums.length;
-
-        for(int i = 0; i < n; ++i) { // loop through each element of nums
-            for(int j = 0; j < i; ++j) {
-                if(nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    maxLength = Math.max(dp[i], maxLength);
-                }
-            }
-        }
-
-        return maxLength;
-
-        // int n = nums.length;
-        // int[][] dp = new int[n + 1][n + 1];
-        // for(int[] row : dp) {
-        //     Arrays.fill(row, -1);
-        // }
-        // return backtrack(nums, 0, -1, dp);
+        return memo[index][prevIndex + 1] = Math.max(skip, take);
     }
 }
