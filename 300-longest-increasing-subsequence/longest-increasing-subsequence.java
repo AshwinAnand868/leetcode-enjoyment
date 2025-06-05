25d9a994278e9754d83374a7b3b1721d58ab137f
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-05 17:56:19 +0530

    Time: 6 ms (87.72%) | Memory: 44.1 MB (77.23%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,32 +1,17 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] memo = new int[n][n + 1];
-
-        for(int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
-        return helper(nums, 0 , -1, memo);
-    }
-
-    private int helper(int[] nums, int index, int prevIndex, int[][] memo) {
-        if(index == nums.length) {
-            return 0;
+        List<Integer> sub = new ArrayList<>();
+        // Binary search
+        for(int num : nums) {
+            int i = Collections.binarySearch(sub, num);
+            if(i < 0) i = -(i + 1);
+            if(i == sub.size()) {
+                sub.add(num);
+            } else {
+                sub.set(i, num);
+            }
         }
 
-        if(memo[index][prevIndex + 1] != -1) { // if it is -1, then shift the index
-            return memo[index][prevIndex + 1];
-        }
-
-        // skip the current num
-        int skip = helper(nums, index + 1, prevIndex, memo);
-
-        // take the current num
-        int take = 0;
-        if(prevIndex == -1 || nums[index] > nums[prevIndex]) {
-            take = 1 + helper(nums, index + 1, index, memo);
-        }
-
-        return memo[index][prevIndex + 1] = Math.max(skip, take);
+        return sub.size();
     }
 }
