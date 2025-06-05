b1b91a7a5b48b931ac5a168598fa7df86a475aa1
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-05 11:13:22 +0530

    Time: 60 ms (38.69%) | Memory: 62.8 MB (42.60%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -2,15 +2,42 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
 
-        int[][][] dp = new int[n][2][2];
-
-        for (int[][] twoD : dp) {
-            for (int[] row : twoD) {
-                Arrays.fill(row, -1);
+        int[][][] dp = new int[n + 1][2][3];
+        
+        for(int buy = 0; buy <= 1; buy++) {
+            for(int transaction = 0; transaction <= 2; transaction++) {
+                dp[n][buy][transaction] = 0;
             }
         }
 
-        return helper(prices, 0, 1, 0, dp);
+        for(int i = 0; i <= n; ++i) {
+            for(int buy = 0; buy <= 1; buy++) {
+                dp[i][buy][2] = 0; // when 2 transactions are completed
+            }
+        }
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                for(int t = 0; t <= 1; ++t) {
+                    if(buy == 1) {
+                        dp[i][buy][t] = Math.max(-prices[i] + dp[i + 1][0][t], dp[i + 1][1][t]);
+                    } else {
+                        dp[i][buy][t] = Math.max(prices[i] + dp[i + 1][1][t + 1], dp[i + 1][0][t]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][0];
+
+        // int[][][] dp = new int[n][2][3];
+        // for (int[][] twoD : dp) {
+        //     for (int[] row : twoD) {
+        //         Arrays.fill(row, -1);
+        //     }
+        // }
+
+        // return helper(prices, 0, 1, 0, dp);
     }
 
     private int helper(int[] prices, int index, int buy, int maxTransactions, int[][][] dp) {
