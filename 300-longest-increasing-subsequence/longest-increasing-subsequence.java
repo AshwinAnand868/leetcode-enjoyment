5a3e21cbeb22d535de1c4348b41127be7d16c1c0
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-06 11:24:09 +0530

    Time: 40 ms (62.30%) | Memory: 43.9 MB (97.51%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
+        // TABULATION ALGORITHM
         int n = nums.length;
+        int[] dp = new int[n];
 
-        int[] next = new int[n + 1]; // by default all 0s
-        int[] curr = new int[n + 1]; // //
+        Arrays.fill(dp, 1);
+        int maxLen = 1;
 
-        for(int i = n - 1; i >= 0; i--) {
-            for(int prev = i - 1; prev >= -1; prev--) {
-                int skip = 0 + next[prev + 1];
-
-                int take = 0;
-                if(prev == -1 || nums[i] > nums[prev]) {
-                    take = 1 + next[i + 1];
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < i; ++j) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-                curr[prev + 1] = Math.max(take, skip);
             }
-            next = curr.clone();
+            maxLen = Math.max(dp[i], maxLen);
         }
-
-        return curr[-1 + 1];
+        return maxLen;
     }
 }
