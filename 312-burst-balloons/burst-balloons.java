84a77b9afd1d36b8ac635c0a9b6ada1180c1be33
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-08 19:30:54 +0530

    Time: 75 ms (57.49%) | Memory: 42.6 MB (66.47%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -8,9 +8,28 @@ class Solution {
         balloonsList.add(1);
 
         int n = nums.length;
-        Integer[][] dp = new Integer[n + 2][n + 2];
 
-        return helper(balloonsList, 1, n, dp);
+        int[][] dp = new int[n + 2][n + 2];
+
+        for(int i = n; i >= 1; --i) {
+            for(int j = i; j <= n; ++j) {
+                int max = 0;
+
+                for (int k = i; k <= j; ++k) {
+                    int coins = balloonsList.get(i - 1) * balloonsList.get(k) * balloonsList.get(j + 1) + dp[i][k - 1] + dp[k + 1][j];
+
+                    max = Math.max(max, coins);
+                }
+
+                dp[i][j] = max;
+            }
+        }
+
+        return dp[1][n];
+
+        // Integer[][] dp = new Integer[n + 2][n + 2];
+
+        // return helper(balloonsList, 1, n, dp);
     }
 
     private int helper(List<Integer> b, int i, int j, Integer[][] dp) {
