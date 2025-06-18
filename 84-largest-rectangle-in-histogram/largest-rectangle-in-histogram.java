3bc53468dab14f86cb4186ef2282312604c95cf1
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-06-18 22:26:06 +0530

    Time: 61 ms (78.76%) | Memory: 57.7 MB (35.47%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,94 +1,26 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int[] leftSmall = new int[n];
-        int[] rightSmall = new int[n];
         Stack<Integer> stack = new Stack<>();
+        int n = heights.length;
 
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                stack.pop();
+        int maxA = Integer.MIN_VALUE;
+
+        for(int i = 0; i <= n; ++i) {
+            while(!stack.isEmpty() && (i == n || heights[stack.peek()] >= heights[i])) {
+                int heightIdx = stack.pop();
+                int height = heights[heightIdx];
+
+                int width = 0;
+                if(stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+                maxA = Math.max(maxA, height * width);
             }
-            leftSmall[i] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(i);
         }
 
-        stack.clear(); // reuse for rightSmall
-
-        // Compute rightSmall[i]
-        for(int i = n - 1; i >= 0; i--) {
-            while(!stack.isEmpty() && heights[stack.peek()] >=  heights[i]) {
-                stack.pop();
-            }
-            rightSmall[i] = stack.isEmpty() ? n : stack.peek();
-            stack.push(i);
-        }
-
-        // Calculate max area
-        int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            int width = rightSmall[i] - leftSmall[i] - 1;
-            int area = heights[i] * width;
-            maxArea = Math.max(maxArea, area);
-        }
-
-        return maxArea;
+        return maxA;
     }
 }
-
-// class Solution {
-//     public int largestRectangleArea(int[] heights) {
-//         int n = heights.length;
-//         int maxArea = Integer.MIN_VALUE;
-
-//         for(int i = 0; i < n; ++i) {
-//             int area = 0;
-//             int width = 1;
-//             if(i == 0) {
-//                 // explore whole right hand side
-//                 for(int j = 1; j < n; ++j) {
-//                     if(heights[j] >= heights[0]) {
-//                         width++;
-//                     } else {
-//                         break;
-//                     }
-//                 }
-//                 area = width * heights[0];
-//             } else if(i == n - 1) {
-//                 // explore whole left hand side
-//                 for(int j = n - 2; j >= 0; --j) {
-//                     if(heights[n - 1] <= heights[j]) {
-//                         width++;
-//                     } else {
-//                         break;
-//                     }
-//                 }
-//                 area = width * heights[n - 1];
-//             } else {
-//                 // explore whole right hand side
-//                 for(int j = i + 1; j < n; ++j) {
-//                     if(heights[j] >= heights[i]) {
-//                         width++;
-//                     } else {
-//                         break;
-//                     }
-//                 }
-
-//                 // explore left hand side
-//                 for(int j = i - 1; j >= 0; --j) {
-//                     if(heights[j] >= heights[i]) {
-//                         width++;
-//                     } else {
-//                         break;
-//                     }
-//                 }
-
-//                 area = width * heights[i];
-//             }
-
-//             maxArea = Math.max(area, maxArea);
-//         }
-
-//         return maxArea;
-//     }
-// }
