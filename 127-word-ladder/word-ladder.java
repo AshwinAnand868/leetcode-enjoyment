0e735813cbdca591293dec5633abd1bc5116df33
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-07-03 13:39:51 +0530

    Time: 89 ms (51.08%) | Memory: 47.8 MB (29.91%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,43 +1,44 @@
-import java.util.*;
-
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
-        if (!wordSet.contains(endWord)) return 0;
+        if(!wordSet.contains(endWord)) return 0;
 
+        // shortest = BFS
         Queue<String> queue = new LinkedList<>();
-        queue.offer(beginWord);
-
+        // to keep track of visited words/nodes
         Set<String> visited = new HashSet<>();
+
+        queue.offer(beginWord);
         visited.add(beginWord);
 
-        int steps = 1;
+        int steps = 1; // inclusive begin word
 
-        while (!queue.isEmpty()) {
-            int size = queue.size(); // number of nodes at current level
+        while(!queue.isEmpty()) {
+            int size = queue.size();
 
-            for (int i = 0; i < size; ++i) {
-                String word = queue.poll();
-                char[] arr = word.toCharArray();
+            for(int i = 0; i < size; ++i) {
+                String curr = queue.poll();
+                char[] currArray = curr.toCharArray();
 
-                for (int j = 0; j < arr.length; ++j) {
-                    char originalChar = arr[j];
-                    for (char c = 'a'; c <= 'z'; ++c) {
-                        arr[j] = c;
-                        String nextWord = new String(arr);
+                for(int j = 0; j < currArray.length; ++j) {
+                    char originalChar = currArray[j];
 
-                        if (nextWord.equals(endWord)) return steps + 1;
-
-                        if (wordSet.contains(nextWord) && !visited.contains(nextWord)) {
-                            queue.offer(nextWord);
-                            visited.add(nextWord);
+                    for(char ch = 'a'; ch <= 'z'; ++ch) {
+                        currArray[j] = ch;
+                        String next = new String(currArray);
+                        if(next.equals(endWord)) return steps + 1; // inclusive end word
+                        if(wordSet.contains(next) && !visited.contains(next)) {
+                            visited.add(next);
+                            queue.offer(next);
                         }
                     }
-                    arr[j] = originalChar; // restore original character
+
+                    currArray[j] = originalChar; // restoring the original character
                 }
             }
 
             steps++;
+        
         }
 
         return 0;
