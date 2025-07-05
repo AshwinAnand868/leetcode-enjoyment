0816004d2d2197cb396a4d2859f54fd4dcba02aa
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-07-05 11:14:36 +0530

    Time: 25 ms (40.33%) | Memory: 46.2 MB (23.36%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+
+        for(char task : tasks) {
+            freq[task - 'A']++;
+        }
+
+        // creating a max heap to maintain the most frequent task at the top
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int f : freq) {
+            if(f > 0) {
+                maxHeap.offer(f);
+            }
+        }
+
+        Queue<int[]> cooldownQ = new LinkedList<>();
+
+        int time = 0;
+
+        // continue processing until priority queue is not empty or cool down queue
+        while(!maxHeap.isEmpty() || !cooldownQ.isEmpty()) {
+            
+            time++;
+
+            if(!cooldownQ.isEmpty() && cooldownQ.peek()[1] <= time) { // the top most task in the queue has been cooled down - ready to processed task
+                maxHeap.offer(cooldownQ.poll()[0]);
+            }
+            if(!maxHeap.isEmpty()) {
+                int current = maxHeap.poll();
+                if(current - 1 > 0) {
+                    cooldownQ.offer(new int[] {current - 1, time + n + 1});
+                }
+            }
+        }
+
+
+        return time;
+    }
+}
