e49c7cfb244cc6930e37b02c8c3ebc23c72600ee
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-07-25 10:54:21 +0530

    Time: 1 ms (59.42%) | Memory: 44.7 MB (90.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,17 +1,45 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
 
+        int count = 0;
+        int i = 0;
+        int j = 1;
         int n = nums.length;
-        int i = 1;
-        
-        for(int j = 1; j < n; ++j) {
-            if(nums[j] != nums[j - 1]) {
-                nums[i] = nums[j];
+
+        while(j < n) {
+            if(nums[i] != nums[j]) {
+                nums[i + 1] = nums[j];
                 i++;
+                j++;
+            } else {
+                j++; // move jth pointer
             }
         }
 
-        return i;
+        return i + 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // // two pointers approach
         // int i = 0; // slow pointer
         // int j = 1; // fast pointer
