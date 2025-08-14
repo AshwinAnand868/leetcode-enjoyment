e5d193b0939321b7bcd9bbd27f50e50a1cbb8ae7
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-08-14 07:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (80.02%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,13 +1,26 @@
 class Solution(object):
-    def isHappy(self, n):
+    def isHappy(self, num):
         """
-        :type n: int
+        :type num: int
         :rtype: bool
         """
-        seen = set()
+        if(num == 1 or num == 7):
+            return True
+        elif(num < 9):
+            return False
 
-        while(n != 1 and n not in seen):
-            seen.add(n)
-            n = sum(int(digit) ** 2 for digit in str(n))
-        return n == 1
+        sum_ = 0
+        while(num > 0):
+            temp = num % 10
+            sum_ += temp * temp
+            num = num/10
+        
+        return self.isHappy(sum_)
+
+        # seen = set()
+
+        # while(n != 1 and n not in seen):
+        #     seen.add(n)
+        #     n = sum(int(digit) ** 2 for digit in str(n))
+        # return n == 1
         
