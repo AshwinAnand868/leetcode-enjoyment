3f2fd65fb0861a154184b199dba668b3aa432360
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-10-15 22:48:33 +0530

    Time: 16 ms (20.35%) | Memory: 50 MB (85.01%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,53 +1,27 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int element1 = Integer.MIN_VALUE;
-        int element2 = Integer.MIN_VALUE;
-        
-        // maximum elements that can occur more than n / 3 times are only two
-        // elements for any given array
-        int count1 = 0;
-        int count2 = 0;
-        
-        for(int i = 0; i < nums.length; ++i) {
-            if(count1 == 0 && element2 != nums[i]) {
-                count1 = 1;
-                element1 = nums[i];
-            } else if(count2 == 0 && element1 != nums[i]) {
-                count2 = 1;
-                element2 = nums[i];
-            } else if(nums[i] == element1) {
-                count1++;
-            } else if(nums[i] == element2) {
-                count2++;
-            } else {
-                count1--;
-                count2--;
-            }
-        }
-        
-        
-        count1 = 0;
-        count2 = 0;
-        for(int elem : nums) {
-            if(elem == element1) {
-                count1++;
-            }
-            if(elem == element2) {
-                count2++;
-            }
-        }
-        
-        
-        List<Integer> res = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        int majorityCount = (int) Math.floor(n / 3);
 
-        if(count1 > (nums.length / 3)) {
-            res.add(element1);
+        for(int i = 0; i < nums.length; ++i) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
         }
-        
-        if(count2 > (nums.length / 3)) {
-            res.add(element2);
+
+        for(Map.Entry<Integer, Integer> keyValue : map.entrySet()) {
+            int key = keyValue.getKey();
+            int value = keyValue.getValue();
+
+            if(value > majorityCount) {
+                result.add(key);
+            }
         }
-        
-        return res;
+
+        return result;
     }
 }
