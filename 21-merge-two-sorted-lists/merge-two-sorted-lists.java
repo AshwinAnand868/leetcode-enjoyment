f8649efd1957ef041e4b566a36cb7a0b930889c8
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-10-24 23:04:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (61.05%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,55 +10,27 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode pointer = dummy;
 
-        if(list1 == null && list2 == null) {
-            return null;
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                pointer.next = list1;
+                list1 = list1.next;
+            } else {
+                pointer.next = list2;
+                list2 = list2.next;
+            }
+
+            pointer = pointer.next;
         }
 
         if(list1 == null) {
-            return list2;
+            pointer.next = list2;
+        } else {
+            pointer.next = list1;
         }
 
-        if(list2 == null) {
-            return list1;
-        }
-
-        ListNode t1 = list1;
-        ListNode t2 = list2;
-        ListNode resultPointer = new ListNode();
-        ListNode result = resultPointer;
-
-        while(t1 != null && t2 != null) {
-            if(t1.val < t2.val) {
-                resultPointer.val = t1.val;
-                t1 = t1.next;
-            } else {
-                resultPointer.val = t2.val;
-                t2 = t2.next;
-            }
-            resultPointer.next = new ListNode();
-            resultPointer = resultPointer.next;
-        
-        }
-
-        while(t1 != null) {
-            resultPointer.val = t1.val;
-            if(t1.next != null) {
-                resultPointer.next = new ListNode();
-                resultPointer = resultPointer.next;
-            }
-            t1 = t1.next;
-        }
-
-        while(t2 != null) {
-            resultPointer.val = t2.val;
-            if(t2.next != null) {
-                resultPointer.next = new ListNode();
-                resultPointer = resultPointer.next;
-            }
-            t2 = t2.next;
-        }
-
-        return result;
+        return dummy.next;
     }
 }
