16f7f0bb707c18438fa61d17cfab849474c632d6
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-10-26 01:17:57 +0530

    Time: 1 ms (59.17%) | Memory: 46.5 MB (41.89%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,40 +1,27 @@
 class Solution {
-    public int[] getLeftMaxArray(int[] height) {
-        int[] leftMax = new int[height.length];
-        leftMax[0] = height[0];
-
-        for (int i = 1; i < leftMax.length; ++i) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
-        }
-
-        return leftMax;
-    }
-
-    public int[] getRightMaxArray(int[] height) {
-        int n = height.length;
-        int[] rightMax = new int[n];
-        rightMax[n - 1] = height[n - 1];
-
-        for (int i = n - 2; i >= 0; --i) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-        }
-
-        return rightMax;
-    }
-
     public int trap(int[] height) {
-        int[] leftMost = getLeftMaxArray(height);
-        int[] rightMost = getRightMaxArray(height);
-        int sum = 0;
+        int n = height.length;
+        if(n == 0) return 0;
 
-        for (int i = 0; i < height.length; ++i) {
-            int width = 1;
-            int h = Math.min(leftMost[i], rightMost[i]) - height[i];
+        int[] left = new int[n];
+        int[] right = new int[n];
 
-            int area = width * h;
-            sum += area;
+        left[0] = height[0];
+        for(int i = 1; i < n; ++i) {
+            left[i] = Math.max(left[i - 1], height[i]);
         }
 
-        return sum;
+        right[n - 1] = height[n - 1];
+        for(int i= n - 2; i >= 0; --i) {
+            right[i] = Math.max(right[i + 1], height[i]);
+        }
+
+
+        int trapped = 0;
+        for(int i = 0; i < n; ++i) {
+            trapped += Math.min(left[i], right[i]) - height[i];
+        }
+
+        return trapped;
     }
 }
