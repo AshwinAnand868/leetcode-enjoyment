72bc096df4d532e18ec13266f1bb1547da4ffab6
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-10-26 00:24:50 +0530

    Time: 34 ms (28.25%) | Memory: 52.6 MB (19.32%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,54 +1,42 @@
 class Solution {
-    private void twoSum(List<List<Integer>> result, int[] nums, int target, int i, int j) {
-
-        while (i < j) {
-            if (nums[i] + nums[j] > target) {
-                j--;
-            } else if (nums[i] + nums[j] < target) {
-                i++;
-            } else { // target locked
-                while (i < j && nums[i] == nums[i + 1])
-                    i++;
-                while (i < j && nums[j] == nums[j - 1])
-                    j--;
-
-                int n2 = nums[i];
-                int n3 = nums[j];
-
-                List<Integer> resultPart = new ArrayList<>();
-                resultPart.add(-target);
-                resultPart.add(n2);
-                resultPart.add(n3);
-                result.add(resultPart);
-
-                i++; // update both pointers
-                j--;
-            }
-        }
-    }
-
     public List<List<Integer>> threeSum(int[] nums) {
-
-        int n = nums.length;
-
-        if (n < 3) {
-            return new ArrayList<>();
-        }
-
         List<List<Integer>> result = new ArrayList<>();
-
-        // sort the array
+        int n = nums.length;
         Arrays.sort(nums);
 
-        for (int i = 0; i <= n - 3; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) { // if the element is already selected
-                continue; // skip the current iteration
+        for(int i = 0; i < n - 2; ++i) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    result.add(temp);
+
+                    while(left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while(left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
 
-            int n1 = nums[i];
-            int target = -n1;
-
-            twoSum(result, nums, target, i + 1, n - 1);
+            
         }
 
         return result;
