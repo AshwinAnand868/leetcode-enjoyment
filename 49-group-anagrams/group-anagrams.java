1f02addecfa881b3e5113450e2eed114a20fb2df
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-11-04 00:57:53 +0530

    Time: 6 ms (98.53%) | Memory: 49.2 MB (11.96%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,121 +1,22 @@
 class Solution {
-
-    private String generate(String str) {
-        // count chars
-        int[] count = new int[26];
-
-        for(int i = 0; i < str.length(); ++i) {
-            count[str.charAt(i) - 'a']++;
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < 26; ++i) {
-            if(count[i] > 0) {
-                sb.append(String.valueOf((char) (i + 'a')).repeat(count[i]));
-            }
-        }
-
-        return sb.toString();
-    }
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> anagramsMapping = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
+        int n = strs.length;
 
-        for(String str : strs) {
-            String newStr = generate(str);
+        for(int i = 0; i < n; i++) {
+            String tempStr = strs[i];
+            char[] charArray = tempStr.toCharArray();
+            Arrays.sort(charArray);
+            String sortedKey = new String(charArray);
 
-            if(!anagramsMapping.containsKey(newStr)) {
-                anagramsMapping.put(newStr, new ArrayList<>());
+            if(map.containsKey(sortedKey)) {
+                map.get(sortedKey).add(tempStr);
+            } else {
+                map.put(sortedKey, new ArrayList<>());
+                map.get(sortedKey).add(tempStr);
             }
-
-            anagramsMapping.get(newStr).add(str);
         }
 
-        return new ArrayList<>(anagramsMapping.values());
+        return new ArrayList<>(map.values());
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-
-//     private String generate(String input) {
-//         int[] counts = new int[26];
-
-//         for (char ch : input.toCharArray()) {
-//             counts[ch - 'a']++;
-//         }
-
-//         StringBuilder sb = new StringBuilder();
-
-//         for (int i = 0; i < 26; ++i) {
-//             int freq = counts[i];
-
-//             if (freq > 0) {
-//                 sb.append(String.valueOf((char) i + 'a').repeat(freq));
-//             }
-//         }
-
-//         return sb.toString();
-//     }
-
-//     public List<List<String>> groupAnagrams(String[] strs) {
-
-//         int n = strs.length;
-//         Map<String, List<String>> sortedToAnagrams = new HashMap<>();
-//         List<List<String>> result = new ArrayList<>();
-
-//         for (int i = 0; i < n; ++i) {
-//             String temp = strs[i];
-
-//             String newWord = generate(temp);
-
-//             if (sortedToAnagrams.containsKey(newWord)) {
-//                 sortedToAnagrams.get(newWord).add(temp);
-//             } else {
-//                 List<String> tempList = new ArrayList<>();
-//                 tempList.add(temp);
-//                 sortedToAnagrams.put(newWord, tempList);
-//             }
-//         }
-
-//         // for (int i = 0; i < n; ++i) {
-//         // String temp = strs[i];
-
-//         // char[] tempCharArray = temp.toCharArray();
-
-//         // Arrays.sort(tempCharArray);
-
-//         // String sorted = String.valueOf(tempCharArray);
-
-//         // if (sortedToAnagrams.containsKey(sorted)) {
-//         // sortedToAnagrams.get(sorted).add(temp);
-//         // } else {
-//         // List<String> tempList = new ArrayList<>();
-//         // tempList.add(temp);
-//         // sortedToAnagrams.put(sorted, tempList);
-//         // }
-//         // }
-
-//         for (List<String> list : sortedToAnagrams.values()) {
-//             result.add(list);
-//         }
-
-//         return result;
-//     }
-// }
