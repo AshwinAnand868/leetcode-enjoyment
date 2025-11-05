8a9eaca23588b37224bb21db4aa926873ee2ea68
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-11-05 23:45:18 +0530

    Time: 11 ms (30.54%) | Memory: 50.6 MB (5.64%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,39 @@
 class Solution {
+
+    public String generateString(String str) {
+        int[] count = new int[26];
+        int n = str.length();
+
+        for(int i = 0; i < n; ++i) {
+            count[str.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < 26; ++i) {
+            int freq = count[i];
+
+            if(freq > 0) {
+                sb.append(String.valueOf(i + 'a').repeat(freq));
+            }
+        }
+
+        return sb.toString();
+    }
+
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         int n = strs.length;
 
         for(int i = 0; i < n; i++) {
-            String tempStr = strs[i];
-            char[] charArray = tempStr.toCharArray();
-            Arrays.sort(charArray);
-            String sortedKey = new String(charArray);
+           
+            String sortedKey = generateString(strs[i]);
 
             if(map.containsKey(sortedKey)) {
-                map.get(sortedKey).add(tempStr);
+                map.get(sortedKey).add(strs[i]);
             } else {
                 map.put(sortedKey, new ArrayList<>());
-                map.get(sortedKey).add(tempStr);
+                map.get(sortedKey).add(strs[i]);
             }
         }
 
