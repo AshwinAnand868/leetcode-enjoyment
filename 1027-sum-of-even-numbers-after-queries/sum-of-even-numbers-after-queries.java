86299ae0afb56ecb1e16488eea0ec2c82272a68a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-12-05 10:41:51 +0530

    Time: 4 ms (98.65%) | Memory: 52.5 MB (49.66%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.java
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.java
@@ -1,57 +1,31 @@
 class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
-        int[] answer = new int[queries.length];
-        int j = 0;
+        int[] answers = new int[queries.length];
+        int evenSum = 0;
+        int n = nums.length;
 
-        // calculate initial sum of even values
-        int sumEven = 0;
-
-        for(int i = 0; i < nums.length; ++i) {
+        for(int i = 0; i < n; ++i) {
             if(nums[i] % 2 == 0) {
-                sumEven += nums[i];
+                evenSum += nums[i];
             }
         }
 
-        for(int[] query : queries) {
-            int val = query[0];
-            int index = query[1];
+        for(int i = 0; i < queries.length; ++i) {
+            int currVal = queries[i][0];
+            int currIndex = queries[i][1];
 
-            int old = nums[index];
-
-            if(old % 2 == 0) {
-                sumEven -= old;
+            if(nums[currIndex] % 2 == 0) {
+                evenSum -= nums[currIndex];
             }
 
-            nums[index] = val + nums[index];
-
-            if(nums[index] % 2 == 0) {
-                sumEven += nums[index];
+            nums[currIndex] += currVal;
+            if(nums[currIndex] % 2 == 0) {
+                evenSum += nums[currIndex];
             }
 
-            answer[j] = sumEven;
-
-            // if(old % 2 != 0 && nums[index] % 2 == 0) {
-            //     answer[j] = sumEvenInitial + nums[index];
-            // } else if(old % 2 == 0 && nums[index] % 2 != 0) {
-            //     answer[j] = sumEvenInitial - old;
-            // } else if(old % 2 == 0 && nums[index] % 2 == 0) {
-            //     sumEvenInitial -= old;
-            //     sumEvenInitial += nums[index];
-            //     answer[j] = sumEvenInitial;
-            // }
-
-            // calculate the sum of even values of nums
-            // int sum = 0;
-            // for(int i = 0; i < nums.length; ++i) {
-            //     if(nums[i] % 2 == 0) {
-            //         sum += nums[i];
-            //     }
-            // }
-
-            ++j;
-
+            answers[i] = evenSum;
         }
 
-        return answer;
+        return answers;
     }
 }
