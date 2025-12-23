b10a32a25de16b16a5b9cb0f6e6ee3a2a1491150
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-12-23 11:44:26 +0530

    Time: 6 ms (71.34%) | Memory: 164.4 MB (85.36%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
@@ -1,70 +1,38 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-
         int n = nums.length;
-        int[] result = new int[n];
-        Arrays.fill(result, -1);
 
-        if (2 * k + 1 > n) return result;
-
-        long windowSum = 0;
-        int windowSize = 2 * k + 1;
-        int left = 0;
-
-        for (int right = 0; right < n; right++) {
-            windowSum += nums[right];
-
-            if (right - left + 1 == windowSize) {
-                int mid = left + k;
-                result[mid] = (int)(windowSum / windowSize);
-                windowSum -= nums[left];
-                left++;
-            }
+        if(k == 0) {
+            return nums;
         }
 
-        return result;
+        int[] avgs = new int[n];
+        Arrays.fill(avgs, -1);
 
+        if(n < 2 * k + 1) {
+            return avgs;
+        }
 
-        // int n= nums.length;
-        // long[] prefixes = new long[n];
-        // prefixes[0] = nums[0];
+        long[] prefixSum = new long[n];
+        prefixSum[0] = nums[0];
 
-        // for(int i = 1; i < n; ++i) {
-        //     prefixes[i] = prefixes[ i - 1] + nums[i];
-        // }
+        for(int i = 1; i < n; ++i) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
 
-        // int[] avgs = new int[n];
+        for(int i = k; i < n - k; ++i) {
+            int left = i - k;
+            int right = i + k;
 
-        // for(int i = 0; i < n; ++i) {
-        //     if((i - k < 0) || (i + k >= n)) {
-        //         avgs[i] = -1;
-        //     } else {
+            long sum = prefixSum[right];
 
-        //         int l = i - k;
-        //         int r = i + k;
-        //         long sum = prefixes[r] - (l > 0 ? prefixes[l - 1] : 0);
-        //         avgs[i] = (int) (sum / (2 * k + 1));
-        //     }
-        // }
+            if(left > 0) {
+                sum = sum - prefixSum[left - 1];
+            }
 
-        // return avgs;
+            avgs[i] = (int) (sum / (2 * k + 1));
+        }
 
-        // int n = nums.length;
-        
-
-        // for(int i = 0; i < n; ++i) {
-        //     if((i - k < 0) || (i + k >= n)) {
-        //         avgs[i] = -1;
-        //     } else {
-        //         int sum = 0;
-        //         for(int j = i - k; j <= i + k; ++j) {
-        //             sum += nums[j];
-        //         }
-
-        //         avgs[i] = (int) (sum / (2 * k + 1));
-        //     }
-        // }
-
-        // return avgs;
+        return avgs;
     }
 }
