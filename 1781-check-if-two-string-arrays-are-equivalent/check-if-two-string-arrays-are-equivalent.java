e77e6a8fef0ffd5457baf1fb8a4e2d9fb0fcfdaf
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-12-26 14:49:05 +0530

    Time: 3 ms (8.32%) | Memory: 43 MB (75.96%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,38 +1,32 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        // one approach is to use string builder and append operation, and then
-        // checking at the end if the given words array combined are equal
-
-        // another is the below approach using pointers
-        int i = 0, j = 0;
-        int currWord1I = 0, currWord2I = 0;
+        int i = 0;
+        int j = 0;
+        int currWord1Index = 0;
+        int currWord2Index = 0;
 
         int m = word1.length;
         int n = word2.length;
 
-        while(currWord1I < m && currWord2I < n) {
-            // the arrays can be considered a 2d array
-            // one dimension of String instances, and other
-            // dimension of characters in each String
-            if(word1[currWord1I].charAt(i) != word2[currWord2I].charAt(j)) {
+        while(currWord1Index < m && currWord2Index < n) {
+            if(word1[currWord1Index].charAt(i) != word2[currWord2Index].charAt(j)) {
                 return false;
             }
 
-            // moves pointers to next character in the current words
             i++;
             j++;
 
-            if(i == word1[currWord1I].length()) {
-                i = 0; // moving to the next word of word1
-                currWord1I++;
+            if(i == word1[currWord1Index].length()) {
+                i = 0;
+                currWord1Index++;
             }
 
-            if(j == word2[currWord2I].length()) {
-                j = 0; // moving to the next word of word2
-                currWord2I++;
+            if(j == word2[currWord2Index].length()) {
+                j = 0;
+                currWord2Index++;
             }
         }
 
-        return currWord1I == m && n == currWord2I;
+        return currWord1Index == m && currWord2Index == n;
     }
 }
