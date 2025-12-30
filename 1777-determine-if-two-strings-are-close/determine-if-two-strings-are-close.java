29f23209873acc88d1786d085353f376efc35069
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2025-12-30 14:51:36 +0530

    Time: 12 ms (58.99%) | Memory: 48 MB (35.24%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-
         int m = word1.length();
         int n = word2.length();
 
@@ -10,25 +7,25 @@ class Solution {
             return false;
         }
 
-        for(int i = 0; i < m; ++i) {
-            char char1 = word1.charAt(i);
-            char char2 = word2.charAt(i);
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
 
-            freq1[char1 - 'a']++;
-            freq2[char2 - 'a']++;
+        for(int i = 0; i < m; ++i) {
+            char ch1 = word1.charAt(i);
+            char ch2 = word2.charAt(i);
+
+            freq1[ch1 - 'a']++;
+            freq2[ch2 - 'a']++;
         }
 
-        // 1 : Check if both the word 1 and word 2 have the same characters
         for(int i = 0; i < 26; ++i) {
-            if((freq1[i] != 0 && freq2[i] != 0) || (freq1[i] == 0 && freq2[i] == 0)) {
+            if((freq1[i] == 0 && freq2[i] == 0) || (freq1[i] != 0 && freq2[i] != 0)) {
                 continue;
             }
 
             return false;
         }
 
-
-        // 2: Compare the frequencies in both the arrays
         Arrays.sort(freq1);
         Arrays.sort(freq2);
 
