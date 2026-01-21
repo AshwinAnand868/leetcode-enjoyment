ccb3d640e7beca852e264e25dc167432e1d73682
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-01-21 10:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (71.01%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -12,7 +12,6 @@ class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null || k == 0) return head;
 
-        // Step 1: Find length and tail
         ListNode tail = head;
         int length = 1;
         while (tail.next != null) {
@@ -20,20 +19,16 @@ class Solution {
             length++;
         }
 
-        // Step 2: Make it circular
         tail.next = head;
 
-        // Step 3: Normalize k
         k = k % length;
         int stepsToNewHead = length - k;
 
-        // Step 4: Find new tail
         ListNode newTail = head;
         for (int i = 0; i < stepsToNewHead - 1; i++) {
             newTail = newTail.next;
         }
 
-        // Step 5: Get new head and break the link
         ListNode newHead = newTail.next;
         newTail.next = null;
 
