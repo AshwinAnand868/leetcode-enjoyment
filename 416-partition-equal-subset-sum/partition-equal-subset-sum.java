ef0509ea3c77db50cf88bd92003f78408c051928
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-02-26 11:06:57 +0530

    Time: 17 ms (95.83%) | Memory: 43.8 MB (94.68%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,46 +1,33 @@
 class Solution {
+
     public boolean canPartition(int[] nums) {
-        int totalSum = 0;
+
+        int sum = 0;
 
         for (int num : nums) {
-            totalSum += num;
+            sum += num;
         }
 
-        // If total sum is odd, cannot partition into equal subsets
-        if (totalSum % 2 != 0) {
+        if (sum % 2 != 0) {
             return false;
         }
 
-        int target = totalSum / 2;
-        return subsetWithTargetExists(nums, target);
-    }
+        int target = sum / 2;
 
-    public boolean subsetWithTargetExists(int[] nums, int target) {
-        int n = nums.length;
-        boolean[][] dp = new boolean[n][target + 1];
+        boolean[] dp = new boolean[target + 1];
 
-        // Base case: zero sum is always possible (empty subset)
-        for (int i = 0; i < n; ++i) {
-            dp[i][0] = true;
-        }
+        dp[0] = true;
 
-        // Base case: first element can form a subset if it's <= target
-        if (nums[0] <= target) {
-            dp[0][nums[0]] = true;
-        }
+        for (int num : nums) {
 
-        for (int i = 1; i < n; ++i) {
-            for (int varTarget = 1; varTarget <= target; varTarget++) {
-                boolean notPick = dp[i - 1][varTarget];
-                boolean pick = false;
-                if (nums[i] <= varTarget) {
-                    pick = dp[i - 1][varTarget - nums[i]];
+            for (int j = target; j >= num; j--) {
+
+                if (dp[j - num]) {
+                    dp[j] = true;
                 }
-
-                dp[i][varTarget] = pick || notPick;
             }
         }
 
-        return dp[n - 1][target];
+        return dp[target];
     }
 }
