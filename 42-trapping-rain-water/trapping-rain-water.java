59b7c788d2d843457b0c212195bed0b4716c391a
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-03-11 00:36:12 +0530

    Time: 1 ms (60.04%) | Memory: 47.7 MB (68.68%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -3,23 +3,27 @@ class Solution {
         int n = height.length;
         if(n == 0) return 0;
 
-        int[] left = new int[n];
-        int[] right = new int[n];
+        int left = 0;
+        int right = n - 1;
 
-        left[0] = height[0];
-        for(int i = 1; i < n; ++i) {
-            left[i] = Math.max(left[i - 1], height[i]);
-        }
-
-        right[n - 1] = height[n - 1];
-        for(int i= n - 2; i >= 0; --i) {
-            right[i] = Math.max(right[i + 1], height[i]);
-        }
-
+        int maxLeft = 0, maxRight = 0;
 
         int trapped = 0;
-        for(int i = 0; i < n; ++i) {
-            trapped += Math.min(left[i], right[i]) - height[i];
+
+        while(left <= right) {
+            if(height[left] <= height[right]) {
+                if(height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } 
+                trapped += maxLeft - height[left];
+                left++;
+            } else {
+                if(height[right] >= maxRight) {
+                    maxRight = height[right];
+                }
+                trapped += maxRight - height[right];
+                right--;
+            }
         }
 
         return trapped;
