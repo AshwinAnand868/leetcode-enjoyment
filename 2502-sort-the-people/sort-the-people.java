13d93898c5b80716d7480a918fe899f460a18655
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-04-13 19:32:04 +0530

    Time: 7 ms (98.60%) | Memory: 47.3 MB (42.39%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,20 +1,11 @@
 class Solution {
-    public String[] sortPeople(String[] names, int[] h) {
+    public String[] sortPeople(String[] names, int[] heights) {
+        TreeMap<Integer, String> treeMap = new TreeMap<>((a, b) -> Integer.compare(b, a));
         int n = names.length;
-
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(h[i] < h[j]) {
-                    int temp = h[i];
-                    h[i] = h[j];
-                    h[j] = temp;
-                    String temp2 = names[i];
-                    names[i] = names[j];
-                    names[j] = temp2;
-                }
-            }
+        for(int i = 0; i < n; ++i) {
+            treeMap.put(heights[i], names[i]);
         }
 
-        return names;
+        return treeMap.values().toArray(new String[n]);
     }
 }
