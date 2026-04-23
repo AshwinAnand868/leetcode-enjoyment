e444a5def1f87add8d921d1312b89d9411cfbaa5
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-04-23 06:26:26 +0530

    Time: 4 ms (93.12%) | Memory: 47.1 MB (49.47%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] p) {
         List<List<Integer>> adjList = new ArrayList<>();
 
-        int[] indegree = new int[numCourses];
+        // int[] indegree = new int[numCourses];
 
         for(int i = 0; i < numCourses; ++i) {
             adjList.add(new ArrayList<>());
@@ -12,41 +12,70 @@ class Solution {
             int a = p[i][0];
             int b = p[i][1];
 
-            adjList.get(b).add(a);
-            indegree[a]++;
+            adjList.get(a).add(b);
+            // indegree[b]++;
         }
 
-       return topology(adjList, numCourses, indegree);
-    }
+       boolean[] visited = new boolean[numCourses];
+       boolean[] pathVisited = new boolean[numCourses];
 
-    private boolean topology(List<List<Integer>> adjList, int numCourses, int[] indegree) {
-        Queue<Integer> queue = new LinkedList<>();
-        int count = 0;
-
-        for(int i = 0; i < numCourses; ++i) {
-            if(indegree[i] == 0) {
-                queue.offer(i);
-                count++;
-            }
-        }
-
-        while(!queue.isEmpty()) {
-            int node = queue.poll();
-
-            for(int neighbour : adjList.get(node)) {
-                indegree[neighbour]--;
-
-                if(indegree[neighbour] == 0) {
-                    queue.offer(neighbour);
-                    count++;
+       for(int i = 0; i < numCourses; ++i) {
+            if(!visited[i]) {
+                if(dfs(i, adjList, visited, pathVisited)) {
+                    return false;
                 }
             }
+       }
+
+        return true;
+    }
+
+    private boolean dfs(int u, List<List<Integer>> adjList, boolean[] visited, boolean[] pathVisited) {
+        visited[u] = true;
+        pathVisited[u] = true;
+
+        for(int v : adjList.get(u)) {
+            if(!visited[v]) {
+                if(dfs(v, adjList, visited, pathVisited)) {
+                    return true;
+                }
+            } else if(pathVisited[v]) {
+                return true;
+            }
         }
 
-        if(count == numCourses) {
-            return true;
-        }
-
+        pathVisited[u] = false; // backtrack
         return false;
     }
+
+    // private boolean topology(List<List<Integer>> adjList, int numCourses, int[] indegree) {
+    //     Queue<Integer> queue = new LinkedList<>();
+    //     int count = 0;
+
+    //     for(int i = 0; i < numCourses; ++i) {
+    //         if(indegree[i] == 0) {
+    //             queue.offer(i);
+    //             count++;
+    //         }
+    //     }
+
+    //     while(!queue.isEmpty()) {
+    //         int node = queue.poll();
+
+    //         for(int neighbour : adjList.get(node)) {
+    //             indegree[neighbour]--;
+
+    //             if(indegree[neighbour] == 0) {
+    //                 queue.offer(neighbour);
+    //                 count++;
+    //             }
+    //         }
+    //     }
+
+    //     if(count == numCourses) {
+    //         return true;
+    //     }
+
+    //     return false;
+    // }
 }
