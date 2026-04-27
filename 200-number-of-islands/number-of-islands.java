488561f12d5c5cc0057cf08e0e676674b8ff1438
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-04-27 23:11:11 +0530

    Time: 3 ms (86.38%) | Memory: 52.4 MB (48.52%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,83 +1,34 @@
-class Pair {
-    int i;
-    int j;
-
-    Pair() {
-        this.i = 0;
-        this.j = 0;
-    }
-
-    Pair(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-}
-
 class Solution {
 
-    private int[][] directions = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+    public void dfs(char[][] grid, int[][] dirs, int row, int col, int m, int n) {
+        grid[row][col] = '0';
+        
+        for(int[] dir : dirs) {
+            int newRow = dir[0] + row;
+            int newCol = dir[1] + col;
 
-    public boolean isSafe(char[][] grid, int i, int j) {
-        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == '1') {
-            return true;
-        }
-        return false;
-    }
-
-    public void bfs(char[][] grid, int i, int j) {
-        Queue<Pair> pairs = new LinkedList<>();
-        pairs.offer(new Pair(i, j));
-        grid[i][j] = '$';
-
-        while (!pairs.isEmpty()) {
-            Pair pair = pairs.poll();
-
-            for (int[] dir : directions) {
-                int newI = dir[0] + pair.i;
-                int newJ = dir[1] + pair.j;
-                
-                if(isSafe(grid, newI, newJ)) {
-                    pairs.offer(new Pair(newI, newJ));
-                    grid[newI][newJ] = '$';
-                }
+            if(newRow >= 0 && newCol >= 0 &&
+                newRow <= m - 1 && newCol <= n - 1 && grid[newRow][newCol] == '1') {
+                dfs(grid, dirs, newRow, newCol, m, n);
             }
         }
-
     }
 
-    // public void dfs(char[][] grid, int i, int j) {
-    // if(!isSafe(grid, i, j)) {
-    // return;
-    // }
-
-    // grid[i][j] = '$'; // mark it as visited
-
-    // for(int[] dir: directions) {
-    // int newI = dir[0] + i;
-    // int newJ = dir[1] + j;
-
-    // dfs(grid, newI, newJ);
-    // }
-    // }
-
     public int numIslands(char[][] grid) {
-        if (grid.length == 0)
-            return 0;
-
+        int count = 0;
         int m = grid.length;
         int n = grid[0].length;
-        int count = 0;
+        int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    bfs(grid, i, j);
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(grid[i][j] == '1') {
+                    dfs(grid, dirs, i, j, m, n);
                     count++;
                 }
             }
         }
 
         return count;
-
     }
 }
