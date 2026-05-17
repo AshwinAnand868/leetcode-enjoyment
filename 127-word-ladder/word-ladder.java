141058f1a24f35e904f924032a23fcc4620d29e3
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-05-17 14:35:55 +0530

    Time: 75 ms (58.34%) | Memory: 47.7 MB (78.79%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,46 +1,47 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
-        if(!wordSet.contains(endWord)) {
-            return 0;
-        }
 
-        Set<String> visited = new HashSet<>();
-        visited.add(beginWord);
+        if (!wordSet.contains(endWord)) return 0;
+
         Queue<String> queue = new LinkedList<>();
-
-        queue.offer(beginWord);
+        queue.add(beginWord);
 
         int steps = 1;
 
-        while(!queue.isEmpty()) {
-            
+        while (!queue.isEmpty()) {
             int size = queue.size();
 
-            for(int i = 0; i < size; ++i) {
+            for (int i = 0; i < size; ++i) {
                 String curr = queue.poll();
                 char[] arr = curr.toCharArray();
-                for(int k = 0; k < arr.length; ++k) {
-                    char originalCh = arr[k];
-                    for(char ch = 'a'; ch <= 'z'; ++ch) {
-                        arr[k] = ch;
-                        String temp = new String(arr);
-                        if(temp.equals(endWord)) {
+
+                for (int j = 0; j < arr.length; ++j) {
+                    char original = arr[j];
+
+                    for (char ch = 'a'; ch <= 'z'; ++ch) {
+                        if (ch == original) continue;
+
+                        arr[j] = ch;
+                        String newWord = new String(arr);
+
+                        if (newWord.equals(endWord)) {
                             return steps + 1;
                         }
 
-                        if(wordSet.contains(temp) && !visited.contains(temp)) {
-                            visited.add(temp);
-                            queue.offer(temp);
+                        if (wordSet.contains(newWord)) {
+                            queue.offer(newWord);
+                            wordSet.remove(newWord); // mark visited
                         }
                     }
 
-                    arr[k] = originalCh;
+                    arr[j] = original;
                 }
-
             }
+
             steps++;
         }
-        return 0; // if no path exists from begin word to end word
+
+        return 0;
     }
 }
