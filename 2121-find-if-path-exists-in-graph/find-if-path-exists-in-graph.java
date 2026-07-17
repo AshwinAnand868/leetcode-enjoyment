d8cb91ac522a5781447b24bcace5470cf13b9669
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-07-17 14:04:02 +0530

    Time: 59 ms (82.88%) | Memory: 281.8 MB (52.86%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,38 +1,47 @@
 class Solution {
 
-    public void dfs(List<List<Integer>> adjList, int curr, int destination, 
-        boolean[] visited) {
-            visited[curr] = true;
+    public void bfs(List<List<Integer>> adjList, int source, int destination,
+                    boolean[] visited) {
 
-            if(curr == destination) return;
+        Queue<Integer> queue = new LinkedList<>();
 
-            for(int i : adjList.get(curr)) {
-                if(!visited[i])
-                    dfs(adjList, i, destination, visited);
+        queue.offer(source);
+        visited[source] = true;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+
+            if (curr == destination)
+                return;
+
+            for (int neighbor : adjList.get(curr)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.offer(neighbor);
+                }
             }
         }
+    }
 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> adjList = new ArrayList<>();
 
-        int edgeCount = edges.length;
-
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             adjList.add(new ArrayList<>());
         }
 
-        for(int i = 0; i < edgeCount; ++i) {
-            int edge1 = edges[i][0];
-            int edge2 = edges[i][1];
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
 
-            // undirectional
-            adjList.get(edge1).add(edge2);
-            adjList.get(edge2).add(edge1);
+            // Undirected graph
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
         }
 
         boolean[] visited = new boolean[n];
 
-        dfs(adjList, source, destination, visited);
+        bfs(adjList, source, destination, visited);
 
         return visited[destination];
     }
