20d96f533ad1593122bc1ce13f89545d80727856
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-07-29 18:58:03 +0530

    Time: 17 ms (73.91%) | Memory: 82.6 MB (67.55%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,58 +1,59 @@
 class Solution {
+    private int[] findMaxAndMinDay(int[] bloomDay) {
+        int maxBloomDay = Integer.MIN_VALUE;
+        int minBloomDay = Integer.MAX_VALUE;
 
-    public int[] findMaximumAndMinimum(int[] bloomDay) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        int n = bloomDay.length;
-
-        for(int i = 0; i < n; ++i) {
-            max = Math.max(max, bloomDay[i]);
-            min = Math.min(min, bloomDay[i]);
+        for(int i = 0; i < bloomDay.length; ++i) {
+            maxBloomDay = Math.max(maxBloomDay, bloomDay[i]);
+            minBloomDay = Math.min(minBloomDay, bloomDay[i]);
         }
 
-        return new int[] {min, max};
+        return new int[] {minBloomDay, maxBloomDay};
     }
 
-    public boolean checkTotalBouquets(int[] bloomDay, int currentDay, int k, int m) {
-        int nFlowers = 0;
-        int nBouquets = 0;
-        for(int i = 0; i < bloomDay.length; ++i) {
-            if(currentDay >= bloomDay[i]) {
-                nFlowers++;
-                if(nFlowers % k == 0) {
-                    nBouquets++;
+    private boolean possibleWithInMidDays(int[] bloomDay, int m, int k, int daysPassed) {
+        int currentBouqets = 0;
+
+        int flowersPicked = 0;
+
+        for(int i = 0; i < bloomDay.length && currentBouqets < m; ++i) {
+            if(daysPassed >= bloomDay[i]) {
+                flowersPicked++;
+
+                if(flowersPicked == k) {
+                    currentBouqets++;
+                    flowersPicked = 0; // for next bouqet
                 }
-            } else {
-                nFlowers = 0;
+            } else  {
+                flowersPicked = 0;
             }
         }
 
-        return nBouquets >= m;
+        return currentBouqets == m ? true : false;
     }
 
     public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        long val = (long) m * k;
 
-        if(val > (long) n) { // if no of flowers needed is greator than total flowers, return -1
-            return -1;
-        }
+        if((long) m * k > bloomDay.length) return -1;
 
-        int[] minMax = findMaximumAndMinimum(bloomDay);
-        int low = minMax[0];
-        int high = minMax[1];
+        int[] maxMin = findMaxAndMinDay(bloomDay);
+
+        int low = maxMin[0];
+        int high = maxMin[1];
+
+        int ans = Integer.MIN_VALUE;
 
         while(low <= high) {
-            int mid = (high + low) / 2;
+            int mid = low + (high - low) / 2;
 
-            if(checkTotalBouquets(bloomDay, mid, k, m)) {
+            if(possibleWithInMidDays(bloomDay, m, k, mid)) {
+                ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
 
-        return low;
+        return ans == Integer.MIN_VALUE ? -1 : ans;
     }
 }
