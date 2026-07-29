cbfcc1aedf0287b1555a9998f580192b61c5f86f
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-07-29 18:21:07 +0530

    Time: 24 ms (22.28%) | Memory: 47.4 MB (96.73%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,23 +1,43 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;
-        int right = Arrays.stream(piles).max().getAsInt(); // max speed
 
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            int hours = 0;
+    private boolean checkMidIsGood(int[] piles, int hours, int potentialSpeed) {
 
-            for(int pile : piles) {
-                hours += Math.ceil((double)pile / mid);
-            }
+        int currHours = 0;
 
-            if(hours <= h) {
-                right = mid - 1; // reduce the speed to try more
+        for(int i = 0; i < piles.length && currHours <= hours; ++i){
+            if(piles[i] <= potentialSpeed) {
+                currHours++;
             } else {
-                left = mid + 1;
+                int pile = piles[i];
+                currHours += Math.ceil((double) piles[i]/potentialSpeed);
             }
         }
 
-        return left;
+        return currHours > hours ? false : true;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int maxSpeed = Integer.MIN_VALUE;
+
+        for(int i = 0; i < piles.length; ++i) {
+            maxSpeed = Math.max(piles[i], maxSpeed);
+        }
+
+        int low = 1;
+        int high = maxSpeed;
+        int minSpeed = Integer.MAX_VALUE;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(checkMidIsGood(piles, h, mid)) {
+                minSpeed = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return minSpeed;
     }
 }
