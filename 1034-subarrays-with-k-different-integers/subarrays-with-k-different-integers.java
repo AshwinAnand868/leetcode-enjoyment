50d4fa50e1c6766c35185eef8ee5d39782bb07d1
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-08-03 18:00:04 +0530

    Time: 49 ms (57.09%) | Memory: 50.2 MB (43.69%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,32 +1,36 @@
 class Solution {
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        return getNGoodSubarrays(nums, k) - getNGoodSubarrays(nums, k - 1);
-    }
-
-    public int getNGoodSubarrays(int[] nums, int k) {
-
-        if(k < 0) return 0;
-
-        int i = 0, j = 0, n = nums.length;
-        int count = 0;
+    private int subarraysWithAtMostElems(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
+
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+
+        int ans = 0;
 
         while(j < n) {
             map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-
+            
+            // if maps size become greater than k or at most window size
             while(map.size() > k) {
                 map.put(nums[i], map.get(nums[i]) - 1);
-                if(map.get(nums[i]) <= 0) {
+
+                if(map.get(nums[i]) == 0) {
                     map.remove(nums[i]);
                 }
+
                 i++;
             }
-            
 
-            count += (j - i + 1);
+            // number of subarrays ending at j is j - i + 1
+            ans += j - i + 1;
             j++;
         }
 
-        return count;
+        return ans;
+    }
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return subarraysWithAtMostElems(nums, k) - subarraysWithAtMostElems(nums, k - 1);
     }
 }
