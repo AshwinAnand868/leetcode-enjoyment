163857b9220adc08f2cd5b3cd4ec8093163ff691
Ashwin Anand <90273728+AshwinAnand868@users.noreply.github.com>
2026-08-10 15:58:44 +0530

    Time: 32 ms (44.67%) | Memory: 143.3 MB (90.24%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -3,23 +3,48 @@ class Solution {
         int i = 0;
         int n = nums.length;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(b[0],a[0]));
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(b[0],a[0]));
 
-        List<Integer> maxElems = new ArrayList<>();
+        // List<Integer> maxElems = new ArrayList<>();
+        int[] maxElems = new int[n - k + 1];
+        // we maintain a deque for storing elements in the backside
+        // but dq doesn't maintain any order, so we will store
+        Deque<Integer> dq = new ArrayDeque<>();
+        int index = 0;
 
         for(int j = 0; j < n; ++j) {
-            pq.offer(new int[] {nums[j], j});
-
-            if(j - i + 1 == k) {
-                while(pq.size() > 0 && pq.peek()[1] < (j - k + 1)) {
-                    pq.poll();
-                }
-
-                maxElems.add(pq.peek()[0]);
-                ++i;
+            
+            // if the peek index goes out of window remove
+            while(!dq.isEmpty() && dq.peekFirst() < i) {
+                dq.pollFirst();
             }
+
+            // smaller elements than current one is not needed
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[j]) {
+                dq.pollLast();
+            }
+
+            dq.offerLast(j);
+
+            if (j - i + 1 == k) {
+                maxElems[index++] = nums[dq.peekFirst()];
+                i++;
+            }
+            
+            // remove elements
+
+            // pq.offer(new int[] {nums[j], j});
+
+            // if(j - i + 1 == k) {
+            //     while(pq.size() > 0 && pq.peek()[1] < (j - k + 1)) {
+            //         pq.poll();
+            //     }
+
+            //     maxElems.add(pq.peek()[0]);
+            //     ++i;
+            // }
         }
 
-        return maxElems.stream().mapToInt(elem -> elem).toArray();
+        return maxElems;
     }
 }
